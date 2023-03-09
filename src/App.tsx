8d34ec0ef047045ad9f6d3e2a9d37c88e0fd7165
JRijo <99342891+John-Stiff@users.noreply.github.com>
2023-03-09 02:10:07 +0000

    implemented filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import cn from 'classnames';
+import React, { useState } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
+import { Person } from './types/Person';
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [query, setQuery] = useState('');
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+  const { name, born, died } = selectedPerson || {};
+
+  const visiblePeople = peopleFromServer.filter(person => {
+    const lowerName = person.name.toLowerCase();
+
+    return lowerName.includes(query.trim().toLowerCase());
+  });
+
+  const shouldShowDropdown = query.trim().length > 0
+  && visiblePeople.length > 0;
 
   return (
     <main className="section">
       <h1 className="title">
-        {`${name} (${born} = ${died})`}
+        {selectedPerson
+          ? `${name} (${born} = ${died})`
+          : 'No selected person'}
       </h1>
 
       <div className="dropdown is-active">
@@ -17,40 +32,39 @@ export const App: React.FC = () => {
             type="text"
             placeholder="Enter a part of the name"
             className="input"
+            value={query}
+            onChange={e => setQuery(e.target.value)}
           />
         </div>
 
-        <div className="dropdown-menu" role="menu">
-          <div className="dropdown-content">
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Bernard Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Antone Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Petronella de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Hercke</p>
+        {shouldShowDropdown && (
+          <div className="dropdown-menu" role="menu">
+            <div className="dropdown-content">
+              { visiblePeople.map((person, index) => (
+                <div
+                  key={person.slug}
+                  className="dropdown-item"
+                  onClick={() => {
+                    setSelectedPerson(person);
+                    setQuery(person.name);
+                  }}
+                  onKeyDown={() => {}}
+                  role="link"
+                  tabIndex={index}
+                >
+                  <p
+                    className={cn({
+                      'has-text-link': person.sex === 'm',
+                      'has-text-danger': person.sex === 'f',
+                    })}
+                  >
+                    {person.name}
+                  </p>
+                </div>
+              ))}
             </div>
           </div>
-        </div>
+        )}
       </div>
     </main>
   );
