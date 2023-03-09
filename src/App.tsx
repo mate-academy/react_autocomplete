f8f4f9bf4e9c50e5d29043f9cecec3ce48f1e9c9
Elias Montauti <egsm147@gmail.com>
2023-03-09 11:51:19 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import classNames from 'classnames';
-import React, { useEffect, useState } from 'react';
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
+import debounce from 'lodash.debounce';
 import './App.scss';
 import { peopleFromServer } from './data/people';
 import { Person } from './types/Person';
@@ -7,25 +15,72 @@ import { Person } from './types/Person';
 export const App: React.FC = () => {
   const [people, setPeople] = useState<Person[]>([]);
   const [query, setQuery] = useState('');
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [appliedQuery, setAppliedQuery] = useState('');
+  // eslint-disable-next-line max-len
+  const [selectedPerson, setSelectedPerson] = useState<Person | undefined>(undefined);
+
+  // const debounce = (
+  //   f: (...args: any[]) => void,
+  //   delay: number,
+  // ) => {
+  //   let timerId: number;
+
+  //   return (...args: any[]) => {
+  //     clearTimeout(timerId);
+  //     timerId = window.setTimeout(f, delay, ...args);
+  //   };
+  // };
+
+  const applyQuery = useCallback(
+    debounce(
+      setAppliedQuery,
+      1000,
+    ), [],
+  );
 
   useEffect(() => {
     setPeople(peopleFromServer);
+    inputRef.current?.focus();
   }, []);
 
-  const visiblePeople = people.filter((person) => {
-    const inputQuery = query.trim().toLocaleLowerCase();
-    const name = person.name.toLocaleLowerCase();
+  const visiblePeople = useMemo(() => (
+    people.filter((person) => person.name.toLocaleLowerCase().includes(
+      appliedQuery.toLocaleLowerCase().trim(),
+    ))
+  ), [appliedQuery]);
 
-    return name.includes(inputQuery);
-  });
+  const findPerson = (slug: string) => {
+    setSelectedPerson(
+      people.find((person) => person.slug === slug),
+    );
+  };
+
+  // const findPerson = useMemo(() => (
+  //   (slug: string) => setSelectedPerson(
+  //     people.find((person) => person.slug === slug),
+  //   )
+  // ), []);
+
+  // const findPerson = useCallback((slug: string) => {
+  //   setSelectedPerson(
+  //     people.find((person) => person.slug === slug),
+  //   );
+  // }, [people]);
 
   return (
     <main className="section">
-      <h1 className="title">React Autocomplete</h1>
+      <h1
+        className="title"
+      >
+        {selectedPerson
+          ? (`${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`)
+          : ('No selected person')}
+      </h1>
 
       <div className={classNames(
         'dropdown',
-        { 'is-active': query },
+        { 'is-active': appliedQuery },
       )}
       >
         <div className="dropdown-trigger">
@@ -36,14 +91,25 @@ export const App: React.FC = () => {
             value={query}
             onChange={(event) => {
               setQuery(event.target.value);
+              applyQuery(event.target.value);
             }}
+            ref={inputRef}
           />
         </div>
 
         <div className="dropdown-menu" role="menu">
           <div className="dropdown-content">
             {visiblePeople.map((person) => (
-              <div className="dropdown-item">
+              <button
+                type="button"
+                className="dropdown-item"
+                key={person.slug}
+                onClick={() => {
+                  findPerson(person.slug);
+                  setQuery('');
+                  setAppliedQuery('');
+                }}
+              >
                 <p
                   className={classNames(
                     { 'has-text-link': person.sex === 'm' },
@@ -52,8 +118,11 @@ export const App: React.FC = () => {
                 >
                   {person.name}
                 </p>
-              </div>
+              </button>
             ))}
+            {query && visiblePeople.length === 0 && (
+              <p>No matching suggestions</p>
+            )}
           </div>
         </div>
       </div>
