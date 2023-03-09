e4df22613b0173f4664b727b2572f47d30ed7e6f
JRijo <99342891+John-Stiff@users.noreply.github.com>
2023-03-09 22:37:27 +0000

    applied debounce feature to query input element

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,56 @@
 import cn from 'classnames';
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
 import { Person } from './types/Person';
 
+// eslint-disable-next-line @typescript-eslint/ban-types
+function debounce(callback: Function, delay: number) {
+  let timerId = 0;
+
+  return (...args: unknown[]) => {
+    window.clearTimeout(timerId);
+
+    timerId = window.setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+}
+
 export const App: React.FC = () => {
   const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
+  const [isInputActive, setIsInputActive] = useState(false);
   const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
   const { name, born, died } = selectedPerson || {};
+
+  const wrapper = useCallback(
+    debounce(setAppliedQuery, 1000),
+    [],
+  );
+
+  const inputWrapper = useCallback(
+    debounce(setIsInputActive, 1000),
+    [],
+  );
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+
+    setIsInputActive(true);
+    setQuery(value);
+    wrapper(value);
+    inputWrapper(false);
+  };
 
   const visiblePeople = peopleFromServer.filter(person => {
     const lowerName = person.name.toLowerCase();
 
-    return lowerName.includes(query.trim().toLowerCase());
+    return lowerName.includes(appliedQuery.trim().toLowerCase());
   });
 
-  const shouldShowDropdown = query.trim().length > 0
-  && visiblePeople.length > 0;
+  const shouldShowDropdown = appliedQuery.trim().length > 0
+  && visiblePeople.length > 0 && !isInputActive;
 
   return (
     <main className="section">
@@ -33,7 +67,7 @@ export const App: React.FC = () => {
             placeholder="Enter a part of the name"
             className="input"
             value={query}
-            onChange={e => setQuery(e.target.value)}
+            onChange={handleQueryChange}
           />
         </div>
 
