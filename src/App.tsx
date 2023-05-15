c73e651b2f3fb69f91356fcacbb49e110b16e0e8
Romeo <69972110+RomeoKs@users.noreply.github.com>
2023-05-15 13:17:44 +0300

    Update App component to handle search and display results - Added search functionality to filter people based on name - Updated JSX structure to handle search input and results display - Improved accessibility by associating label with input control - Aligned clear button to the right and centered it with the input field

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,70 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React, { useState } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
+import { Person } from './types/Person';
 
 export const App: React.FC = () => {
   const { name, born, died } = peopleFromServer[0];
+  const [query, setQuery] = useState('');
+  const [searchResults, setSearchResults] = useState<Person[] | null>(null);
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const lowerQuery = event.target.value.toLowerCase();
+
+    setQuery(lowerQuery);
+
+    const results = peopleFromServer.filter(
+      (person) => person.name.toLowerCase().includes(lowerQuery),
+    );
+
+    setSearchResults(results);
+  };
+
+  const clearQuery = () => {
+    setQuery('');
+    setSearchResults(null);
+  };
 
   return (
     <main className="section">
-      <h1 className="title">
-        {`${name} (${born} = ${died})`}
-      </h1>
+      <h1 className="title">{`${name} (${born} = ${died})`}</h1>
 
       <div className="dropdown is-active">
         <div className="dropdown-trigger">
-          <input
-            type="text"
-            placeholder="Enter a part of the name"
-            className="input"
-          />
+          <div className="control has-icons-right">
+            <input
+              type="text"
+              placeholder="Enter a part of the name"
+              className="input"
+              value={query}
+              onChange={handleSearch}
+            />
+            <span
+              className="icon is-right"
+            >
+              <button
+                type="button"
+                className="delete"
+                onClick={() => clearQuery()}
+              />
+            </span>
+          </div>
         </div>
 
         <div className="dropdown-menu" role="menu">
           <div className="dropdown-content">
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Bernard Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Antone Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Petronella de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Hercke</p>
-            </div>
+            {searchResults ? (
+              searchResults.map((person) => (
+                <div className="dropdown-item" key={person.slug}>
+                  <p className="has-text-link">{person.name}</p>
+                </div>
+              ))
+            ) : (
+              <div className="dropdown-item">
+                <p className="has-text-danger">No matching suggestions</p>
+              </div>
+            )}
           </div>
         </div>
       </div>
