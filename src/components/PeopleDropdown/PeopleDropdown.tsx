6a7c643189807818b4708a81622881aca0907018
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2023-05-22 23:53:22 +0300

    autocomplete lodash

---
+++ src/components/PeopleDropdown/PeopleDropdown.tsx
@@ -0,0 +1,29 @@
+import { Person } from '../../types/Person';
+
+type Props = {
+  people: Person[];
+  onSelect: (person: Person) => void;
+};
+
+export const PeopleDropdown: React.FC<Props> = ({ people, onSelect }) => {
+  return (
+    <div className="dropdown-content">
+      {people.map(person => (
+        <button
+          type="button"
+          style={{ cursor: 'pointer' }}
+          className="dropdown-item"
+          onClick={() => onSelect(person)}
+        >
+          <p className="has-text-link">{person.name}</p>
+        </button>
+      ))}
+
+      {!!people && (
+        <div className="dropdown-item">
+          No matching suggestion
+        </div>
+      )}
+    </div>
+  );
+};
