fc2f232b05b0c88c3d4a6828cb1afb9941ed5629
Nazar Kharkevych <nkharkevych@gmail.com>
2023-05-30 16:34:17 +0300

    Fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
 import React, { useState } from 'react';
 import './App.scss';
-import { peopleFromServer } from './data/people';
 import { Autocomplete } from './components/Autocomplete';
 import { Person } from './types/Person';
 
 export const App: React.FC = () => {
-  const [{ name, born, died }, setUser] = useState(peopleFromServer[0]);
-  const [userIsSelected, setUserIsSelected] = useState(false);
+  const [user, setUser] = useState<Person | null>(null);
 
-  const handleOnSelected = (user: Person) => {
-    setUserIsSelected(true);
-    setUser(user);
+  const handleOnSelected = (newUser: Person) => {
+    setUser(newUser);
   };
 
   return (
     <main className="section">
       <h1 className="title">
-        {userIsSelected
-          ? `${name} (${born} = ${died})`
+        {user
+          ? `${user.name} (${user.born} = ${user.died})`
           : 'No selected person'}
       </h1>
 
