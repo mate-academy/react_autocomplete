a9a342658e382f99d85994d41d176f3cc429fde4
Anna Holovko <anutkagolovko1472@gmail.com>
2023-06-26 01:47:57 +0300

    solution

---
+++ src/components/DropdownMenu.tsx
@@ -0,0 +1,20 @@
+import { Person } from '../types/Person';
+import { DropdownItem } from './DropdownItem';
+
+export interface Props {
+  people: Person[];
+  onSelected: (person: Person) => void;
+}
+
+export const DropdownMenu: React.FC<Props> = ({ people, onSelected }) => {
+  return (
+    <div className="dropdown-menu" role="menu">
+      <div className="dropdown-content">
+        <DropdownItem
+          people={people}
+          onSelected={onSelected}
+        />
+      </div>
+    </div>
+  );
+};
