56671cb62262c5ec475caf1b42f7c96e96d1386d
Maksym Nemera <nickmitchell@ukr.net>
2023-06-26 18:51:35 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,104 @@
-import React from 'react';
+import {
+  ChangeEvent,
+  FC,
+  MouseEvent,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react';
+import cn from 'classnames';
 import './App.scss';
 import { peopleFromServer } from './data/people';
+import { Person } from './types/Person';
+import { DropDownContent } from './components/DropDownContent/DropDownContent';
 
-export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+const debounce = <T extends (...args: never[]) => void>(
+  func: T,
+  delay: number,
+) => {
+  let timerId: NodeJS.Timeout;
+
+  return (...args: Parameters<T>): void => {
+    clearTimeout(timerId);
+    timerId = setTimeout(func, delay, ...args);
+  };
+};
+
+export const App: FC = () => {
+  const [peoples, setPeoples] = useState<Person[]>([]);
+  const [query, setQuery] = useState('');
+  const [isSelectedQuery, setIsSelectedQuery] = useState(false);
+  const [selectedSlug, setSelectedSlug] = useState('');
+
+  useEffect(() => {
+    setPeoples(peopleFromServer);
+  }, []);
+
+  const queryWithDelay = useCallback(debounce(setIsSelectedQuery, 300), []);
+
+  const changeQuery = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+
+    setQuery(value);
+
+    setQuery(value);
+    if (!value) {
+      queryWithDelay(false);
+    } else {
+      queryWithDelay(true);
+    }
+  };
+
+  const onSelectPerson = (
+    event: MouseEvent,
+    person: Person,
+  ) => {
+    event.preventDefault();
+
+    setSelectedSlug(person.slug);
+    setQuery(person.name);
+    setIsSelectedQuery(false);
+  };
+
+  const visiblePeoples = peoples.filter(person => (
+    person.name.toLowerCase().trim().includes(query.toLowerCase())
+  ));
+
+  const getPeopleBySlug = (slug: string) => {
+    return peoples.find(person => person.slug === slug);
+  };
+
+  const selectedPerson = getPeopleBySlug(selectedSlug);
+  const { name, born, died } = selectedPerson ?? {};
 
   return (
     <main className="section">
       <h1 className="title">
-        {`${name} (${born} = ${died})`}
+        {selectedSlug
+          ? `${name} (${born} = ${died})`
+          : 'No selected person'}
       </h1>
 
-      <div className="dropdown is-active">
+      <div className={cn('dropdown', {
+        'is-active': isSelectedQuery,
+      })}
+      >
         <div className="dropdown-trigger">
           <input
             type="text"
             placeholder="Enter a part of the name"
             className="input"
+            value={query}
+            onChange={changeQuery}
           />
         </div>
 
         <div className="dropdown-menu" role="menu">
-          <div className="dropdown-content">
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Bernard Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Antone Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Petronella de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Hercke</p>
-            </div>
-          </div>
+          <DropDownContent
+            visiblePeoples={visiblePeoples}
+            selectedSlug={selectedSlug}
+            onSelectPerson={onSelectPerson}
+          />
         </div>
       </div>
     </main>
