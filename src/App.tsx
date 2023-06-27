c22e74050e9c8d03710a4f62cda977387c2d3142
Alex Nastychenko <nastychenko1993@gmail.com>
2023-06-27 07:51:08 +0300

    fix remarks

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,29 @@
-import { useCallback, useMemo, useState } from 'react';
-import { debounce } from 'lodash';
+import { useState } from 'react';
 import './App.scss';
 import { Autocomplete } from './components/Autocomplete';
 import { peopleFromServer } from './data/people';
 import { Person } from './types/Person';
 
-type Props = {
-  delay: number;
-};
+export const App: React.FC = () => {
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
 
-export const App: React.FC<Props> = ({ delay = 1000 }) => {
-  const [query, setQuery] = useState('');
-  const [appliedQuery, setAppliedQuery] = useState('');
-  const [showList, setShowList] = useState(true);
-  const [onSelected, setOnSelected] = useState('No selected person');
-
-  const applyQuery = useCallback(
-    debounce(setAppliedQuery, delay),
-    [],
-  );
-
-  const getVisiblePeople = () => {
-    return peopleFromServer
-      .filter(person => person.name.toLocaleLowerCase()
-        .includes(appliedQuery.toLocaleLowerCase()));
-  };
-
-  const visiplePeople = useMemo(
-    getVisiblePeople,
-    [peopleFromServer, appliedQuery],
-  );
-
-  const handlePersonSelect = (person: Person) => {
-    setOnSelected(`${person.name} (${person.born} = ${person.died})`);
-    setShowList(false);
-    setQuery(person.name);
-  };
-
-  const handleInputChange = (inputValue: string) => {
-    setQuery(inputValue);
-    applyQuery(inputValue);
-    setShowList(true);
+  const onSelected = (person: Person) => {
+    setSelectedPerson(person);
   };
 
   return (
     <main className="section">
       <h1 className="title">
-        {onSelected}
+        {selectedPerson
+          ? `${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`
+          : 'No selected person'}
       </h1>
 
-      <div className="dropdown is-active">
-        <div className="dropdown-trigger">
-          <input
-            type="text"
-            placeholder="Enter a part of the name"
-            className="input"
-            value={query}
-            onChange={e => handleInputChange(e.target.value)}
-          />
-        </div>
-
-        {showList && (
-          <Autocomplete
-            people={visiplePeople}
-            onPersonSelect={handlePersonSelect}
-          />
-        )}
-      </div>
+      <Autocomplete
+        people={peopleFromServer}
+        delay={1000}
+        onSelected={onSelected}
+      />
     </main>
   );
 };
--- src/components/Autocomplete.tsx
+++ src/components/Autocomplete.tsx
@@ -1,37 +1,112 @@
-import React from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
+import { debounce } from 'lodash';
+import classNames from 'classnames';
 import { Person } from '../types/Person';
 
 type Props = {
   people: Person[];
-  onPersonSelect: (person: Person) => void;
+  delay: number;
+  onSelected: (person: Person) => void;
 };
 
-export const Autocomplete: React.FC<Props> = React.memo(
-  ({
-    people,
-    onPersonSelect,
-  }) => {
-    return (
-      <div className="dropdown-menu" role="menu">
-        <ul className="dropdown-content">
-          {people.length !== 0
-            ? (people.map(person => (
-              // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
-              <li
-                key={person.slug}
-                className="dropdown-item"
-                onClick={() => onPersonSelect(person)}
-              >
-                <p
-                  className="has-text-link"
-                >
-                  {person.name}
-                </p>
-              </li>
-            )))
-            : ('No matching suggestions')}
-        </ul>
+export const Autocomplete: React.FC<Props> = ({
+  people,
+  delay,
+  onSelected,
+}) => {
+  const [query, setQuery] = useState('');
+  const [showList, setShowList] = useState(true);
+
+  const getVisiblePeople = () => {
+    return people
+      .filter(person => person.name.toLocaleLowerCase()
+        .includes(query.toLocaleLowerCase()));
+  };
+
+  const handleTyping = (value: string) => {
+    setQuery(value);
+    setShowList(true);
+  };
+
+  const applyQuery = useCallback(
+    debounce(handleTyping, delay),
+    [],
+  );
+
+  const handleInputChange = (inputValue: string) => {
+    setQuery(inputValue);
+    applyQuery(inputValue);
+    setShowList(false);
+  };
+
+  const visiplePeople = useMemo(
+    getVisiblePeople,
+    [people, query],
+  );
+
+  const onPersonSelect = (person: Person) => {
+    onSelected(person);
+    setQuery(person.name);
+    setShowList(false);
+  };
+
+  const onPressKey = (person: Person) => {
+    return (event: React.KeyboardEvent<HTMLDivElement>) => {
+      if (event.key === 'Enter') {
+        setQuery(person.name);
+        onSelected(person);
+      }
+    };
+  };
+
+  return (
+    <div
+      className={classNames(
+        'dropdown',
+        { 'is-active': showList },
+      )}
+    >
+      <div className="dropdown-trigger">
+        <input
+          type="text"
+          placeholder="Enter a part of the name"
+          className="input"
+          autoComplete="off"
+          value={query}
+          onChange={e => handleInputChange(e.target.value)}
+        />
       </div>
-    );
-  },
-);
+
+      {showList && (
+        <div className="dropdown-menu" role="menu">
+          <ul className="dropdown-content">
+            {visiplePeople.length
+              ? (visiplePeople.map(person => (
+                <div
+                  role="button"
+                  tabIndex={0}
+                  className="dropdown-item"
+                  key={person.slug}
+                  onClick={() => onPersonSelect(person)}
+                  onKeyUp={() => onPressKey(person)}
+                >
+                  <p
+                    className={person.sex === 'm'
+                      ? 'has-text-link'
+                      : 'has-text-danger'}
+                  >
+                    {person.name}
+                  </p>
+                </div>
+              )))
+              : (
+                <div className="dropdown-item">
+                  No matching suggestions
+                </div>
+              )}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
