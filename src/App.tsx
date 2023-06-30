796553683ebbfec9ac579224b39339ca782004ef
Sviatoslav Koziatynskyi <koziatynskyi2013@gmail.com>
2023-06-30 13:44:38 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, { useMemo, useState, useCallback } from 'react';
+import classNames from 'classnames';
+import { debounce } from 'lodash';
 import './App.scss';
 import { peopleFromServer } from './data/people';
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [query, setQuery] = useState('');
+  const [applyQuery, setApplyQuery] = useState('');
+  const [debounceDelay, setDebounceDelay] = useState(1000);
+  const [selectedPerson, setSelectedPerson] = useState({
+    ...peopleFromServer[0],
+    name: 'No selected person',
+  });
+  const { name, born, died } = selectedPerson;
+  const applyedQuery = useCallback(
+    debounce(setApplyQuery, debounceDelay),
+    [debounceDelay],
+  );
+
+  const visiblePeople = useMemo(() => {
+    if (applyQuery === '') {
+      return [];
+    }
+
+    const peoples = peopleFromServer.filter(el => (
+      el.name.toLocaleLowerCase()
+        .includes(applyQuery.toLocaleLowerCase().trim())
+    ));
+
+    if (peoples.length === 0) {
+      return [{
+        ...peopleFromServer[0],
+        name: 'No matching suggestions',
+        sex: 'no',
+      }];
+    }
+
+    return peoples;
+  }, [peopleFromServer, applyQuery]);
 
   return (
     <main className="section">
       <h1 className="title">
-        {`${name} (${born} = ${died})`}
+        {name === 'No selected person' ? name : `${name} (${born} = ${died})`}
       </h1>
 
       <div className="dropdown is-active">
@@ -17,39 +51,42 @@ export const App: React.FC = () => {
             type="text"
             placeholder="Enter a part of the name"
             className="input"
+            value={query}
+            onChange={e => {
+              setDebounceDelay(1000);
+              setQuery(e.target.value);
+              applyedQuery(e.target.value);
+            }}
           />
         </div>
 
         <div className="dropdown-menu" role="menu">
-          <div className="dropdown-content">
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Haverbeke</p>
+          {visiblePeople.length !== 0 && (
+            <div className="dropdown-content">
+              {visiblePeople.map(el => (
+                // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
+                <div
+                  key={el.name}
+                  className="dropdown-item"
+                  onClick={() => {
+                    setSelectedPerson(el);
+                    setQuery(el.name);
+                    setApplyQuery('');
+                  }}
+                >
+                  <p
+                    className={classNames(
+                      { 'has-text-link': el.sex === 'm' },
+                      { 'has-text-danger': el.sex === 'f' },
+                      { 'has-text-grey-light': el.sex === 'no' },
+                    )}
+                  >
+                    {el.name}
+                  </p>
+                </div>
+              ))}
             </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Bernard Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Antone Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Petronella de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Hercke</p>
-            </div>
-          </div>
+          )}
         </div>
       </div>
     </main>
