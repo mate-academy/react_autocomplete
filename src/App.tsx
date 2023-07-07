c5933673f42d3f0905cbd16c4f569ca4c552a444
Alla Gutsul <allagutsul1990@gmail.com>
2023-07-07 21:05:42 +0300

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,61 @@
-import React, { useCallback, useState } from 'react';
-import './App.scss';
-import debounce from 'lodash.debounce';
+import React, { useState, useMemo, useCallback } from 'react';
 import cn from 'classnames';
+import debounce from 'lodash.debounce';
+import './App.scss';
 import { peopleFromServer } from './data/people';
-import { Dropdown } from './components/Dropdown';
+import { Person } from './types/Person';
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
   const [query, setQuery] = useState('');
   const [appliedQuery, setAppliedQuery] = useState('');
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+  const [isDropdownActive, setIsDropdownActive] = useState(false);
+
+  const delay = 1000;
 
   const applyQuery = useCallback(
-    debounce(setAppliedQuery, 1000),
+    debounce(setAppliedQuery, delay),
     [],
   );
-
-  const appliedQueryNorm = appliedQuery.trim().toLowerCase();
-
-  const filteredPeople = React.useMemo(() => {
-    // console.log(appliedQueryNorm);
-
-    return peopleFromServer.filter(
-      person => person.name.toLowerCase().includes(appliedQueryNorm),
-    );
-  }, [appliedQueryNorm, peopleFromServer]);
 
   const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(event.target.value);
     applyQuery(event.target.value);
   };
 
+  const handleClick = (person: Person) => {
+    setSelectedPerson(person);
+    setQuery(person.name);
+    setIsDropdownActive(false);
+  };
+
+  const visiblePeople = useMemo(() => {
+    setIsDropdownActive(!!appliedQuery);
+
+    if (!appliedQuery) {
+      setSelectedPerson(null);
+      setIsDropdownActive(false);
+    } else {
+      setIsDropdownActive(true);
+    }
+
+    return peopleFromServer.filter(
+      person => person.name.toLowerCase().includes(
+        appliedQuery.trim().toLowerCase(),
+      ),
+    );
+  }, [peopleFromServer, appliedQuery]);
+
   return (
     <main className="section">
       <h1 className="title">
-        {`${name} (${born} = ${died})`}
+        {selectedPerson
+          ? `${selectedPerson?.name} (${selectedPerson?.born} - ${selectedPerson?.died})`
+          : 'No selected person'}
       </h1>
 
       <div className={cn('dropdown', {
-        'is-active': appliedQueryNorm,
+        'is-active': isDropdownActive,
       })}
       >
         <div className="dropdown-trigger">
@@ -49,10 +67,33 @@ export const App: React.FC = () => {
             onChange={handleQueryChange}
           />
         </div>
+
+        <div className="dropdown-menu" role="menu">
+          <div className="dropdown-content">
+            {visiblePeople.length > 0 ? (
+              visiblePeople.map(person => (
+                <div
+                  className="dropdown-item"
+                  key={person.slug}
+                >
+                  <tr
+                    className={cn('has-text-link', {
+                      'has-text-danger': person.sex === 'f',
+                    })}
+                    onClick={() => handleClick(person)}
+                  >
+                    <td>{person.name}</td>
+                  </tr>
+                </div>
+              ))
+            ) : (
+              <div className="dropdown-item">
+                <p className="has-text-danger">No matches</p>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
-      <Dropdown
-        visiblePeople={filteredPeople}
-      />
     </main>
   );
 };
