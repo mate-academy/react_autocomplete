3a36bb77b41cdebc55940fc32a840835ec16bf19
pasternakv19@gmail.com <pasternakv19@gmail.com>
2023-07-17 14:39:23 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import './App.scss';
+import classNames from 'classnames';
 import { peopleFromServer } from './data/people';
+import { Person } from './types/Person';
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [text, setText] = useState('');
+  const [person, setPerson] = useState<Person | null>();
+  const [search, setSearch] = useState(false);
+  const [same, setSame] = useState<Person[]>([]);
+
+  const searchMethod = (value: string) => {
+    setSearch(false);
+    setTimeout(() => {
+      const filtered = peopleFromServer
+        .filter((item) => item.name.includes(value));
+      // const res = filtered
+      //   .find((item) => value.localeCompare(item.name));
+
+      setSame(filtered);
+      // setPerson(res);
+      setSearch(true);
+    }, 1000);
+  };
+
+  useMemo(() => {
+    if (text !== '') {
+      searchMethod(text);
+    } else {
+      setSame([]);
+      setPerson(null);
+    }
+  }, [text]);
 
   return (
     <main className="section">
       <h1 className="title">
-        {`${name} (${born} = ${died})`}
+        { person
+          ? `${person.name} (${person.born} = ${person.died})`
+          : 'No matching suggestions'}
       </h1>
 
       <div className="dropdown is-active">
@@ -17,38 +47,33 @@ export const App: React.FC = () => {
             type="text"
             placeholder="Enter a part of the name"
             className="input"
+            onChange={(e) => setText(e.target.value)}
           />
         </div>
 
         <div className="dropdown-menu" role="menu">
           <div className="dropdown-content">
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Bernard Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Antone Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Petronella de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Hercke</p>
-            </div>
+            { search && (
+              same.map((pers) => {
+                return (
+                  <div
+                    className="dropdown-item"
+                    style={{ cursor: 'pointer' }}
+                    key={pers.name}
+                    onDoubleClick={() => setPerson(pers)}
+                  >
+                    <p
+                      className={classNames(
+                        { 'has-text-danger': pers === person },
+                        { 'has-text-link': pers !== person },
+                      )}
+                    >
+                      {pers.name}
+                    </p>
+                  </div>
+                );
+              })
+            )}
           </div>
         </div>
       </div>
