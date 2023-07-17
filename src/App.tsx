ce51068b823a5e4ccefaf4689a0974929f7c4afe
artkas03 <zhonayer2017@gmail.com>
2023-07-17 18:29:55 +0300

    finished task

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,73 @@
-import React from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import './App.scss';
+import debounce from 'lodash.debounce';
 import { peopleFromServer } from './data/people';
+import { Person } from './types/Person';
+import { PeopleList } from './components/PeopleList';
+
+type FilterParams = {
+  query: string;
+};
+
+function filterPeople(people: Person[], { query }: FilterParams): Person[] {
+  let resultPeople = [...people];
+  const normalazidQuery = query.toLowerCase();
+
+  if (query) {
+    resultPeople = resultPeople.filter((person) => {
+      const normalazidName = person.name.toLowerCase();
+
+      return normalazidName.includes(normalazidQuery);
+    });
+  }
+
+  return resultPeople;
+}
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [query, setQuery] = useState('');
+  const [preparedQuery, setPreparedQueary] = useState('');
+  const setQueryToFilter = useCallback(
+    debounce(setPreparedQueary, 1000),
+    [],
+  );
+
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+
+  const visiblePeople = useMemo(
+    () => filterPeople(peopleFromServer, { query: preparedQuery }),
+    [preparedQuery],
+  );
+
+  function handleOnChange(newQuery: string): void {
+    setQuery(newQuery);
+    setQueryToFilter(newQuery);
+    setSelectedPerson(null);
+  }
+
+  function handleOnClick(person: Person): void {
+    setSelectedPerson(person);
+
+    setQuery(person.name.toString());
+  }
+
+  const dropdownList = (
+    <PeopleList
+      people={visiblePeople}
+      onClick={(newPerson) => handleOnClick(newPerson)}
+    />
+  );
+
+  const isToShowSugestion = preparedQuery
+    && selectedPerson === null
+    && query === preparedQuery;
 
   return (
     <main className="section">
       <h1 className="title">
-        {`${name} (${born} = ${died})`}
+        {selectedPerson
+          ? `${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`
+          : 'No selected person'}
       </h1>
 
       <div className="dropdown is-active">
@@ -17,39 +76,16 @@ export const App: React.FC = () => {
             type="text"
             placeholder="Enter a part of the name"
             className="input"
+            value={query}
+            onChange={(event) => handleOnChange(event.currentTarget.value)}
           />
         </div>
 
         <div className="dropdown-menu" role="menu">
-          <div className="dropdown-content">
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Bernard Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Antone Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Petronella de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Hercke</p>
-            </div>
-          </div>
+          {isToShowSugestion
+          && (visiblePeople.length > 0
+            ? dropdownList
+            : 'No matching suggestions')}
         </div>
       </div>
     </main>
