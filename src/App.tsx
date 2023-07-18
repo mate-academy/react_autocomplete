a1d56ca82df1275b1a7090423e4a62d875bbbcbc
Oleksandr Marchenko <sasha@mate.academy>
2023-07-18 19:58:55 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,90 @@
-import React from 'react';
+import React, {
+  useState, useCallback, useMemo,
+} from 'react';
 import './App.scss';
+import debounce from 'lodash.debounce';
+import classNames from 'classnames';
+import { Person } from './types/Person';
 import { peopleFromServer } from './data/people';
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+  const [showList, setShowList] = useState(false);
+
+  const handlePersonChange = (person: Person) => {
+    setQuery(person.name);
+    setSelectedPerson(person);
+    setShowList(false);
+  };
+
+  const applyQuery = useCallback(
+    debounce(setAppliedQuery, 1000),
+    [],
+  );
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+    applyQuery(event.target.value);
+  };
+
+  const filteredPeople = useMemo(() => {
+    if (!applyQuery) {
+      setSelectedPerson(null);
+      setShowList(false);
+    }
+
+    return peopleFromServer.filter(person => person.name.toLowerCase().trim()
+      .includes(appliedQuery.toLowerCase().trim()));
+  }, [appliedQuery, peopleFromServer]);
 
   return (
     <main className="section">
       <h1 className="title">
-        {`${name} (${born} = ${died})`}
+        {selectedPerson ? `${selectedPerson.name} (${selectedPerson.born} = ${selectedPerson.died})` : 'No selected person'}
       </h1>
 
-      <div className="dropdown is-active">
+      <div className={classNames('dropdown', { 'is-active': showList })}>
         <div className="dropdown-trigger">
           <input
             type="text"
             placeholder="Enter a part of the name"
             className="input"
+            value={query}
+            onChange={handleQueryChange}
+            onBlur={() => setShowList(true)}
           />
         </div>
 
         <div className="dropdown-menu" role="menu">
           <div className="dropdown-content">
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Haverbeke</p>
-            </div>
 
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Bernard Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Antone Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Petronella de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Hercke</p>
-            </div>
+            {filteredPeople.length ? (
+              filteredPeople.map(person => (
+                <div
+                  className="dropdown-item"
+                  key={person.slug}
+                >
+                  <button
+                    type="button"
+                    className={classNames({
+                      'has-text-link': person.sex === 'm',
+                      'has-text-danger': person.sex === 'f',
+                    })}
+                    onClick={() => handlePersonChange(person)}
+                  >
+                    {person.name}
+                  </button>
+                </div>
+              ))
+            ) : (
+              <div className="dropdown-item">
+                <p className="has-text-danger">
+                  No matching suggestions
+                </p>
+              </div>
+            )}
           </div>
         </div>
       </div>
