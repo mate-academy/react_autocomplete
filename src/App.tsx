d6c2be1bfed65f206fb7d1d2a9a2b59f231f8924
Eleonora Demikh <deademikh@gmail.com>
2023-07-19 15:35:16 +0300

    Solution without delay

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,90 @@
-import React from 'react';
+import classNames from 'classnames';
+import React, { useState } from 'react';
+
 import './App.scss';
 import { peopleFromServer } from './data/people';
+import { Person } from './types/Person';
+
+const getPreparedPeople = (people: Person[], { query }) => {
+  let filteredPeople = [...people];
+
+  if (query && typeof query === 'string') {
+    filteredPeople = filteredPeople.filter(
+      person => person.name.toLowerCase().includes(query.trim().toLowerCase()),
+    );
+  }
+
+  return filteredPeople;
+};
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [query, setQuery] = useState('');
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+
+  const visiblePeople = getPreparedPeople(
+    peopleFromServer,
+    { query },
+  );
+
+  const handleSelectPerson = (
+    pers: Person,
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    event.preventDefault();
+    setSelectedPerson(pers);
+    setQuery(pers.name);
+  };
 
   return (
     <main className="section">
       <h1 className="title">
-        {`${name} (${born} = ${died})`}
+        {selectedPerson
+          ? `${selectedPerson?.name} (${selectedPerson?.born} - ${selectedPerson?.died})`
+          : 'No selected person'}
       </h1>
 
-      <div className="dropdown is-active">
+      <div
+        className={classNames('dropdown', {
+          'is-active': query && (query !== selectedPerson?.name),
+        })}
+      >
         <div className="dropdown-trigger">
           <input
             type="text"
             placeholder="Enter a part of the name"
             className="input"
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
           />
         </div>
 
         <div className="dropdown-menu" role="menu">
           <div className="dropdown-content">
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Bernard Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Antone Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Petronella de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Hercke</p>
-            </div>
+            {visiblePeople.length > 0
+              ? (visiblePeople.map(vPerson => (
+                <a
+                  href="#/"
+                  className="dropdown-item"
+                  key={vPerson.slug}
+                  onClick={(event) => handleSelectPerson(vPerson, event)}
+                >
+                  <p
+                    className={classNames({
+                      'has-text-link': vPerson.sex === 'm',
+                      'has-text-danger': vPerson.sex === 'f',
+                    })}
+                  >
+                    {vPerson.name}
+                  </p>
+                </a>
+              )))
+              : (
+                <div className="dropdown-item">
+                  <p>
+                    No matching suggestions
+                  </p>
+                </div>
+              )}
           </div>
         </div>
       </div>
