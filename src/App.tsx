6cdafd493f920abb38a0c26e26b0be18d32c3eb1
Ihor Oksentiuk <winbka.lem@gmail.com>
2023-07-19 19:34:55 +0300

    Finished task

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useState, useMemo, useCallback } from 'react';
+import { debounce } from 'lodash';
 import './App.scss';
 import { peopleFromServer } from './data/people';
+import { PersonList } from './components/PersonList';
+import { Person } from './types/Person';
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [query, setQuery] = useState('');
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+  const [appliedQuery, setAppliedQuery] = useState('');
+  const delay = 1000;
+
+  const filteredPeople = useMemo(() => {
+    const newQuery = query.toLowerCase().trim();
+
+    return peopleFromServer.filter(
+      person => person.name.toLowerCase().includes(newQuery),
+    );
+  }, [appliedQuery]);
+
+  const applyQuery = useCallback(debounce(setAppliedQuery, delay), []);
+
+  const selectPerson = useCallback((person: Person) => {
+    setSelectedPerson(person);
+    setQuery('');
+    setAppliedQuery('');
+  }, []);
 
   return (
     <main className="section">
       <h1 className="title">
-        {`${name} (${born} = ${died})`}
+        {
+          selectedPerson
+            ? `${selectedPerson.name} (${selectedPerson.born} = ${selectedPerson.died})`
+            : 'No selected person'
+        }
       </h1>
 
       <div className="dropdown is-active">
@@ -17,40 +43,23 @@ export const App: React.FC = () => {
             type="text"
             placeholder="Enter a part of the name"
             className="input"
+            value={query}
+            onChange={(event) => {
+              setQuery(event.target.value);
+              applyQuery(event.target.value);
+            }}
           />
         </div>
 
-        <div className="dropdown-menu" role="menu">
-          <div className="dropdown-content">
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Haverbeke</p>
+        {appliedQuery
+          && (
+            <div className="dropdown-menu" role="menu">
+              <PersonList
+                people={filteredPeople}
+                onSelect={selectPerson}
+              />
             </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Bernard Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Antone Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Petronella de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Hercke</p>
-            </div>
-          </div>
-        </div>
+          )}
       </div>
     </main>
   );
---
+++ src/components/PersonItem.tsx
@@ -0,0 +1,29 @@
+import classNames from 'classnames';
+import { Person } from '../types/Person';
+
+type Props = {
+  person: Person;
+  onSelect?: (person: Person) => void;
+};
+
+export const PersonItem: React.FC<Props> = ({
+  person,
+  onSelect = () => { },
+}) => (
+
+  <button
+    type="button"
+    key={person.slug}
+    className="dropdown-item button"
+    onClick={() => onSelect(person)}
+  >
+    <p className={classNames(
+      'has-text-link',
+      { 'has-text-danger': person.sex === 'f' },
+    )}
+    >
+      {person.name}
+    </p>
+  </button>
+
+);
