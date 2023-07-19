9297f658bbeaa9e921b19dc35d67491a831f1acc
Liza Strykharchuk <ol220681vol@gmail.com>
2023-07-19 20:41:31 +0300

    code

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,53 @@
-import React from 'react';
+import React, { useState, useCallback, useMemo } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
+import { Person } from './types/Person';
+
+import { Dropdown } from './conponents/Dropdown';
+
+function debounce(callback: (...args: string[]) => void, delay: number) {
+  let timer = 0;
+
+  return (...args: string[]) => {
+    window.clearTimeout(timer);
+
+    timer = window.setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+}
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [selectPerson, setSelectPerson] = useState<Person | null>(null);
+  const [query, setQuery] = useState('');
+  const [usedQuery, setUsedQuery] = useState('');
+
+  const timerOfChange = useCallback(debounce(setUsedQuery, 1000), []);
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+    timerOfChange(event.target.value);
+  };
+
+  const filterPeople = useMemo(() => {
+    return peopleFromServer
+      .filter(person => person.name.toLocaleLowerCase()
+        .includes(usedQuery.toLocaleLowerCase()));
+  }, [usedQuery]);
+
+  const onSelected = useCallback((newPeron: Person) => {
+    setSelectPerson(newPeron);
+    setQuery(newPeron.name);
+  }, []);
 
   return (
     <main className="section">
       <h1 className="title">
-        {`${name} (${born} = ${died})`}
+        { selectPerson ? (
+          `${selectPerson.name} (${selectPerson.born} = ${selectPerson.died})`
+        ) : (
+          'No selected person'
+        )}
       </h1>
 
       <div className="dropdown is-active">
@@ -17,39 +56,20 @@ export const App: React.FC = () => {
             type="text"
             placeholder="Enter a part of the name"
             className="input"
+            value={query}
+            onChange={handleOnChange}
           />
         </div>
 
         <div className="dropdown-menu" role="menu">
-          <div className="dropdown-content">
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Bernard Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Antone Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Petronella de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Hercke</p>
-            </div>
-          </div>
+          {filterPeople.length === 0
+            ? (
+              <p className="has-text-danger">No matching suggestions</p>
+            ) : (
+              usedQuery && query !== selectPerson?.name && (
+                <Dropdown people={filterPeople} onSelected={onSelected} />
+              )
+            )}
         </div>
       </div>
     </main>
