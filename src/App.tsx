e2ed56a8167b9d2e70c882fe4b5ffe85cfe8a7b0
Mariia <mariia.monakhova1@gmail.com>
2023-07-20 17:07:26 +0300

    add delay via props

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React, { useCallback, useMemo, useState } from 'react';
-import debounce from 'lodash.debounce';
+import React, { useMemo, useState } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
 import { Person } from './types/Person';
@@ -31,20 +30,10 @@ export const App: React.FC = () => {
     return filterPeople(peopleFromServer, query);
   }, [appliedQuery]);
 
-  const applyQuery = useCallback(
-    debounce(setAppliedQuery, 1000), [],
-  );
-
   const handlePersonSelect = (person: Person) => {
     setSelectedPerson(person);
     setQuery('');
     setAppliedQuery('');
-  };
-
-  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    event.preventDefault();
-    setQuery(event.target.value);
-    applyQuery(event.target.value);
   };
 
   return (
@@ -58,26 +47,16 @@ export const App: React.FC = () => {
 
       </h1>
 
-      <div className="dropdown is-active">
-        <div className="dropdown-trigger">
-          <input
-            type="text"
-            placeholder="Enter a part of the name"
-            className="input"
-            value={query}
-            onChange={(event) => {
-              handleQueryChange(event);
-              setAppliedQuery('');
-            }}
-          />
-        </div>
-        {appliedQuery && (
-          <PeopleList
-            people={filteredPeople}
-            onSelected={handlePersonSelect}
-          />
-        )}
-      </div>
+      {appliedQuery && (
+        <PeopleList
+          people={filteredPeople}
+          onSelected={handlePersonSelect}
+          query={query}
+          setQuery={setQuery}
+          setAppliedQuery={setAppliedQuery}
+          delay={1000}
+        />
+      )}
     </main>
   );
 };
