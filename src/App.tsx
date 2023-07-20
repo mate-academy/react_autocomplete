7935e20647709fad4f43d977604813848af46d75
Anna Shults <anyashults@gmail.com>
2023-07-20 11:09:16 +0300

    fix issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,77 @@
-import React, { useMemo, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
+import debounce from 'lodash.debounce';
 
 import './App.scss';
 import { Dropdown } from './components';
 import { peopleFromServer } from './data/people';
 import { Person } from './types/Person';
 
-export const App: React.FC = () => {
-  const [onSelected, setOnSelected] = useState<Person | null>(null);
+type Props = {
+  delay: number,
+};
 
-  const selectedPerson = useMemo(() => {
-    if (onSelected) {
-      const { name, born, died } = onSelected;
+export const App: React.FC<Props> = ({ delay }) => {
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+  const [value, setValue] = useState('');
+  const [appliedValue, setAppliedValue] = useState('');
 
-      return `${name} (${born} - ${died})`;
-    }
+  const preparedPeople = useMemo(() => {
+    return peopleFromServer.filter(person => (
+      person.name.toLowerCase().includes(appliedValue)
+    )) || null;
+  }, [appliedValue, peopleFromServer]);
 
-    return 'No selected person';
-  }, [onSelected]);
+  const applyValue = useCallback(
+    debounce(setAppliedValue, delay),
+    [delay],
+  );
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const normalisedEvent = event.target.value.toLowerCase();
+
+    setValue(event.target.value);
+    applyValue(normalisedEvent);
+  };
+
+  const handleClick = useCallback((
+    person: Person,
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) => {
+    event.preventDefault();
+
+    setValue(person.name);
+    setSelectedPerson(person);
+    setAppliedValue('');
+  }, []);
 
   return (
     <main className="section">
       <h1 className="title">
-        {selectedPerson}
+        {selectedPerson
+          ? `${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`
+          : 'No selected person'}
       </h1>
 
-      <Dropdown
-        people={peopleFromServer}
-        setOnSelected={setOnSelected}
-        delay={1000}
-      />
+      <div className="dropdown is-active">
+        <div className="dropdown-trigger">
+          <input
+            type="text"
+            placeholder="Enter a part of the name"
+            className="input"
+            value={value}
+            onChange={handleChange}
+          />
+        </div>
+
+        {appliedValue && (
+          <Dropdown
+            people={preparedPeople}
+            handleClick={handleClick}
+          />
+        )}
+      </div>
     </main>
   );
 };
--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,91 +1,42 @@
-import React, { useCallback, useMemo, useState } from 'react';
-import debounce from 'lodash.debounce';
+import React from 'react';
 import cn from 'classnames';
 
 import { Person } from '../../types/Person';
 
 type Props = {
   people: Person[],
-  setOnSelected: (person: Person) => void,
-  delay: number,
+  handleClick: (
+    person: Person, event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ) => void,
 };
 
 export const Dropdown: React.FC<Props> = React.memo(({
   people,
-  setOnSelected,
-  delay,
+  handleClick,
 }) => {
-  const [value, setValue] = useState('');
-  const [appliedValue, setAppliedValue] = useState('');
-
-  const preparedPeople = useMemo(() => {
-    return people.filter(person => (
-      person.name.toLowerCase().includes(appliedValue)
-    )) || null;
-  }, [appliedValue]);
-
-  const applyValue = useCallback(
-    debounce(setAppliedValue, delay),
-    [delay],
-  );
-
-  const handleChange = (
-    event: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    const normalisedEvent = event.target.value.toLowerCase();
-
-    setValue(normalisedEvent);
-    applyValue(normalisedEvent);
-  };
-
-  const handleClick = useCallback((
-    person: Person,
-    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
-  ) => {
-    event.preventDefault();
-
-    setValue(person.name);
-    setOnSelected(person);
-    setAppliedValue('');
-  }, []);
-
   return (
-    <div className="dropdown is-active">
-      <div className="dropdown-trigger">
-        <input
-          type="text"
-          placeholder="Enter a part of the name"
-          className="input"
-          value={value}
-          onChange={handleChange}
-        />
+    <div className="dropdown-menu" role="menu">
+      <div className="dropdown-content">
+        {people.length
+          ? people.map(person => (
+            <a
+              href={`#${person.slug}`}
+              className={cn('dropdown-item', {
+                'has-text-link': person.sex === 'm',
+                'has-text-danger': person.sex === 'f',
+              })}
+              onClick={(event) => handleClick(person, event)}
+              key={person.slug}
+            >
+              {person.name}
+            </a>
+          ))
+          : (
+            <p className="dropdown-item">
+              No matching suggestions
+            </p>
+          )}
       </div>
-
-      {(appliedValue) && (
-        <div className="dropdown-menu" role="menu">
-          <div className="dropdown-content">
-            {preparedPeople.length
-              ? preparedPeople.map(person => (
-                <a
-                  href={`#${person.slug}`}
-                  className={cn('dropdown-item', {
-                    'has-text-link': person.sex === 'm',
-                    'has-text-danger': person.sex === 'f',
-                  })}
-                  onClick={(event) => handleClick(person, event)}
-                  key={person.slug}
-                >
-                  {person.name}
-                </a>
-              ))
-              : (
-                <p className="dropdown-item">
-                  No matching suggestions
-                </p>
-              )}
-          </div>
-        </div>
-      )}
     </div>
   );
 });
