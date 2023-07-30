04492c8c0cbbeabfd1c94692a20e655f94001ddd
anton_chornobai <antonchornobajj@gmail.com>
2023-07-30 14:48:46 +0200

    Task was done, I could add styles to user-buttons such as cursor pointer etc. but i think its unnecessary

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,41 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
+import { PeopleList } from './components/PeopleList';
+import { PeopleSearchBar } from './components/PeopleSearchBar';
+import { Person } from './types/Person';
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
+  const [selectedUser, setSelectedUser] = useState<Person | null>(null);
+
+  const filteredPeople = useMemo(() => {
+    return [...peopleFromServer]
+      .filter((person) => person.name.toLowerCase()
+        .includes(appliedQuery.toLowerCase()));
+  }, [peopleFromServer, appliedQuery]);
 
   return (
     <main className="section">
-      <h1 className="title">
-        {`${name} (${born} = ${died})`}
-      </h1>
+      {selectedUser ? (
+        <h1 className="title">
+          {`${selectedUser.name} (${selectedUser.born} = ${selectedUser.died})`}
+        </h1>
+      ) : (
+        <h1 className="title">No selected person</h1>
+      )}
 
       <div className="dropdown is-active">
-        <div className="dropdown-trigger">
-          <input
-            type="text"
-            placeholder="Enter a part of the name"
-            className="input"
-          />
-        </div>
-
-        <div className="dropdown-menu" role="menu">
-          <div className="dropdown-content">
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Bernard Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Antone Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Petronella de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Hercke</p>
-            </div>
-          </div>
-        </div>
+        <PeopleSearchBar
+          onType={setQuery}
+          query={query}
+          setAppliedQuery={setAppliedQuery}
+          delay={1000}
+        />
+        {appliedQuery && (
+          <PeopleList people={filteredPeople} onSelected={setSelectedUser} />
+        )}
       </div>
     </main>
   );
---
+++ src/components/PeopleSearchBar.tsx
@@ -0,0 +1,48 @@
+import { useCallback } from 'react';
+
+type Props = {
+  onType: (str: string) => void;
+  delay: number;
+  setAppliedQuery: (str: string) => void;
+  query: string;
+};
+
+type DebouncedFunction = (...args: string[]) => void;
+
+const debounce = (callback: DebouncedFunction, delay: number) => {
+  let timerId = 0;
+
+  return (...args: string[]) => {
+    window.clearTimeout(timerId);
+
+    timerId = window.setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+};
+
+export const PeopleSearchBar: React.FC<Props> = ({
+  query,
+  onType = () => {},
+  delay = 1000,
+  setAppliedQuery = () => {},
+}) => {
+  const applyQuery = useCallback(debounce(setAppliedQuery, delay), []);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onType(e.target.value);
+    applyQuery(e.target.value);
+  };
+
+  return (
+    <div className="dropdown-trigger">
+      <input
+        type="text"
+        value={query}
+        placeholder="Enter a part of the name"
+        className="input"
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
