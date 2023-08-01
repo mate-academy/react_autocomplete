27dace013a919293a4dfb5a61c974b59a7ec1bf1
Natalia Tsiutsiura <89_22@ukr.net>
2023-08-01 15:57:42 +0300

    Solution 3

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,27 @@ import classNames from 'classnames';
 import debounce from 'lodash.debounce';
 import './App.scss';
 import 'bulma';
+import '@fortawesome/fontawesome-free/css/all.css';
 import { peopleFromServer } from './data/people';
 import { Person } from './types/Person';
-
-function filterName(people: Person[], query: string): Person[] {
-  const searchQuery = query.trim().toLowerCase();
-
-  if (searchQuery.length) {
-    return people.filter(
-      person => person.name.trim().toLowerCase().includes(searchQuery),
-    );
-  }
-
-  return people;
-}
 
 export const App: React.FC = () => {
   const [selectName, setSelectName] = useState<Person | null>(null);
   const [query, setQuery] = useState('');
   const [applyQuery, setApplyQuery] = useState('');
+  const [visibleName, setVisibleName] = useState(false);
+
+  function filterName(people: Person[], item: string): Person[] {
+    const searchQuery = item.trimStart().toLowerCase();
+
+    if (!searchQuery) {
+      setVisibleName(false);
+    }
+
+    return people.filter(
+      person => person.name.toLowerCase().includes(searchQuery),
+    );
+  }
 
   const selectPerson = useMemo(
     () => filterName(peopleFromServer, applyQuery), [applyQuery],
@@ -31,16 +33,25 @@ export const App: React.FC = () => {
     debounce(setApplyQuery, 1000), [],
   );
 
+  const clearSearch = () => {
+    setVisibleName(false);
+    setSelectName(null);
+    setQuery('');
+  };
+
   const handleQuery = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+
     setQuery(event.target.value);
     appliedQuery(event.target.value);
+    setVisibleName(true);
   };
 
   const setPersonName = (person: Person | null) => {
-    if (person !== null) {
+    if (person) {
       setSelectName(person);
-      setQuery('');
-      setApplyQuery('');
+      setQuery(person.name);
+      setVisibleName(false);
     }
   };
 
@@ -48,26 +59,40 @@ export const App: React.FC = () => {
     <main className="section">
       <h1 className="title">
         {selectName
-          ? `${selectName?.name} (${selectName?.born} = ${selectName?.died})`
+          ? `${selectName.name} (${selectName.born} = ${selectName.died})`
           : 'No selected name'}
       </h1>
 
-      <div className="dropdown is-active">
-        <div className="dropdown-trigger">
+      <div className={classNames('dropdown', { 'is-active': visibleName })}>
+        <div className="dropdown-trigger control has-icons-right">
           <input
-            type="text"
-            placeholder="Enter a part of the name"
+            aria-controls="dropdown-menu"
+            aria-haspopup="true"
             className="input"
+            placeholder="Enter a part of the name"
             value={query}
             onChange={handleQuery}
           />
+
+          <span
+            className="icon is-small is-right"
+            style={{ cursor: 'pointer' }}
+            role="button"
+          >
+            <i
+              className={`fas ${query && 'delete is-small'}`}
+              onClick={clearSearch}
+              onKeyDown={clearSearch}
+              aria-hidden="true"
+            />
+          </span>
         </div>
 
         <div className="dropdown-menu" role="menu">
           <div className="dropdown-content">
             {selectPerson.length === 0 ? (
               <p className="has-text-danger">
-                No suggestions
+                No matches
               </p>
             ) : selectPerson.map((person, i) => (
               <div
