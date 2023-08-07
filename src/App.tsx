4a355a7409b21cf7f2d48621e6de1c2620f8e642
KseniyaGonchar <kseniyadf888@gmail.com>
2023-08-08 00:46:27 +0300

    solution changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,11 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
-import debounce from 'lodash.debounce';
-import cn from 'classnames';
 import { peopleFromServer } from './data/people';
 import { Person } from './types/Person';
 import { Dropdown } from './components/Dropdown';
 
 export const App: React.FC = () => {
-  const [query, setQuery] = useState('');
-  const people = [...peopleFromServer];
-  const [appliedQuery, setAppliedQuery] = useState('');
   const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
-
-  const filteredNames = useMemo(() => {
-    return people
-      .filter(item => item.name.toLocaleLowerCase()
-        .includes(query.toLocaleLowerCase()));
-  }, [appliedQuery]);
-
-  const applyQuery = useCallback(
-    debounce(setAppliedQuery, 1000),
-    [],
-  );
-
-  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setQuery(event.target.value);
-    applyQuery(event.target.value);
-  };
-
-  const selectPerson = useCallback((person: Person) => {
-    setSelectedPerson(person);
-    setQuery('');
-    setAppliedQuery('');
-  }, []);
 
   return (
     <main className="section">
@@ -42,28 +15,10 @@ export const App: React.FC = () => {
           : 'No selected person'}
       </h1>
 
-      <div className={cn(
-        'dropdown',
-        { 'is-active': appliedQuery !== '' },
-      )}
-      >
-        <div className="dropdown-trigger">
-          <input
-            type="text"
-            placeholder="Enter a part of the name"
-            className="input"
-            value={query}
-            onChange={handleQueryChange}
-          />
-        </div>
-
-        <div className="dropdown-menu" role="menu">
-          <Dropdown
-            people={filteredNames}
-            selectPerson={selectPerson}
-          />
-        </div>
-      </div>
+      <Dropdown
+        people={peopleFromServer}
+        selectPerson={(person) => setSelectedPerson(person)}
+      />
     </main>
   );
 };
--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import debounce from 'lodash/debounce';
 import cn from 'classnames';
 import { Person } from '../types/Person';
 
 type Props = {
   people: Person[],
-  selectPerson: (person: Person) => void,
+  selectPerson: (person: Person | null) => void,
 };
 
 export const Dropdown: React.FC<Props> = React.memo(
@@ -12,30 +13,90 @@ export const Dropdown: React.FC<Props> = React.memo(
     people,
     selectPerson,
   }) => {
+    const [query, setQuery] = useState('');
+    const [appliedQuery, setAppliedQuery] = useState('');
+    const [showListOfPeople, setShowListOfPeople] = useState(false);
+
+    const applyQuery = useCallback(
+      debounce(setAppliedQuery, 1000),
+      [],
+    );
+
+    const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setQuery(event.target.value);
+      applyQuery(event.target.value);
+    };
+
+    const onMouseDown
+      = (person: Person) => {
+        setQuery(person.name);
+        setAppliedQuery(person.name);
+        selectPerson(person);
+        setShowListOfPeople(false);
+      };
+
+    const filteredNames = React.useMemo(() => {
+      return people
+        .filter(item => item.name.toLocaleLowerCase()
+          .includes(appliedQuery.toLocaleLowerCase()));
+    }, [people, appliedQuery]);
+
+    const handleClear = useCallback(() => {
+      setShowListOfPeople(false);
+      setQuery('');
+      setAppliedQuery('');
+    }, []);
+
     return (
-      <div className="dropdown-content">
-        {people.length > 0
-          ? (people.map(person => (
-            <button
-              type="button"
-              className="dropdown-item button"
-              key={person.slug}
-              onClick={() => selectPerson(person)}
-            >
-              <p className={cn(
-                { 'has-text-link': person.sex === 'm' },
-                { 'has-text-danger': person.sex === 'f' },
-              )}
-              >
-                {person.name}
-              </p>
-            </button>
-          )))
-          : (
-            <div className="dropdown-item">
-              No matching suggestions
-            </div>
+      <div className="dropdown is-active">
+        <div className="dropdown-trigger">
+          <input
+            type="text"
+            placeholder="Enter a part of the name"
+            className="input"
+            value={query}
+            onChange={handleQueryChange}
+            onFocus={() => setShowListOfPeople(true)}
+            onBlur={() => setShowListOfPeople(false)}
+          />
+        </div>
+        <button
+          aria-label="Close dropdown"
+          onClick={handleClear}
+          type="button"
+          className={cn(
+            'button_delete delete is-medium',
+            { 'is-hidden': query === '' && !showListOfPeople },
           )}
+        />
+
+        {showListOfPeople && (
+          <div className="dropdown-menu" role="menu">
+            <div className="dropdown-content">
+              {filteredNames.map(person => (
+                <button
+                  type="button"
+                  className={cn(
+                    'dropdown-item',
+                    { 'has-text-link': person.sex === 'm' },
+                    { 'has-text-danger': person.sex === 'f' },
+                  )}
+                  key={person.slug}
+                  onMouseDown={() => onMouseDown(person)}
+                >
+                  {person.name}
+                </button>
+              ))}
+
+              {filteredNames.length === 0
+                && (
+                  <div className="dropdown-item">
+                    No matching suggestions
+                  </div>
+                )}
+            </div>
+          </div>
+        )}
       </div>
     );
   },
