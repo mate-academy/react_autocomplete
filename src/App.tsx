d95755505811eb24ebf942f48d64f4da7b8ef864
Rodionsav <rodionsav@mate.academy>
2023-08-15 15:50:34 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import './App.scss';
+import debounce from 'lodash.debounce';
 import { peopleFromServer } from './data/people';
+import { Person } from './types/Person';
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
+  const [people] = useState(peopleFromServer);
+  const [hasClick, setHasClick] = useState(false);
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+
+  const filteredPeople = useMemo(() => {
+    return people.filter(
+      person => person.name.toLowerCase().includes(appliedQuery.toLowerCase()),
+    );
+  }, [people, appliedQuery]);
+
+  const applyQuery = useCallback(debounce(setAppliedQuery, 1000), []);
+
+  const handleQuery = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+
+    setQuery(value);
+    applyQuery(value);
+    setHasClick(value.length > 0);
+  };
+
+  const handleNameSelect = (person: Person) => {
+    setSelectedPerson(person);
+    setHasClick(false);
+    setQuery('');
+  };
 
   return (
     <main className="section">
       <h1 className="title">
-        {`${name} (${born} = ${died})`}
+        {selectedPerson ? `${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})` : 'No selected person'}
       </h1>
 
       <div className="dropdown is-active">
@@ -17,40 +45,33 @@ export const App: React.FC = () => {
             type="text"
             placeholder="Enter a part of the name"
             className="input"
+            value={query}
+            onChange={handleQuery}
+            onClick={() => setHasClick(query.length > 0)}
           />
         </div>
 
-        <div className="dropdown-menu" role="menu">
-          <div className="dropdown-content">
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Bernard Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Antone Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Petronella de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Hercke</p>
+        {hasClick && (
+          <div className="dropdown-menu" role="menu">
+            <div className="dropdown-content">
+              {filteredPeople.length > 0 ? (
+                filteredPeople.map(person => (
+                  <div
+                    onClick={() => handleNameSelect(person)}
+                    className="dropdown-item"
+                    key={person.slug}
+                  >
+                    <p className="has-text-link">{person.name}</p>
+                  </div>
+                ))
+              ) : (
+                <div className="dropdown-item">
+                  <p className="has-text-link">No matching suggestions</p>
+                </div>
+              )}
             </div>
           </div>
-        </div>
+        )}
       </div>
     </main>
   );
