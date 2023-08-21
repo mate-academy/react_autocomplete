1c45ef907b3cb66ce1936e750e0b1c4f8bc21441
Adrian Yevchuk <adrianyevchuk@MacBook-Air-Adrian.local>
2023-08-21 17:33:33 +0300

    beta

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import cn from 'classnames';
 import './App.scss';
+
 import { peopleFromServer } from './data/people';
+import { Person } from './types/Person';
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [query, setQuery] = useState('');
+  const [people] = useState(peopleFromServer);
+  const [isDropdownActive, setIsDropdownActive] = useState(false);
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+
+    setQuery(value);
+  };
+
+  const filteredPeople = people.filter(person => (
+    person.name.toLowerCase().includes(query.toLowerCase())
+  ));
 
   return (
     <main className="section">
       <h1 className="title">
-        {`${name} (${born} = ${died})`}
+        {selectedPerson ? (
+          `${selectedPerson.name} (${selectedPerson.born} = ${selectedPerson.died})`
+        ) : (
+          'No selected person'
+        )}
       </h1>
 
       <div className="dropdown is-active">
@@ -17,39 +37,55 @@ export const App: React.FC = () => {
             type="text"
             placeholder="Enter a part of the name"
             className="input"
+            value={query}
+            onChange={handleQueryChange}
+            onClick={() => {
+              setIsDropdownActive(!isDropdownActive);
+            }}
           />
         </div>
 
-        <div className="dropdown-menu" role="menu">
-          <div className="dropdown-content">
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Haverbeke</p>
-            </div>
+        <div
+          className="dropdown-menu"
+          role="menu"
+        >
+          {isDropdownActive && (
+            <div
+              className="dropdown-content"
+            >
+              {filteredPeople.map(person => {
+                const isSelectedPerson = selectedPerson?.slug === person.slug;
 
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Bernard Haverbeke</p>
+                return (
+                  <div
+                    className="dropdown-item"
+                    role="button"
+                    tabIndex={0}
+                    onClick={() => {
+                      setSelectedPerson(person);
+                      setIsDropdownActive(false);
+                    }}
+                    onKeyDown={(event) => {
+                      if (event.key === 'Enter') {
+                        setSelectedPerson(person);
+                      }
+                    }}
+                  >
+                    <p
+                      className={cn(
+                        'has-text-link',
+                        {
+                          'has-text-danger': isSelectedPerson,
+                        },
+                      )}
+                    >
+                      {person.name}
+                    </p>
+                  </div>
+                );
+              })}
             </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Antone Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Petronella de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Hercke</p>
-            </div>
-          </div>
+          )}
         </div>
       </div>
     </main>
