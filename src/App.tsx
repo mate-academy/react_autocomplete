3ff65c5523ee7e37aa8217e201b745f3561c6ad4
tndeineka <tetianadeinekaa@gmail.com>
2023-08-31 11:59:49 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
+import { Person } from './types/Person';
+import { debounce } from './helpers/debounce';
+import { Dropdown } from './components/Dropdown/Dropdown';
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [selectedPerson, setSelectedPerson] = useState<Person>();
+  const [query, setQuery] = useState('');
+  const [isDropdownActive, setIsDropdownActive] = useState(false);
+
+  const visiblePeople = useMemo(() => {
+    return peopleFromServer.filter(people => people.name
+      .toLowerCase().includes(query.toLowerCase()));
+  }, [query]);
+
+  const selectPerson = (personName: string) => {
+    setSelectedPerson(
+      peopleFromServer.find(person => person.name === personName),
+    );
+    setQuery(personName);
+  };
+
+  const applyQuery = useCallback(debounce(setQuery, 1000), []);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+    applyQuery(event.target.value);
+  };
 
   return (
     <main className="section">
       <h1 className="title">
-        {`${name} (${born} = ${died})`}
+        {selectedPerson
+          ? `${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`
+          : 'No selected person'}
       </h1>
 
       <div className="dropdown is-active">
@@ -17,40 +43,18 @@ export const App: React.FC = () => {
             type="text"
             placeholder="Enter a part of the name"
             className="input"
+            value={query}
+            onChange={handleInputChange}
+            onFocus={() => setIsDropdownActive(true)}
+            onBlur={() => setIsDropdownActive(false)}
           />
         </div>
-
-        <div className="dropdown-menu" role="menu">
-          <div className="dropdown-content">
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Bernard Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Antone Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Petronella de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Hercke</p>
-            </div>
-          </div>
-        </div>
+        {isDropdownActive && (
+          <Dropdown
+            people={visiblePeople}
+            onSelect={selectPerson}
+          />
+        )}
       </div>
     </main>
   );
