5342e9dcc30333f17f52e1813a2294d23416df97
vadymboichenkoAcupower <vadym.boichenko@acupower.co.uk>
2023-09-04 09:35:05 +0300

    autocomplete

--- src/сomponents/Dropdown.tsx
+++ src/сomponents/Dropdown.tsx
@@ -12,6 +12,7 @@ type Props = {
 function debounce(callback: Function, delay: number) {
   let timerId = 0;
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return (...args: any) => {
     window.clearTimeout(timerId);
 
