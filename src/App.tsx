a9d5aafa58ce85499a644b2f1134b504c5ae317f
Anton Tsurkan <wesses1999@gmail.com>
2023-09-08 18:52:51 +0300

    implement DropDown component

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
+import { DropDown } from './components/DropDown';
+import { Person } from './types/Person';
+
+const DELAY = 1000;
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [currentUser, setCurrentUser] = useState<Person | null>(null);
 
   return (
     <main className="section">
       <h1 className="title">
-        {`${name} (${born} = ${died})`}
+        {currentUser
+          ? `${currentUser.name} (${currentUser.born} - ${currentUser.died})`
+          : 'No selected person'}
       </h1>
-
-      <div className="dropdown is-active">
-        <div className="dropdown-trigger">
-          <input
-            type="text"
-            placeholder="Enter a part of the name"
-            className="input"
-          />
-        </div>
-
-        <div className="dropdown-menu" role="menu">
-          <div className="dropdown-content">
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Bernard Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Antone Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Petronella de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Hercke</p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <DropDown
+        people={peopleFromServer}
+        onSelected={setCurrentUser}
+        delay={DELAY}
+      />
     </main>
   );
 };
---
+++ src/components/DropDown/DropDown.tsx
@@ -0,0 +1,126 @@
+import React, {
+  useState,
+  useCallback,
+  useMemo,
+} from 'react';
+
+import debounce from 'lodash.debounce';
+import cn from 'classnames';
+
+import { Person } from '../../types/Person';
+
+interface Props {
+  people: Person[],
+  onSelected?: (user: Person) => void,
+  delay: number,
+}
+
+const filterPeople = (people: Person[], query: string) => {
+  const filterQuery = query.toLowerCase().trim();
+
+  return people.filter(({ name }) => name.toLowerCase().includes(filterQuery));
+};
+
+export const DropDown: React.FC<Props> = ({
+  people,
+  onSelected = () => {},
+  delay,
+}) => {
+  const [humans, setHumans] = useState(people);
+
+  const [userQuery, setUserQuery] = useState('');
+  const [serverQuery, setServerQuery] = useState('');
+
+  const [dropDownVisible, setDropDownVisible] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
+
+  const hasDropDown = dropDownVisible
+  && userQuery === serverQuery && isFocused;
+
+  const filtredPeople = useMemo(
+    () => {
+      const tempPeople = filterPeople(people, serverQuery);
+
+      return tempPeople;
+    },
+    [people, serverQuery],
+  );
+
+  const delayedSetQuery = useCallback(debounce((
+    str: string,
+  ) => {
+    const result = filterPeople(people, str);
+
+    setHumans(result);
+
+    setServerQuery(str);
+    setDropDownVisible(true);
+  }, delay), []);
+
+  const handleQuaryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUserQuery(event.target.value);
+    setDropDownVisible(false);
+    delayedSetQuery(event.target.value);
+  };
+
+  const handleInputBlur = () => {
+    setDropDownVisible(false);
+    setIsFocused(false);
+    setUserQuery('');
+    delayedSetQuery('');
+  };
+
+  const handleInputFocus = () => {
+    setDropDownVisible(true);
+    setIsFocused(true);
+  };
+
+  return (
+    <div className="dropdown is-active">
+      <div className="dropdown-trigger">
+        <input
+          type="text"
+          placeholder="Enter a part of the name"
+          className="input"
+          value={userQuery}
+          onChange={handleQuaryChange}
+          onFocus={handleInputFocus}
+          onBlur={handleInputBlur}
+        />
+      </div>
+
+      <div className="dropdown-menu" role="menu">
+        {hasDropDown && (
+          <div className={cn({
+            'dropdown-content': humans.length,
+          })}
+          >
+            {humans.map((person) => {
+              const { name, slug, sex } = person;
+
+              return (
+                <a
+                  href={`#${slug}`}
+                  className="dropdown-item"
+                  key={slug}
+                  onMouseDown={() => {
+                    onSelected(person);
+                  }}
+                >
+                  <p className={cn({
+                    'has-text-link': sex === 'm',
+                    'has-text-danger': sex === 'f',
+                  })}
+                  >
+                    {name}
+                  </p>
+                </a>
+              );
+            })}
+          </div>
+        )}
+        {!filtredPeople.length && (<p>No matching suggestions</p>)}
+      </div>
+    </div>
+  );
+};
