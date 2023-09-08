e992a8d47aad660d07fec43ad217f18a19b24bbf
Mykhailo Shevchuk <mishashevchuk@gmail.com>
2023-09-09 00:51:03 +0300

    requested changes have been made, debounce functionality was fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
 import React, { useState, useMemo, useCallback } from 'react';
 import './App.scss';
-import classNames from 'classnames';
 import { peopleFromServer } from './data/people';
 import { Person } from './types/Person';
-
-const debounce = (callback: (state: string) => void, delay: number) => {
-  let timerId = 0;
-
-  return (...args: string[]) => {
-    window.clearTimeout(timerId);
-
-    timerId = window.setTimeout(callback, delay, ...args);
-  };
-};
+import { Dropdown } from './components/Dropdown/Dropdown';
+import { debounce } from './services/debounce';
 
 export const App: React.FC = () => {
   const [selectedPerson, setSelectedPerson] = useState<Person>();
+  const [query, setQuery] = useState('');
   const [appliedQuery, setAppliedQuery] = useState('');
   const [isDropdownVisible, setIsDropdownVisible] = useState(false);
   const customDelay = 1000;
@@ -39,8 +31,12 @@ export const App: React.FC = () => {
   const applyQuery = useCallback(debounce(setAppliedQuery, customDelay), []);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setAppliedQuery(event.target.value);
+    setQuery(event.target.value);
     applyQuery(event.target.value);
+  };
+
+  const handleDropdownToggle = (isVisible: boolean) => {
+    setIsDropdownVisible(isVisible);
   };
 
   return (
@@ -57,41 +53,18 @@ export const App: React.FC = () => {
             type="text"
             placeholder="Enter a part of the name"
             className="input"
-            value={appliedQuery}
+            value={query}
             onChange={handleChange}
-            onFocus={() => setIsDropdownVisible(true)}
-            onBlur={() => setIsDropdownVisible(false)}
+            onFocus={() => handleDropdownToggle(true)}
+            onBlur={() => handleDropdownToggle(false)}
           />
         </div>
 
         {isDropdownVisible && (
-          <div className="dropdown-menu" role="menu">
-            <div className="dropdown-content">
-              {filteredPeople.length > 0
-                ? filteredPeople.map((person: Person) => (
-                  <a
-                    href="*"
-                    className="dropdown-item"
-                    onMouseDown={() => {
-                      selectPerson(person.name);
-                    }}
-                    key={person.name}
-                  >
-                    <p className={classNames('has-text-link', {
-                      'has-text-danger': person.sex === 'f',
-                    })}
-                    >
-                      {person.name}
-                    </p>
-                  </a>
-                ))
-                : (
-                  <div>
-                    No matching suggestions
-                  </div>
-                )}
-            </div>
-          </div>
+          <Dropdown
+            people={filteredPeople}
+            onSelected={selectPerson}
+          />
         )}
       </div>
     </main>
---
+++ src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import classNames from 'classnames';
+import { Person } from '../../types/Person';
+
+type Props = {
+  people: Person[];
+  onSelected: (name: string) => void;
+};
+
+export const Dropdown: React.FC<Props> = ({ people, onSelected }) => {
+  return (
+    <div className="dropdown-menu" role="menu">
+      <div className="dropdown-content">
+        {people.length > 0
+          ? people.map((person: Person) => (
+            <a
+              href={person.slug}
+              className="dropdown-item"
+              onMouseDown={() => {
+                onSelected(person.name);
+              }}
+              key={person.slug}
+            >
+              <p className={classNames('has-text-link', {
+                'has-text-danger': person.sex === 'f',
+              })}
+              >
+                {person.name}
+              </p>
+            </a>
+          ))
+          : (
+            <div>
+              No matching suggestions
+            </div>
+          )}
+      </div>
+    </div>
+  );
+};
