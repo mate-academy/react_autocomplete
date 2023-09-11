56748352996a8fa844b19f52fc6f8b3271d49186
Олег Кулик <olegkulik@MacBook-Pro-Oleg.local>
2023-09-11 16:35:30 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,83 @@
-import React from 'react';
+import debounce from 'lodash.debounce';
+import React, { useCallback, useState } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
+import { DropDownContent } from './components/DropDownContent';
+import { Person } from './types/Person';
+
+function showSelectedPersonInfo(selectedPerson: Person | null) {
+  if (selectedPerson) {
+    const { name, born, died } = selectedPerson;
+
+    return `${name} (${born} - ${died})`;
+  }
+
+  return 'No selected person';
+}
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+  const [people, setPeople] = useState<Person[]>(peopleFromServer);
+  const [inputValue, setInputValue] = useState('');
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleSelectPerson = (newPerson: Person) => {
+    setSelectedPerson(newPerson);
+    setInputValue('');
+    setIsFocused(false);
+    setPeople(peopleFromServer);
+  };
+
+  const filterPeople = () => {
+    setIsFocused(true);
+    if (!inputValue) {
+      return;
+    }
+
+    const filteredPeople = peopleFromServer.filter((person) => {
+      const normaliziedInput = inputValue.toLowerCase().trim();
+      const normaliziedName = person.name?.toLowerCase().trim();
+
+      return normaliziedName?.includes(normaliziedInput);
+    });
+
+    setPeople(filteredPeople);
+  };
+
+  const applyInput = useCallback(
+    debounce(filterPeople, 500),
+    [],
+  );
+
+  const handleInputValueChange
+    = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setInputValue(event.target.value);
+      setIsFocused(false);
+      applyInput();
+    };
 
   return (
     <main className="section">
       <h1 className="title">
-        {`${name} (${born} = ${died})`}
+        {showSelectedPersonInfo(selectedPerson)}
       </h1>
 
-      <div className="dropdown is-active">
-        <div className="dropdown-trigger">
-          <input
-            type="text"
-            placeholder="Enter a part of the name"
-            className="input"
-          />
+      {peopleFromServer.length ? (
+        <DropDownContent
+          people={people}
+          onSelected={handleSelectPerson}
+          isFocused={isFocused}
+          onSearch={handleInputValueChange}
+          setIsFocused={setIsFocused}
+          query={inputValue}
+        />
+      ) : (
+        <div className="dropdown-content">
+          <p className="dropdown-item">
+            There are no people to select
+          </p>
         </div>
-
-        <div className="dropdown-menu" role="menu">
-          <div className="dropdown-content">
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Bernard Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Antone Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Petronella de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Hercke</p>
-            </div>
-          </div>
-        </div>
-      </div>
+      )}
     </main>
   );
 };
