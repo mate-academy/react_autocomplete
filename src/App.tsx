05c340fa548ab8874319d73f489a288c9ce9de81
Yuliia Antonenko <shinjisan311@gmail.com>
2023-09-12 19:43:32 +0300

    changed app structure and deleted unuseful functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,11 @@
-import React, { useCallback, useMemo, useState } from 'react';
-import { debounce } from 'lodash';
-import cn from 'classnames';
+import React, { useState } from 'react';
 import './App.scss';
 
-import { peopleFromServer } from './data/people';
 import { Person } from './types/Person';
-import { DropdownMenu } from './DropdownMenu';
-import { DropdownInput } from './DropdownInput';
-
-const search = (people: Person[], query: string) => {
-  const preparedQuery = query.trim().toLowerCase();
-
-  return people
-    .filter(person => person.name.toLowerCase().includes(preparedQuery));
-};
+import { Autocomplete } from './Autocomplete';
 
 export const App: React.FC = () => {
-  const [query, setQuery] = useState('');
-  const [debouncedQuery, setDebouncedQuery] = useState('');
   const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
-  const [isListShown, setIsListShown] = useState(false);
-
-  const applyDebounceQuery = useCallback(
-    debounce(setDebouncedQuery, 800),
-    [],
-  );
-
-  const filteredPeople = useMemo(() => {
-    return search(peopleFromServer, query);
-  }, [debouncedQuery]);
-
-  const onSelect = useCallback(
-    (person: Person) => {
-      setSelectedPerson(person);
-      setIsListShown(false);
-    },
-    [debouncedQuery],
-  );
-
-  const handleQueryChange = useCallback((newQuery: string) => {
-    setQuery(newQuery);
-    applyDebounceQuery(newQuery);
-  }, [debouncedQuery]);
-
-  const handleResetQuery = useCallback(() => {
-    setQuery('');
-    setDebouncedQuery('');
-  }, []);
-
-  const handlePersonClick = useCallback((person: Person) => {
-    setSelectedPerson(person);
-    setQuery(person.name);
-    setDebouncedQuery('');
-  }, []);
 
   return (
     <main className="section">
@@ -62,37 +15,15 @@ export const App: React.FC = () => {
             <div>
               {`${selectedPerson.name}`
                 + ` (${selectedPerson.born} - ${selectedPerson.died})`}
-
-              <span>
-                <button
-                  type="button"
-                  className="delete"
-                  aria-label="close"
-                  onClick={() => setSelectedPerson(null)}
-                />
-              </span>
             </div>
           )
           : 'No selected person'}
       </h1>
 
-      <div className={cn('dropdown', { 'is-active': isListShown })}>
-        <DropdownInput
-          query={query}
-          handleQueryChange={handleQueryChange}
-          setIsListShown={setIsListShown}
-          handleResetQuery={handleResetQuery}
-        />
-
-        {isListShown && (
-          <DropdownMenu
-            filteredPeople={filteredPeople}
-            selectedPerson={selectedPerson}
-            handlePersonClick={handlePersonClick}
-            onSelect={onSelect}
-          />
-        )}
-      </div>
+      <Autocomplete
+        selectedPerson={selectedPerson}
+        setSelectedPerson={setSelectedPerson}
+      />
     </main>
   );
 };
--- src/DropdownItem.tsx
+++ src/DropdownItem.tsx
@@ -9,40 +9,38 @@ type Props = {
   onSelect: (person: Person) => void,
 };
 
-export const DropdownItem: React.FC<Props> = React.memo(
-  ({
-    person,
-    onSelect,
-    selectedPerson,
-    handlePersonClick,
-  }) => {
-    return (
-      <div
-        className={cn('dropdown-item',
-          {
-            'has-background-link-light':
-              selectedPerson?.slug === person.slug,
-          })}
-        key={person.slug}
+export const DropdownItem: React.FC<Props> = ({
+  person,
+  onSelect,
+  selectedPerson,
+  handlePersonClick,
+}) => {
+  return (
+    <div
+      className={cn('dropdown-item',
+        {
+          'has-background-link-light':
+            selectedPerson?.slug === person.slug,
+        })}
+      key={person.slug}
+    >
+      <p
+        className={cn({
+          'has-text-link': person.sex === 'm',
+          'has-text-danger': person.sex === 'f',
+        })}
       >
-        <p
-          className={cn({
-            'has-text-link': person.sex === 'm',
-            'has-text-danger': person.sex === 'f',
-          })}
+        <button
+          type="button"
+          className="selector"
+          onClick={() => {
+            onSelect(person);
+            handlePersonClick(person);
+          }}
         >
-          <button
-            type="button"
-            className="selector"
-            onClick={() => {
-              onSelect(person);
-              handlePersonClick(person);
-            }}
-          >
-            {person.name}
-          </button>
-        </p>
-      </div>
-    );
-  },
-);
+          {person.name}
+        </button>
+      </p>
+    </div>
+  );
+};
