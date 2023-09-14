5ac615ac3579166629fdcbb347e73757787d4eb9
Oleh Hurmanchuk <gyrmanchyk230506@gmail.com>
2023-09-14 21:18:47 +0300

    place input inside dropdownMenu

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,23 @@
-import React, { useCallback, useMemo, useState } from 'react';
-import { debounce } from 'lodash';
-// import debounce from 'lodash.debounce';
+import React, { useState } from 'react';
 import './App.scss';
-import { peopleFromServer } from './data/people';
 import { DropdownMenu } from './components/DropdownMenu/DropdownMenu';
 import { Person } from './types/Person';
 
 export const App: React.FC = () => {
-  const [isListVisible, setIsListVisible] = useState(false);
-  const [query, setQuery] = useState('');
-  const [appliedQuery, setAppliedQuery] = useState('');
   const [selectedUser, setSelectedUser] = useState<Person | null>(null);
-
-  const filteredPeople = useMemo(() => {
-    return peopleFromServer.filter(
-      person => person.name.toLowerCase().includes(appliedQuery.toLowerCase()),
-    );
-  }, [appliedQuery]);
-
-  const applyQuery = useCallback(
-    debounce(setAppliedQuery, 1000),
-    [],
-  );
-
-  const handleInputChange = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) => {
-      setQuery(event.target.value);
-      applyQuery(event.target.value);
-    }, [],
-  );
-
-  const handleUserSelection = useCallback((person: Person) => {
-    setSelectedUser(person);
-    setQuery(person.name);
-    setAppliedQuery(person.name);
-    setIsListVisible(false);
-  }, []);
 
   return (
     <main className="section">
       <h1 className="title">
         {selectedUser
-          ? `${selectedUser?.name} (${selectedUser?.born} = ${selectedUser?.died})`
+          ? `${selectedUser?.name} (${selectedUser?.born} - ${selectedUser?.died})`
           : 'No selected person'}
       </h1>
 
-      <div className="dropdown is-active">
-        <div className="dropdown-trigger">
-          <input
-            type="text"
-            placeholder="Enter a part of the name"
-            className="input"
-            aria-haspopup="true"
-            aria-controls="dropdown-menu"
-            value={query}
-            onChange={handleInputChange}
-            onFocus={() => setIsListVisible(true)}
-
-          />
-        </div>
-
-        {isListVisible && (
-          <DropdownMenu
-            people={filteredPeople}
-            onSelected={handleUserSelection}
-          />
-        )}
-      </div>
+      <DropdownMenu
+        onUserSelection={setSelectedUser}
+        delay={1000}
+      />
     </main>
   );
 };
--- src/components/DropdownMenu/DropdownMenu.tsx
+++ src/components/DropdownMenu/DropdownMenu.tsx
@@ -1,52 +1,97 @@
-import React from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import classNames from 'classnames';
+import { debounce } from 'lodash';
 import { Person } from '../../types/Person';
+import { peopleFromServer } from '../../data/people';
 
 type Props = {
-  people: Person[],
-  onSelected: (person: Person) => void,
+  onUserSelection: (person: Person) => void,
+  delay: number,
 };
 
-export const DropdownMenu: React.FC<Props> = React.memo(
-  ({ people, onSelected }) => {
-    return (
-      <div
-        className="dropdown-menu"
-        role="menu"
-        id="dropdown-menu"
-      >
-        <div className="dropdown-content">
-          {people.length === 0
-            ? (
-              <div className="dropdown-item" style={{ cursor: 'pointer' }}>
-                <p>
-                  No matching suggestions
-                </p>
-              </div>
-            ) : (
-              people.map(person => (
+export const DropdownMenu: React.FC<Props> = ({
+  onUserSelection, delay,
+}) => {
+  const [isListVisible, setIsListVisible] = useState(false);
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
 
-                /* eslint-disable */
-                <div
-                  className="dropdown-item"
-                  key={person.slug}
-                  onClick={() => onSelected(person)}
-                  style={{ cursor: 'pointer' }}
-                  /* eslint-enable */
-                >
-                  <p
-                    className={classNames({
-                      'has-text-link': person.sex === 'm',
-                      'has-text-danger': person.sex === 'f',
-                    })}
-                  >
-                    {person.name}
+  const filteredPeople = useMemo(() => {
+    return peopleFromServer.filter(
+      person => person.name.toLowerCase().includes(appliedQuery.toLowerCase()),
+    );
+  }, [appliedQuery]);
+
+  const applyQuery = debounce(setAppliedQuery, delay);
+
+  const handleUserSelection = useCallback((person: Person) => {
+    onUserSelection(person);
+    setQuery(person.name);
+    setAppliedQuery(person.name);
+    setIsListVisible(false);
+  }, []);
+
+  const handleInputChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setQuery(event.target.value);
+      applyQuery(event.target.value);
+    }, [],
+  );
+
+  return (
+
+    <div className="dropdown is-active">
+      <div className="dropdown-trigger">
+        <input
+          type="text"
+          placeholder="Enter a part of the name"
+          className="input"
+          aria-haspopup="true"
+          aria-controls="dropdown-menu"
+          value={query}
+          onChange={handleInputChange}
+          onFocus={() => setIsListVisible(true)}
+          onBlur={() => setIsListVisible(false)}
+        />
+      </div>
+      {isListVisible && (
+        <div
+          className="dropdown-menu"
+          role="menu"
+          id="dropdown-menu"
+        >
+          <div className="dropdown-content">
+            {!filteredPeople.length
+              ? (
+                <div className="dropdown-item" style={{ cursor: 'pointer' }}>
+                  <p>
+                    No matching suggestions
                   </p>
                 </div>
-              ))
-            )}
+              ) : (
+                filteredPeople.map(person => (
+
+                  <div
+                    className="dropdown-item"
+                    key={person.slug}
+                    onMouseDown={() => handleUserSelection(person)}
+                    style={{ cursor: 'pointer' }}
+                    aria-hidden="true"
+                  >
+                    <p
+                      className={classNames({
+                        'has-text-link': person.sex === 'm',
+                        'has-text-danger': person.sex === 'f',
+                      })}
+                    >
+                      {person.name}
+                    </p>
+                  </div>
+                ))
+              )}
+          </div>
         </div>
-      </div>
-    );
-  },
-);
+      )}
+    </div>
+  );
+};
