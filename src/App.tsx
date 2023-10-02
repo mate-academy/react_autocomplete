aea272714697f17a7605a7036fc144cc18831de7
Oleksandr Chernikov <pjatochkin2013@gmail.com>
2023-10-02 15:39:56 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import classNames from 'classnames';
 import './App.scss';
 import { peopleFromServer } from './data/people';
+import { Person } from './types/Person';
 
 /* eslint-disable  @typescript-eslint/no-explicit-any */
 type Callback = (...args: any[]) => void;
@@ -18,10 +20,11 @@ function debounce(callback: Callback, delay: number) {
 }
 
 export const App: React.FC = () => {
-  const [selectedUser, setSelectedUser] = useState('');
+  const [selectedUser, setSelectedUser] = useState<Person | null>(null);
   const [inputValue, setInputValue] = useState('');
   const [appliedInputValue, setAppliedInputValue] = useState('');
   const [touched, setTouched] = useState(false);
+  const [visiblePeople, setVisiblePeople] = useState(peopleFromServer);
 
   const handleInputFocus = () => {
     setTouched(true);
@@ -34,48 +37,33 @@ export const App: React.FC = () => {
   const handleReset = () => {
     setInputValue('');
     setTouched(false);
+    setSelectedUser(null);
+    setVisiblePeople(peopleFromServer);
   };
 
   const applyValue = useCallback(debounce(setAppliedInputValue, 1000), []);
 
-  // const titleField = useRef<HTMLInputElement>(null);
-
-  // useEffect(() => {
-  //   if(titleField.current) {
-  //     titleField.current.focus()
-  //   }
-  // }, [])
-
-  let foundPeopleFromServer;
-
-  if (selectedUser === '') {
-    foundPeopleFromServer = 0;
-  } else {
-    foundPeopleFromServer = peopleFromServer.findIndex((
-      person,
-    ) => person.name.toLowerCase() === selectedUser.toLowerCase());
-  }
-
-  const { name, born, died } = peopleFromServer[foundPeopleFromServer];
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
     applyValue(event.target.value);
   };
 
-  const foundName = useMemo(() => {
-    return peopleFromServer.filter((person) => {
+  useEffect(() => {
+    const filteredPeople = peopleFromServer.filter((person) => {
       const personName = person.name.toLowerCase();
       const value = appliedInputValue.toLowerCase();
 
       return personName.includes(value);
     });
-  }, [appliedInputValue, foundPeopleFromServer]);
+
+    setVisiblePeople(filteredPeople);
+  }, [appliedInputValue, selectedUser, inputValue]);
 
   return (
     <main className="section">
-      {inputValue !== '' && selectedUser && name === inputValue ? (
+      {selectedUser && (selectedUser.name === inputValue) ? (
         <h1 className="title">
-          {`${name} (${born} = ${died})`}
+          {`${selectedUser.name} (${selectedUser.born} = ${selectedUser.died})`}
         </h1>
       ) : (<h1 className="title">No selected person</h1>)}
 
@@ -89,7 +77,6 @@ export const App: React.FC = () => {
             onChange={handleInputChange}
             onFocus={handleInputFocus}
             onBlur={handleInputBlur}
-            // ref={titleField}
           />
         </div>
         <button
@@ -99,86 +86,51 @@ export const App: React.FC = () => {
         >
           Reset
         </button>
-        {foundName.length === 0 && touched && (
+        {touched && (
           <div className="dropdown-menu" role="menu">
-            <div className="dropdown-content">
-              <div className="dropdown-item">
-                <div
-                  role="button"
-                  tabIndex={0}
-                  className="has-text-link"
-                >
+            <div
+              className="dropdown-content"
+              style={{ maxHeight: '300px', overflow: 'auto' }}
+            >
+              {visiblePeople.length ? (
+                <>
+                  {visiblePeople.map((person) => (
+                    <div
+                      className={classNames(
+                        'dropdown-item button is-small is-link is-inverted',
+                        {
+                          'button is-light': selectedUser
+                          && person.name === inputValue,
+                        },
+                      )}
+                      key={person.name}
+                    >
+                      <div
+                        role="button"
+                        tabIndex={0}
+                        className="has-text-link"
+                        key={person.name}
+                        onMouseDown={() => {
+                          setSelectedUser(person);
+                          setInputValue(person.name);
+                        }}
+                        onKeyDown={(event) => {
+                          if (event.key === 'Enter') {
+                            setSelectedUser(person);
+                            setInputValue(person.name);
+                          }
+                        }}
+                      >
+                        {person.name}
+                      </div>
+                    </div>
+                  ))}
+                </>
+              ) : (
+                <div className="dropdown-item">
                   No matching suggestions
                 </div>
-              </div>
-            </div>
-          </div>
-        )}
-        {inputValue && foundName.length > 0 && foundName && (
-          <div className="dropdown-menu" role="menu">
-            <div className="dropdown-content">
-              {foundName.map((person) => (
-                <div className="dropdown-item" key={person.name}>
-                  <div
-                    role="button"
-                    tabIndex={0}
-                    className={`${
-                      selectedUser === person.name
-                        ? 'has-background-info has-text-white'
-                        : 'has-text-link'
-                    }`}
-                    key={person.name}
-                    onMouseDown={() => {
-                      setSelectedUser(person.name);
-                      setInputValue(person.name);
-                    }}
-                    onKeyDown={(event) => {
-                      if (event.key === 'Enter') {
-                        setSelectedUser(person.name);
-                        setInputValue(person.name);
-                      }
-                    }}
-                  >
-                    {person.name}
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-        )}
-
-        {inputValue === '' && touched && (
-          <div
-            className="dropdown-menu"
-            role="menu"
-          >
-            <div className="dropdown-content">
-              {peopleFromServer.map((person) => (
-                <div className="dropdown-item" key={person.name}>
-                  <div
-                    role="button"
-                    tabIndex={0}
-                    className={`${
-                      selectedUser === person.name
-                        ? 'has-background-info has-text-white'
-                        : 'has-text-link'
-                    }`}
-                    key={person.name}
-                    onMouseDown={() => {
-                      setSelectedUser(person.name);
-                      setInputValue(person.name);
-                    }}
-                    onKeyDown={(event) => {
-                      if (event.key === 'Enter') {
-                        setSelectedUser(person.name);
-                        setInputValue(person.name);
-                      }
-                    }}
-                  >
-                    {person.name}
-                  </div>
-                </div>
-              ))}
+              )}
             </div>
           </div>
         )}
