13c7d3efbdff40d4f34dd280459eb265d0b2c524
Sergej <sergej161995@gmail.com>
2023-10-06 13:21:13 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,79 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo, useCallback } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
 import { Person } from './types/Person';
-import { DropdownList } from './components/DropdownList';
+import { PeopleList } from './components/DropdownList';
+
+function debounce(callback: (...args: string[]) => void, delay: number) {
+  let timeId = 0;
+
+  return (...args: string[]) => {
+    window.clearTimeout(timeId);
+
+    timeId = window.setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+}
+
+function filterPeople(people: Person[], appliedQuery: string) {
+  return people.filter((person) => person.name
+    .toLowerCase()
+    .includes(appliedQuery.toLowerCase()));
+}
 
 export const App: React.FC = () => {
   const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
+
+  const applyQuery = useCallback(debounce(setAppliedQuery, 1000), []);
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+    applyQuery(event.target.value);
+  };
+
+  const filteredPeople = useMemo(() => {
+    return filterPeople(peopleFromServer, appliedQuery);
+  }, [appliedQuery]);
+
+  const onSelected = useCallback((newSelectPerson: Person) => {
+    setSelectedPerson(newSelectPerson);
+    setQuery(newSelectPerson.name);
+  }, []);
 
   return (
     <main className="section">
       <h1 className="title">
-        {selectedPerson
-          ? `${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`
-          : 'No selected person'}
+        {selectedPerson ? (
+          `${selectedPerson.name} (${selectedPerson.born} = ${selectedPerson.died})`
+        ) : (
+          'No selected person'
+        )}
       </h1>
 
-      <DropdownList
-        onSelect={setSelectedPerson}
-        people={peopleFromServer}
-        delay={1000}
-      />
+      <div className="dropdown is-active">
+        <div className="dropdown-trigger">
+          <input
+            type="text"
+            placeholder="Enter a part of the name"
+            className="input"
+            value={query}
+            onChange={handleQueryChange}
+          />
+        </div>
+
+        <div className="dropdown-menu" role="menu">
+          {filteredPeople.length === 0 && (
+            <p className="has-text-danger">No matching suggestions</p>
+          )}
+
+          {appliedQuery !== '' && query !== selectedPerson?.name && (
+            <PeopleList people={filteredPeople} onSelected={onSelected} />
+          )}
+        </div>
+      </div>
     </main>
   );
 };
--- src/components/DropdownList.tsx
+++ src/components/DropdownList.tsx
@@ -1,79 +1,33 @@
-import React, { useState, useMemo, useCallback } from 'react';
-import debounce from 'lodash.debounce';
-import cn from 'classnames';
+import React from 'react';
+import classNames from 'classnames';
 import { Person } from '../types/Person';
 
 type Props = {
-  onSelect: (person: Person | null) => void;
   people: Person[];
-  delay?: number
+  onSelected: (newSelectPerson: Person) => void;
 };
 
-export const DropdownList: React.FC<Props> = ({ onSelect, people, delay }) => {
-  const [query, setQuery] = useState('');
-  const [appliedQuery, setAppliedQuery] = useState('');
-  const [isShown, setIsShown] = useState(false);
-
-  const applyQuery = useCallback(
-    debounce(setAppliedQuery, delay),
-    [delay],
-  );
-
-  const filteredPeople = useMemo(() => {
-    const normalizedQuery = appliedQuery.toLowerCase().trim();
-
-    return people.filter((person) => person.name
-      .toLowerCase()
-      .includes(normalizedQuery));
-  }, [people, appliedQuery]);
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setQuery(event.target.value);
-    applyQuery(event.target.value);
-    onSelect(null);
-  };
-
-  const handlePersonSelect = (person: Person) => {
-    setQuery(person.name);
-    setIsShown(false);
-    onSelect(person);
-  };
-
-  return (
-    <div className={cn('dropdown', {
-      'is-active': isShown,
-    })}
-    >
-      <div className="dropdown-trigger">
-        <input
-          type="text"
-          placeholder="Enter a part of the name"
-          className="input"
-          value={query}
-          onChange={handleInputChange}
-          onFocus={() => setIsShown(true)}
-          onBlur={() => setIsShown(false)}
-        />
+export const PeopleList: React.FC<Props> = React.memo(({
+  people,
+  onSelected,
+}) => (
+  <div className="dropdown-content">
+    {people.map(person => (
+      <div className="dropdown-item" key={person.name}>
+        <a
+          href="#/"
+          className={classNames({
+            'has-text-link': person.sex === 'm',
+            'has-text-danger': person.sex === 'f',
+          })}
+          onClick={(event) => {
+            event.preventDefault();
+            onSelected(person);
+          }}
+        >
+          {person.name}
+        </a>
       </div>
-
-      <div className="dropdown-menu" role="menu">
-        <div className="dropdown-content">
-          {filteredPeople.length > 0 ? (
-            filteredPeople.map((person) => (
-              <a
-                href="/"
-                className="dropdown-item"
-                key={person.slug}
-                onMouseDown={() => handlePersonSelect(person)}
-              >
-                {person.name}
-              </a>
-            ))
-          ) : (
-            <p>No matching suggestions</p>
-          )}
-        </div>
-      </div>
-    </div>
-  );
-};
+    ))}
+  </div>
+));
