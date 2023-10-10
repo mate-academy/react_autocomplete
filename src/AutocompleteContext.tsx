c9a5b04b91024ca8c7bb7e972818b2c4ffa66d75
domkab <dominykas.bobinas@gmail.com>
2023-10-10 13:17:31 +0300

    1.0.0 AutocompleteContext

---
+++ src/AutocompleteContext.tsx
@@ -0,0 +1,92 @@
+/* eslint-disable no-console */
+import { createContext, useReducer } from 'react';
+import { peopleFromServer } from './data/people';
+import { Person } from './types/Person';
+
+interface AutocompleteState {
+  inputText: string;
+  debouncedText: string;
+  suggestions: Person[];
+  selectedPerson: Person | null;
+}
+
+export enum AutocompleteActions {
+  UpdateText = 'updateText',
+  UpdateDebouncedText = 'updateDebouncedText',
+  UpdateSuggestions = 'updateSuggestions',
+  SelectPerson = 'selectPerson',
+}
+
+type AutocompleteAction =
+  | { type: AutocompleteActions.UpdateText; payload: string }
+  | { type: AutocompleteActions.UpdateDebouncedText; payload: string }
+  | { type: AutocompleteActions.UpdateSuggestions; payload: Person[] }
+  | { type: AutocompleteActions.SelectPerson; payload: Person };
+
+const autocompleteReducer = (
+  state: AutocompleteState,
+  action: AutocompleteAction,
+): AutocompleteState => {
+  console.log(state);
+  console.log(action);
+
+  switch (action.type) {
+    case AutocompleteActions.UpdateText:
+      return {
+        ...state,
+        inputText: action.payload,
+      };
+
+    case AutocompleteActions.UpdateDebouncedText:
+      return {
+        ...state,
+        debouncedText: action.payload,
+      };
+
+    case AutocompleteActions.UpdateSuggestions:
+      return {
+        ...state,
+        suggestions: action.payload,
+      };
+
+    case AutocompleteActions.SelectPerson:
+      return {
+        ...state,
+        selectedPerson: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+const initialAutocompleteState: AutocompleteState = {
+  inputText: '',
+  debouncedText: '',
+  suggestions: peopleFromServer,
+  selectedPerson: null,
+};
+
+interface StateProviderProps {
+  children: React.ReactNode;
+}
+
+export const AutocompleteDispatchContext
+= createContext<(action: AutocompleteAction) => void>(() => {});
+
+export const AutocompleteStateContext = createContext(initialAutocompleteState);
+
+export const AutocompleteProvider: React.FC<
+StateProviderProps> = ({ children }) => {
+  const [state, dispatch] = useReducer(
+    autocompleteReducer, initialAutocompleteState,
+  );
+
+  return (
+    <AutocompleteDispatchContext.Provider value={dispatch}>
+      <AutocompleteStateContext.Provider value={state}>
+        {children}
+      </AutocompleteStateContext.Provider>
+    </AutocompleteDispatchContext.Provider>
+  );
+};
