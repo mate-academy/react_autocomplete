3934856a9d379e33989007344de9d812bdaacdac
Oleg Tarasov <tarasov.oleg.9779@gmail.com>
2023-10-12 16:36:55 +0300

    react autocomplete v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import './App.scss';
+import debounce from 'lodash.debounce';
+import classNames from 'classnames';
 import { peopleFromServer } from './data/people';
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [inputText, setInputText] = useState('');
+  const [query, setQuery] = useState('');
+  const [chooseHuman, setChooseHuman] = useState('No Selected person');
+
+  const filteredPeople = useMemo(() => {
+    const people = [...peopleFromServer];
+
+    return people.filter(
+      human => human.name.toLowerCase().includes(query.toLowerCase()),
+    );
+  }, [query]);
+
+  const applyQuery = useCallback(
+    debounce(setQuery, 1000),
+    [],
+  );
+
+  const handleChangeInput = (event: { target: {
+    value: React.SetStateAction<string>;
+  }; }) => {
+    setInputText(event.target.value);
+    applyQuery(event.target.value);
+    setChooseHuman('No Selected person');
+  };
 
   return (
     <main className="section">
       <h1 className="title">
-        {`${name} (${born} = ${died})`}
+        {chooseHuman}
       </h1>
 
       <div className="dropdown is-active">
@@ -17,38 +42,32 @@ export const App: React.FC = () => {
             type="text"
             placeholder="Enter a part of the name"
             className="input"
+            value={inputText}
+            onChange={handleChangeInput}
           />
         </div>
 
         <div className="dropdown-menu" role="menu">
           <div className="dropdown-content">
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Haverbeke</p>
-            </div>
+            <ul>
+              {filteredPeople.map(human => (
+                <li
+                  key={human.slug}
 
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Bernard Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Antone Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Petronella de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Hercke</p>
-            </div>
+                >
+                  <button
+                    type="button"
+                    className={classNames('dropdown-item', { 'has-background-info': chooseHuman === `${human.name} (${human.born} - ${human.died})` })}
+                    onClick={() => setChooseHuman(`${human.name} (${human.born} - ${human.died})`)}
+                  >
+                    <p className="has-text-link">{human.name}</p>
+                  </button>
+                </li>
+              ))}
+            </ul>
+            {filteredPeople.length === 0 && (
+              <div>No matching suggestions</div>
+            )}
           </div>
         </div>
       </div>
