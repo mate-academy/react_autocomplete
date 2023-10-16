6e690ccad98e4b8c37c73c093e3060799c0dcc95
Kostia Hudzenko <kostya.gudzenko1991@gmail.com>
2023-10-16 10:03:09 +0300

    Solution

---
+++ src/components/Autocomplete.tsx
@@ -0,0 +1,104 @@
+import classNames from 'classnames';
+import {
+  useCallback,
+  useMemo,
+  useState,
+} from 'react';
+import { Person } from '../types/Person';
+
+type Props = {
+  people: Person[];
+  onSelected: (person: Person) => void;
+  delay: number;
+};
+
+function debounce(
+  callback: (value: string) => void,
+  delay: number,
+) {
+  let timerId = 0;
+
+  return (args: string) => {
+    window.clearTimeout(timerId);
+
+    timerId = window.setTimeout(() => {
+      callback(args);
+    }, delay);
+  };
+}
+
+export const Autocomplete: React.FC<Props> = ({
+  people,
+  onSelected,
+  delay,
+}) => {
+  const [hasInputFocus, setHasInputFocus] = useState(false);
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
+
+  const applyQuery = useCallback(
+    debounce(setAppliedQuery, delay),
+    [],
+  );
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+    applyQuery(event.target.value);
+  };
+
+  const filteredPeople = useMemo(() => {
+    return people.filter(person => person.name.includes(appliedQuery));
+  }, [people, appliedQuery]);
+
+  const handleSelection = (person: Person) => {
+    onSelected(person);
+    setQuery(person.name);
+    setAppliedQuery(person.name);
+  };
+
+  return (
+    <div className={classNames('dropdown', {
+      'is-active': hasInputFocus,
+    })}
+    >
+      <div className="dropdown-trigger">
+        <input
+          type="text"
+          placeholder="Enter a part of the name"
+          className="input"
+          value={query}
+          onChange={handleQueryChange}
+          onFocus={() => setHasInputFocus(true)}
+          onBlur={() => setHasInputFocus(false)}
+        />
+      </div>
+
+      <div
+        className="dropdown-menu"
+        role="menu"
+      >
+
+        {filteredPeople.length > 0 ? (
+          <div className="dropdown-content">
+            {filteredPeople.map(item => (
+              <div
+                className="dropdown-item"
+                key={item.name}
+                role="button"
+                tabIndex={0}
+                onMouseDown={() => handleSelection(item)}
+              >
+                <p className="has-text-link">{item.name}</p>
+              </div>
+            ))}
+
+          </div>
+        ) : (
+          <p className="notification is-info">
+            No matching suggestions
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
