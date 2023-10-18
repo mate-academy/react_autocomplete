c53a9d0c358468c74515e4c89b683dd26f375595
Astra09 <sichkarstepan09@gmail.com>
2023-10-18 18:04:34 +0300

    dropdown developed

---
+++ src/Components/Dropdown.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import classNames from 'classnames';
+import { useState } from 'react';
+import debounce from 'lodash.debounce';
+import { useCallback } from 'react';
+import './Dropdown.scss';
+import { Person } from '../types/Person';
+
+type Props = {
+  delay: number,
+  personsDisplayed: Person[],
+  onSelected: (person: Person | null) => void,
+  setQuery: (query: string) => void
+};
+
+export const Dropdown: React.FC<Props> = ({ delay, personsDisplayed, onSelected, setQuery }) => {
+  const [dropdownActive, setDropdownActive] = useState(false);
+  const [filterQuery, setFilterQuery] = useState('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFilterQuery(event.target.value);
+    applyQuery(event.target.value);
+  }
+
+  const handleFocus = () => {
+    if (filterQuery.length === 0) {
+      setDropdownActive(true);
+    }
+  }
+
+  const handleSelect = (person: Person) => {
+    onSelected(person);
+    setFilterQuery(person.name);
+    setDropdownActive(false);
+    setQuery('');
+  }
+
+  const applyQuery = useCallback(
+    debounce((query: string) => {
+      setQuery(query);
+      console.log(query);
+    }, delay),
+    []
+  )
+
+  return (
+    <div className={classNames('dropdown', { 'is-active': dropdownActive })}>
+      <div className="dropdown-trigger">
+        <input
+          type="text"
+          placeholder="Enter a part of the name"
+          className="input"
+          onChange={(event) => { handleInputChange(event); setDropdownActive(true) }}
+          value={filterQuery}
+          onFocus={handleFocus}
+        />
+      </div>
+
+      <div className="dropdown-menu" role="menu">
+        <div className="dropdown-content">
+          {personsDisplayed.length > 0 && personsDisplayed.map(person => (
+            <div
+              className="dropdown-item"
+              key={person.name}
+              onClick={() => handleSelect(person)}
+            >
+              <p className="has-text-link">{person.name}</p>
+            </div>
+          ))}
+          {personsDisplayed.length === 0 && (
+            <div className="dropdown-item">
+              <p>No matching suggestions</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
