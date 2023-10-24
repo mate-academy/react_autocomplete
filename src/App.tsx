7a59f8dec280c5b48e26a92925ec31105088b4c1
Oleh <olezhkapopovych@gmail.com>
2023-10-24 11:14:46 +0300

    solution with incorrect behavior

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,93 @@
-import React from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
+import debounce from 'lodash.debounce';
 import './App.scss';
+import cn from 'classnames';
 import { peopleFromServer } from './data/people';
+import { Person } from './types/Person';
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState<string>('');
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+  const [isFocusedInput, setIsFocusedInput] = useState(false);
+
+  const aplyQuery = useCallback(
+    debounce(setAppliedQuery, 1000),
+    [],
+  );
+
+  const handleQueryChange = ((event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+
+    aplyQuery(event.target.value);
+
+    if (event.target.value === appliedQuery) {
+      aplyQuery.cancel();
+    }
+
+    setQuery(event.target.value);
+  });
+
+  const filteredPeople = useMemo(() => {
+    return peopleFromServer
+      .filter(person => person.name.includes(appliedQuery))
+      .map(person => {
+        return (
+          <>
+            <a
+              key={person.slug}
+              href="/#"
+              onClick={() => {
+                setSelectedPerson(person);
+                setQuery(person.name);
+                setIsFocusedInput(false);
+              }}
+              className="dropdown-item"
+            >
+              {person.name}
+            </a>
+
+            <hr className="dropdown-divider" />
+          </>
+        );
+      });
+  }, [appliedQuery]);
 
   return (
     <main className="section">
       <h1 className="title">
-        {`${name} (${born} = ${died})`}
+        {selectedPerson
+          ? `${selectedPerson.name} (${selectedPerson.born} = ${selectedPerson.died})`
+          : 'No selected person'}
       </h1>
 
-      <div className="dropdown is-active">
+      <div className={cn(
+        'dropdown',
+        { 'is-active': isFocusedInput },
+      )}
+      >
         <div className="dropdown-trigger">
           <input
+            value={query}
             type="text"
             placeholder="Enter a part of the name"
             className="input"
+            onChange={(event => handleQueryChange(event))}
+            onFocus={() => setIsFocusedInput(true)}
           />
         </div>
 
-        <div className="dropdown-menu" role="menu">
+        <div className="dropdown-menu" id="dropdown-menu" role="menu">
           <div className="dropdown-content">
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Bernard Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Antone Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Petronella de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Hercke</p>
-            </div>
+            {filteredPeople.length
+              ? filteredPeople
+              : (
+                <div
+                  className="dropdown-item"
+                >
+                  No matching suggestions
+                </div>
+              )}
           </div>
         </div>
       </div>
