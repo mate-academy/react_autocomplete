7d38d564f31ddedeb389e8f57452506f6fd9bab8
Yaroslav <slavik2607.lviv@gmail.com>
2023-10-24 19:41:06 +0300

    Solution

---
+++ src/helpers/helpers.ts
@@ -0,0 +1,11 @@
+export const debounce = (callback: any, delay: number) => {
+  let timerId = 0;
+
+  return (...args: any) => {
+    window.clearTimeout(timerId);
+
+    timerId = window.setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+};
