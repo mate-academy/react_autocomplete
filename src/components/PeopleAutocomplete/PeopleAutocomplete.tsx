075a3a760b96b03e7cb8b8c510b5b28ac1ac3b38
your_username <web.shepel@gmail.com>
2023-10-24 15:08:24 +0300

    solution

---
+++ src/components/PeopleAutocomplete/PeopleAutocomplete.tsx
@@ -0,0 +1,105 @@
+import React, {
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
+import debounce from 'lodash.debounce';
+import classNames from 'classnames';
+import { Person } from '../../types/Person';
+
+type Props = {
+  people: Person[],
+  delay?: number,
+  onSelect?: (person: Person) => void,
+};
+
+export const PeopleAutocomplete: React.FC<Props> = (
+  {
+    people,
+    delay = 1000,
+    onSelect = () => {},
+  },
+) => {
+  const refPeopleAutocomplete = useRef<HTMLDivElement>(null);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const applyQuery = useMemo(
+    () => debounce(setAppliedQuery, delay),
+    [delay],
+  );
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsLoading(true);
+    setQuery(event.target.value);
+    applyQuery(event.target.value);
+  };
+
+  const handleSelectPerson = (person: Person) => {
+    onSelect(person);
+    setQuery(person.name);
+    setShowSuggestions(false);
+  };
+
+  const filteredPeople = useMemo(() => {
+    setIsLoading(false);
+
+    return people.filter(person => person.name.includes(appliedQuery));
+  }, [people, appliedQuery]);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      setShowSuggestions(!(refPeopleAutocomplete.current
+        && !refPeopleAutocomplete.current.contains(event.target as Node)));
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div
+      ref={refPeopleAutocomplete}
+      className={classNames('dropdown', { 'is-active': showSuggestions })}
+    >
+      <div className="dropdown-trigger">
+        <div className={classNames('control', { 'is-loading': isLoading })}>
+          <input
+            type="text"
+            placeholder="Enter a part of the name"
+            className="input"
+            value={query}
+            onChange={handleQueryChange}
+          />
+        </div>
+      </div>
+
+      <div className="dropdown-menu" role="menu">
+        <div className="dropdown-content">
+          {filteredPeople.length
+            ? filteredPeople.map(person => (
+              <a
+                href="/#"
+                className="dropdown-item"
+                key={person.slug}
+                onClick={() => handleSelectPerson(person)}
+              >
+                <p className="has-text-link">{person.name}</p>
+              </a>
+            ))
+            : (
+              <div className="dropdown-item">
+                <p>No matching suggestions</p>
+              </div>
+            )}
+        </div>
+      </div>
+    </div>
+  );
+};
