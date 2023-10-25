977891573e8b53a9610a6c47d6cecc3a1542e4f8
Leonid <aleksandrovi4.l.p@gmail.com>
2023-10-26 00:39:34 +0300

    changes due to lint issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable */
 import ReactDOM from 'react-dom';
 import 'bulma/css/bulma.css';
 import { App } from './App';
+import React from 'react';
 
 ReactDOM.render(<App />, document.getElementById('root'));
