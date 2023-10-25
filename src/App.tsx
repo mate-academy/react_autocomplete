3e0788ae3aa12608ab24bace39316591575a71a8
Nazar Hnatyuk <108331415+Stim1@users.noreply.github.com>
2023-10-25 16:24:49 +0300

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,88 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
+import { Person } from './types/Person';
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [inputValue, setInputValue] = useState('');
+  const [isFocused, setIsFocused] = useState(false);
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+
+  const handleInputBlur = () => {
+    setTimeout(() => {
+      setIsFocused(false);
+    }, 200);
+  };
+
+  const handleInputFocus = () => {
+    clearTimeout(0);
+    setIsFocused(true);
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSuggestionClick = (person: Person) => {
+    setInputValue(person.name);
+    setSelectedPerson(person);
+    setIsFocused(false);
+  };
+
+  const filteredPeople = useMemo(() => {
+    return peopleFromServer.filter((person) => {
+      return person.name.toLowerCase().includes(inputValue.toLowerCase());
+    });
+  }, [inputValue]);
 
   return (
     <main className="section">
       <h1 className="title">
-        {`${name} (${born} = ${died})`}
+        {selectedPerson
+          ? `${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`
+          : 'No selected person'}
       </h1>
 
-      <div className="dropdown is-active">
+      <div className={`dropdown ${isFocused ? 'is-active' : ''}`}>
         <div className="dropdown-trigger">
           <input
             type="text"
             placeholder="Enter a part of the name"
             className="input"
+            value={inputValue}
+            onChange={handleInputChange}
+            onFocus={handleInputFocus}
+            onBlur={handleInputBlur}
           />
         </div>
 
         <div className="dropdown-menu" role="menu">
-          <div className="dropdown-content">
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Bernard Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Antone Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Petronella de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Hercke</p>
-            </div>
-          </div>
+          {(isFocused || inputValue === '') && (
+            filteredPeople.length > 0 ? (
+              filteredPeople.map(person => (
+                <button
+                  type="button"
+                  className="dropdown-content"
+                  key={person.name}
+                  onClick={() => handleSuggestionClick(person)}
+                >
+                  <div className="dropdown-item">
+                    <p className="has-text-link">
+                      {person.name}
+                    </p>
+                  </div>
+                </button>
+              ))
+            ) : (
+              <div className="dropdown-content">
+                <div className="dropdown-item">
+                  <p className="has-text-danger">
+                    No matching suggestions
+                  </p>
+                </div>
+              </div>
+            )
+          )}
         </div>
       </div>
     </main>
