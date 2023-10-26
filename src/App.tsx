b228013df11b422ca80516af18d5f1fb5c65dbbf
Nazar Hnatyuk <108331415+Stim1@users.noreply.github.com>
2023-10-26 19:30:44 +0300

    made all changes according to README

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
 import { Person } from './types/Person';
@@ -10,9 +10,10 @@ interface AppProps {
 }
 
 export const App: React.FC<AppProps> = ({ debounceDelay = 300 }) => {
-  const [inputValue, setInputValue] = useState('');
-  const [isFocused, setIsFocused] = useState(false);
+  const [inputValue, setInputValue] = useState<string>('');
+  const [isFocused, setIsFocused] = useState<boolean>(false);
   const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+  const [showSuggestions, setShowSuggestions] = useState<boolean>(true);
 
   const debouncedInputValue = useDebounce(inputValue, debounceDelay);
 
@@ -28,20 +29,36 @@ export const App: React.FC<AppProps> = ({ debounceDelay = 300 }) => {
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
+    setSelectedPerson(null);
+
+    if (!event.target.value) {
+      setShowSuggestions(true);
+    }
+
+    setShowSuggestions(false);
   };
+
+  useEffect(() => {
+    setShowSuggestions(true);
+  }, [debouncedInputValue]);
 
   const handleSuggestionClick = (person: Person) => {
     setInputValue(person.name);
     setSelectedPerson(person);
     setIsFocused(false);
+    setShowSuggestions(false);
   };
 
   const filteredPeople = useMemo(() => {
+    if (!inputValue) {
+      return peopleFromServer;
+    }
+
     return peopleFromServer.filter((person) => {
-      return person.name.toLowerCase().includes(debouncedInputValue
-        .toLowerCase());
+      return person.name.toLowerCase()
+        .includes(debouncedInputValue.toLowerCase());
     });
-  }, [debouncedInputValue]);
+  }, [debouncedInputValue, inputValue]);
 
   return (
     <main className="section">
@@ -55,6 +72,7 @@ export const App: React.FC<AppProps> = ({ debounceDelay = 300 }) => {
         isFocused={isFocused}
         inputValue={inputValue}
         filteredPeople={filteredPeople}
+        showSuggestions={showSuggestions}
         handleInputChange={handleInputChange}
         handleInputFocus={handleInputFocus}
         handleInputBlur={handleInputBlur}
--- src/Dropdown.tsx
+++ src/Dropdown.tsx
@@ -4,6 +4,7 @@ import { Person } from './types/Person';
 interface DropdownProps {
   isFocused: boolean;
   inputValue: string;
+  showSuggestions: boolean;
   filteredPeople: Person[];
   handleInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   handleInputFocus: () => void;
@@ -15,13 +16,17 @@ export const Dropdown: React.FC<DropdownProps> = ({
   isFocused,
   inputValue,
   filteredPeople,
+  showSuggestions,
   handleInputChange,
   handleInputFocus,
   handleInputBlur,
   handleSuggestionClick,
 }) => {
   return (
-    <div className={`dropdown ${isFocused ? 'is-active' : ''}`}>
+    <div className={`dropdown ${isFocused
+      ? 'is-active'
+      : ''}`}
+    >
       <div className="dropdown-trigger">
         <input
           type="text"
@@ -35,38 +40,39 @@ export const Dropdown: React.FC<DropdownProps> = ({
       </div>
 
       <div className="dropdown-menu" role="menu">
-        {(isFocused || inputValue === '') && (
-          filteredPeople.length > 0 ? (
-            filteredPeople.map(person => {
-              const { name, sex } = person;
+        {showSuggestions && (
+          filteredPeople.length > 0
+            ? (
+              filteredPeople.map(person => {
+                const { name, sex } = person;
 
-              return (
-                <button
-                  type="button"
-                  className="dropdown-content"
-                  key={name}
-                  onClick={() => handleSuggestionClick(person)}
-                >
-                  <div className="dropdown-item">
-                    <p className={sex === 'f'
-                      ? 'has-text-danger'
-                      : 'has-text-link'}
-                    >
-                      {name}
-                    </p>
-                  </div>
-                </button>
-              );
-            })
-          ) : (
-            <div className="dropdown-content">
-              <div className="dropdown-item">
-                <p className="has-text-danger">
-                  No matching suggestions
-                </p>
+                return (
+                  <button
+                    type="button"
+                    className="dropdown-content"
+                    key={name}
+                    onClick={() => handleSuggestionClick(person)}
+                  >
+                    <div className="dropdown-item">
+                      <p className={sex === 'f'
+                        ? 'has-text-danger'
+                        : 'has-text-link'}
+                      >
+                        {name}
+                      </p>
+                    </div>
+                  </button>
+                );
+              })
+            ) : (
+              <div className="dropdown-content">
+                <div className="dropdown-item">
+                  <p className="has-text-danger">
+                    No matching suggestions
+                  </p>
+                </div>
               </div>
-            </div>
-          )
+            )
         )}
       </div>
     </div>
