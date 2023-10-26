839d2c28eff2dd0d5eb4773b2b66dd78146a5f92
Artem Trofymovych <navrotskymetra@outlook.com>
2023-10-26 19:44:27 +0200

    dropdown menu implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
+import { Person } from './types/Person';
 
-export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+interface AppProps {
+  debounceDelay: number;
+}
+
+export const App: React.FC<AppProps> = ({ debounceDelay }) => {
+  const [text, setInput] = useState('');
+  const [onFocus, setOnFocus] = useState(false);
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+  const [debouncedInput, setDebouncedInput] = useState('');
+
+  useEffect(() => {
+    const debounceTimeout = setTimeout(() => {
+      setDebouncedInput(text);
+    }, debounceDelay);
+
+    return () => clearTimeout(debounceTimeout);
+  }, [text, debounceDelay]);
+
+  const handleSuggestionClick = (person: Person) => {
+    setInput(person.name);
+    setSelectedPerson(person);
+    setOnFocus(false);
+  };
+
+  function filteredPeople(people: Person[], input: string) {
+    return people.filter(person => person.name
+      .toLowerCase()
+      .includes(input.toLowerCase()));
+  }
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(event.target.value);
+
+    // Clear the selected person and set onFocus to true
+    setSelectedPerson(null);
+    setOnFocus(true);
+  };
 
   return (
     <main className="section">
       <h1 className="title">
-        {`${name} (${born} = ${died})`}
+        {selectedPerson ? `${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})` : 'No selected person'}
       </h1>
 
       <div className="dropdown is-active">
@@ -17,40 +53,37 @@ export const App: React.FC = () => {
             type="text"
             placeholder="Enter a part of the name"
             className="input"
+            value={text}
+            onChange={handleInputChange}
           />
         </div>
 
-        <div className="dropdown-menu" role="menu">
-          <div className="dropdown-content">
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Bernard Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Antone Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Petronella de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Hercke</p>
+        {debouncedInput && onFocus && (
+          <div className="dropdown-menu" role="menu">
+            <div className="dropdown-content">
+              {filteredPeople(peopleFromServer, debouncedInput).length === 0 ? (
+                <div className="dropdown-item">No matching suggestions</div>
+              ) : (
+                filteredPeople(peopleFromServer, debouncedInput).map(person => (
+                  <div
+                    className="dropdown-item"
+                    key={person.slug}
+                    onClick={() => handleSuggestionClick(person)}
+                    onKeyDown={(e) => {
+                      if (e.key === 'Enter') {
+                        handleSuggestionClick(person);
+                      }
+                    }}
+                    role="button"
+                    tabIndex={0}
+                  >
+                    <p className="has-text-link">{person.name}</p>
+                  </div>
+                ))
+              )}
             </div>
           </div>
-        </div>
+        )}
       </div>
     </main>
   );
