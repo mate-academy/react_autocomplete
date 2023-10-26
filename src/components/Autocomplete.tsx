ead3f3c654fbe22430475e205372196cd5f45a58
Yuriy Bilskiy <yuriy.bilskiy@gmail.com>
2023-10-26 18:07:37 +0300

    fix

--- src/components/Autocomplete.tsx
+++ src/components/Autocomplete.tsx
@@ -9,11 +9,10 @@ type Props = {
 
 export const Autocomplete: React.FC<Props> = ({ onSelected, people }) => {
   const [query, setQuery] = useState('');
-  const [focusInput, setFocusInput] = useState(false);
   const [appliedQuery, setAppliedQuery] = useState('');
+  const [isFocusInput, setIsFocusInput] = useState(false);
 
   const timerId = useRef(0);
-
   const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(event.target.value);
     window.clearTimeout(timerId.current);
@@ -30,13 +29,14 @@ export const Autocomplete: React.FC<Props> = ({ onSelected, people }) => {
 
   const filteredPeople = useMemo(() => {
     return people.filter(
-      person => person.name.toLowerCase().includes(appliedQuery.toLowerCase()),
+      person => person.name
+        .toLowerCase().includes(appliedQuery.trim().toLowerCase()),
     );
   }, [appliedQuery, people]);
 
   return (
     <div className={cn('dropdown', {
-      'is-active': focusInput,
+      'is-active': isFocusInput,
     })}
     >
       <div className="dropdown-trigger">
@@ -45,8 +45,8 @@ export const Autocomplete: React.FC<Props> = ({ onSelected, people }) => {
           placeholder="Enter a part of the name"
           className="input"
           value={query}
-          onFocus={() => setFocusInput(true)}
-          onBlur={() => setFocusInput(false)}
+          onFocus={() => setIsFocusInput(true)}
+          onBlur={() => setIsFocusInput(false)}
           onChange={handleQueryChange}
         />
       </div>
@@ -58,23 +58,27 @@ export const Autocomplete: React.FC<Props> = ({ onSelected, people }) => {
               <p className="has-text-danger">No select person</p>
             </div>
           ) : (
-            filteredPeople.map(person => (
-              <div
-                key={person.slug}
-                className="dropdown-item"
-                role="button"
-                tabIndex={0}
-                onMouseDown={() => handleSelected(person)}
-              >
-                <p className={cn(
-                  { 'has-text-link': person.sex === 'm' },
-                  { 'has-text-danger': person.sex === 'f' },
-                )}
+            filteredPeople.map(person => {
+              const { name, sex, slug } = person;
+
+              return (
+                <div
+                  key={slug}
+                  className="dropdown-item"
+                  role="button"
+                  tabIndex={0}
+                  onMouseDown={() => handleSelected(person)}
                 >
-                  {person.name}
-                </p>
-              </div>
-            ))
+                  <p className={cn(
+                    { 'has-text-link': sex === 'm' },
+                    { 'has-text-danger': sex === 'f' },
+                  )}
+                  >
+                    {name}
+                  </p>
+                </div>
+              );
+            })
           )}
         </div>
       </div>
