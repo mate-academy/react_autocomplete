52473248d4be887b7c88183246f1a81c41c4c981
«Maria» <nedymai@gmail.com>
2023-10-27 12:41:43 +0300

    solution

---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,90 @@
+import cn from 'classnames';
+import debounce from 'lodash.debounce';
+
+import React, { useState, useCallback, useMemo } from 'react';
+import { Person } from '../types/Person';
+
+interface Props {
+  people: Person[]
+  delay?: number
+  onSelected: (value: Person | null) => void
+}
+
+export const Dropdown: React.FC<Props> = ({
+  people,
+  delay,
+  onSelected,
+}) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
+
+  const applyQuery = useCallback(debounce((value: string) => {
+    setAppliedQuery(value);
+    setIsDropdownOpen(true);
+  }, delay), [delay]);
+
+  const filteredPeople = useMemo(() => {
+    return people.filter(
+      person => person.name
+        .toLowerCase()
+        .includes(appliedQuery.toLowerCase().trim()),
+    );
+  }, [appliedQuery, people]);
+
+  const handleSelect = (person: Person) => {
+    setQuery(person.name);
+    onSelected(person);
+    setIsDropdownOpen(false);
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsDropdownOpen(false);
+    setQuery(event.target.value);
+    applyQuery(event.target.value);
+    onSelected(null);
+  };
+
+  return (
+    <div className={cn('dropdown', {
+      'is-active': isDropdownOpen,
+    })}
+    >
+      <div className="dropdown-trigger">
+        <input
+          type="text"
+          placeholder="Enter a part of the name"
+          className="input"
+          value={query}
+          onFocus={() => setIsDropdownOpen(true)}
+          onBlur={() => setIsDropdownOpen(false)}
+          onChange={handleInputChange}
+        />
+      </div>
+
+      <div className="dropdown-menu" role="menu">
+        <div className="dropdown-content">
+          {filteredPeople.length >= 1 ? (
+            filteredPeople.map(person => (
+              <a
+                className="dropdown-item"
+                href="/"
+                key={person.slug}
+                onMouseDown={() => handleSelect(person)}
+              >
+                <p className={person.sex === 'm'
+                  ? 'has-text-link'
+                  : 'has-text-danger'}
+                >
+                  {person.name}
+                </p>
+              </a>
+            ))
+          ) : (
+            <p>No matching suggestions</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
