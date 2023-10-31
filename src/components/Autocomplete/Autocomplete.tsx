c04f944d8399d16ce33a7ce92ee5bc8acf465c25
Valeria Oleynik <oleynik.valeria27@gmail.com>
2023-10-31 16:32:31 +0200

    implemented Autocomplete component

---
+++ src/components/Autocomplete/Autocomplete.tsx
@@ -0,0 +1,82 @@
+import React, { useCallback, useMemo, useState } from 'react';
+import classNames from 'classnames';
+import debounce from 'lodash.debounce';
+
+import { Person } from '../../types/Person';
+
+type Props = {
+  people: Person[];
+  onSelected: (person: Person) => void;
+};
+
+export const Autocomplete: React.FC<Props> = ({
+  people,
+  onSelected,
+}) => {
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const applyQuery = useCallback(
+    debounce(setAppliedQuery, 1000),
+    [],
+  );
+
+  const handleFindPerson = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+    applyQuery(event.target.value);
+  };
+
+  const filteredPeople = useMemo(() => {
+    return people.filter(({ name }) => {
+      return name.toLowerCase().includes(appliedQuery.toLowerCase().trim());
+    });
+  }, [appliedQuery, people]);
+
+  return (
+    <div className="dropdown is-active">
+      <div className="dropdown-trigger">
+        <input
+          type="text"
+          placeholder="Enter a part of the name"
+          className="input"
+          value={query}
+          onChange={handleFindPerson}
+          onFocus={() => setShowDropdown(true)}
+          onBlur={() => setShowDropdown(false)}
+        />
+      </div>
+
+      {showDropdown && (
+        <div className="dropdown-menu" role="menu">
+          <div className="dropdown-content">
+            {filteredPeople.length
+              ? filteredPeople.map(person => (
+                <div className="dropdown-item" key={person.slug}>
+                  <a
+                    className={classNames({
+                      'has-text-link': person.sex === 'm',
+                      'has-text-danger': person.sex === 'f',
+                    })}
+                    href="/"
+                    onMouseDown={() => {
+                      onSelected(person);
+                      setAppliedQuery(person.name);
+                      setQuery(person.name);
+                    }}
+                  >
+                    {person.name}
+                  </a>
+                </div>
+              ))
+              : (
+                <div className="dropdown-item">
+                  <p className="has-text-link">No matching suggestion</p>
+                </div>
+              )}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
