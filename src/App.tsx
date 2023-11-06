9534585ffb3c2050f1d40ea2fda37b51b8d04317
FurArt <iiionerart@gmail.com>
2023-11-06 16:04:00 +0200

    added solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,111 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import './App.scss';
+import { debounce } from 'lodash';
 import { peopleFromServer } from './data/people';
+import { Person } from './types/Person';
+
+// #start region Autocomplete
+const Autocomplete: React.FC<{
+  delay: number;
+  people: Person[];
+  onSelected: (person: Person | null) => void;
+}> = ({ delay, people, onSelected }) => {
+  const [text, setText] = useState<string>('');
+  const [suggestions, setSuggestions] = useState<Person[]>(people);
+
+  const filterPeopleWithDelay = useMemo(() => {
+    return debounce((input: string) => {
+      const filteredPeople = people.filter((person) => (
+        person.name.toLowerCase().includes(input.toLowerCase())
+      ));
+
+      setSuggestions(filteredPeople);
+    }, delay);
+  }, [people, delay]);
+
+  useEffect(() => {
+    filterPeopleWithDelay(text);
+  }, [text, filterPeopleWithDelay]);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const input = e.target.value;
+
+    setText(input);
+
+    // Відображаємо попередній список з затримкою
+    filterPeopleWithDelay(input);
+  };
+
+  const handleInputFocus = () => {
+    if (text === '') {
+      setSuggestions(people);
+    }
+  };
+
+  const handleSuggestionClick = (person: Person) => {
+    setText(person.name);
+    setSuggestions([]);
+    onSelected(person);
+  };
+
+  return (
+    <div className="dropdown is-active">
+      <div className="dropdown-trigger">
+        <input
+          type="text"
+          placeholder="Enter a part of the name"
+          className="input"
+          onFocus={handleInputFocus}
+          onChange={handleInputChange}
+          value={text}
+        />
+      </div>
+
+      <div className="dropdown-menu" role="menu">
+        <div className="dropdown-content">
+          {suggestions.length === 0 ? (
+            <div className="dropdown-item">No matching suggestions</div>
+          ) : (
+            suggestions.map((person) => (
+              <button
+                type="button"
+                className="dropdown-item"
+                key={person.slug}
+                onClick={() => handleSuggestionClick(person)}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter' || e.key === ' ') {
+                    handleSuggestionClick(person);
+                  }
+                }}
+              >
+                <p>
+                  {person.name}
+
+                </p>
+              </button>
+            )))}
+        </div>
+      </div>
+    </div>
+  );
+};
+// #end region Autocomplete
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
 
   return (
     <main className="section">
       <h1 className="title">
-        {`${name} (${born} = ${died})`}
+        {selectedPerson
+          ? `${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`
+          : 'No selected person'}
       </h1>
-
-      <div className="dropdown is-active">
-        <div className="dropdown-trigger">
-          <input
-            type="text"
-            placeholder="Enter a part of the name"
-            className="input"
-          />
-        </div>
-
-        <div className="dropdown-menu" role="menu">
-          <div className="dropdown-content">
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Bernard Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Antone Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Petronella de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Hercke</p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Autocomplete
+        delay={1000}
+        people={peopleFromServer}
+        onSelected={(person) => setSelectedPerson(person)}
+      />
     </main>
   );
 };
