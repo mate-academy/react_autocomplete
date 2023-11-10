8cf8cbf2cb090e96e76afc0094bec3c9862c3c31
Wita-Shchurko <vitaliya.mememe@gmail.com>
2023-11-10 19:21:12 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,35 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
+import { Autocomplete } from './components/Autocomplete';
+import { Person } from './types/Person';
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
+  const [selectedHuman, setSelectedHuman] = useState<Person | null>(null);
+
+  const filteredList = useMemo(() => {
+    return peopleFromServer.filter(human => {
+      return human.name.toLowerCase().includes(appliedQuery.toLowerCase());
+    });
+  }, [appliedQuery]);
 
   return (
     <main className="section">
       <h1 className="title">
-        {`${name} (${born} = ${died})`}
+        {selectedHuman
+          ? `${selectedHuman.name} (${selectedHuman.born} = ${selectedHuman.died})`
+          : 'No selected person'}
       </h1>
 
-      <div className="dropdown is-active">
-        <div className="dropdown-trigger">
-          <input
-            type="text"
-            placeholder="Enter a part of the name"
-            className="input"
-          />
-        </div>
-
-        <div className="dropdown-menu" role="menu">
-          <div className="dropdown-content">
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Bernard Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Antone Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Petronella de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Hercke</p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Autocomplete
+        filteredList={filteredList}
+        query={query}
+        setQuery={setQuery}
+        setAppliedQuery={setAppliedQuery}
+        onSelected={setSelectedHuman}
+      />
     </main>
   );
 };
