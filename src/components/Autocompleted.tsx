baa8f4847a29a230a3e2d6a7f11677a397f7c9d7
Bohdan Antipenko <dr.antipenko2016@gmail.com>
2023-11-15 11:18:42 +0200

    Add tast solution

---
+++ src/components/Autocompleted.tsx
@@ -0,0 +1,89 @@
+import React, { useCallback, useMemo, useState } from 'react';
+import classNames from 'classnames';
+import debounce from 'lodash.debounce';
+import { Person } from '../types/Person';
+
+interface Props {
+  people: Person[],
+  timer?: number,
+  onSelect?: (person: Person | null) => void,
+}
+
+export const Autocomplete: React.FC<Props> = ({
+  people,
+  timer = 1000,
+  onSelect = () => { },
+}) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
+
+  const applyQuery = useCallback(
+    debounce(setAppliedQuery, timer),
+    [timer],
+  );
+
+  const filteredPeople = useMemo(() => {
+    return people.filter(p => {
+      return p.name.toLowerCase().includes(appliedQuery.toLowerCase());
+    });
+  }, [people, appliedQuery]);
+
+  const handlePersonSelect = (person: Person) => {
+    onSelect(person);
+    setQuery(person.name);
+  };
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value;
+
+    setQuery(inputValue);
+    applyQuery(inputValue);
+  };
+
+  return (
+    <div className={classNames('dropdown', {
+      'is-active': isFocused,
+    })}
+    >
+      <div className="dropdown-trigger">
+        <input
+          type="text"
+          placeholder="Enter a part of the name"
+          className="input"
+          value={query}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          onChange={handleQueryChange}
+        />
+      </div>
+
+      <div className="dropdown-menu" role="menu">
+        <div className="dropdown-content">
+          {filteredPeople.length
+            ? filteredPeople.map(person => (
+              <a
+                href="/#"
+                className="dropdown-item"
+                key={person.name}
+                onMouseDown={() => handlePersonSelect(person)}
+              >
+                <p className={classNames({
+                  'has-text-link': person.sex === 'm',
+                  'has-text-danger': person.sex === 'f',
+                })}
+                >
+                  {person.name}
+                </p>
+              </a>
+            ))
+            : (
+              <div className="dropdown-item">
+                <p>No matching suggestions</p>
+              </div>
+            )}
+        </div>
+      </div>
+    </div>
+  );
+};
