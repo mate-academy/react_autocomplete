fd25feab388dd012e94426ef286164645839d279
Oksana Shvets <ksenyasvatko1993@gmail.com>
2023-11-21 14:55:39 +0100

    Solution - 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,25 @@
-import React, {
-  useCallback, useState,
-} from 'react';
+/* eslint-disable no-console */
+import React, { useCallback, useState } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
 import { Namelist } from './components/NameList';
 import { Person } from './types/Person';
 
 export const App: React.FC = () => {
-  const [selectedPerson, setSelectedPerson] = useState<Person | null>();
+  const [, setSelectedPerson] = useState<Person | null>();
 
-  const handlePersonSelect = useCallback((person: Person) => {
+  const handlePersonSelect = useCallback((person: Person | null) => {
+    console.log('Selected Person:', person);
+
     setSelectedPerson(person);
   }, []);
 
   return (
-    <main className="section">
-      <h1 className="title">
-        {selectedPerson
-          ? `${selectedPerson.name} (${selectedPerson.born} = ${selectedPerson.died})`
-          : 'No selected persone'}
-      </h1>
-      <Namelist
-        names={peopleFromServer}
-        onSelect={handlePersonSelect}
-        delay={500}
-      />
-    </main>
+
+    <Namelist
+      names={peopleFromServer}
+      onSelect={handlePersonSelect}
+      delay={500}
+    />
   );
 };
--- src/components/NameList.tsx
+++ src/components/NameList.tsx
@@ -14,6 +14,7 @@ export const Namelist: React.FC<Props>
   const [query, setQuery] = useState('');
   const [filteredPeople, setFilteredPeople] = useState('');
   const [isFocused, setIsFocused] = useState(false);
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>();
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const handleQueryChange = useCallback(
@@ -21,6 +22,10 @@ export const Namelist: React.FC<Props>
   );
 
   const handleCurrentChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.value) {
+      setSelectedPerson(null);
+    }
+
     setQuery(event.target.value);
     handleQueryChange(event.target.value);
     setIsFocused(true);
@@ -34,6 +39,7 @@ export const Namelist: React.FC<Props>
 
     setQuery(person.name);
     onSelect(person);
+    setSelectedPerson(person);
     setIsFocused(false);
   };
 
@@ -56,44 +62,53 @@ export const Namelist: React.FC<Props>
   }, [filteredPeople, names]);
 
   return (
-    <div className="dropdown is-active">
-      <div className="dropdown-trigger">
-        <input
-          type="text"
-          placeholder="Enter a part of the name"
-          value={query}
-          onChange={handleCurrentChange}
-          onFocus={handleFocus}
-          onBlur={handleBlur}
-          className="input"
-        />
-      </div>
+    <main className="section">
+      <h1 className="title">
+        {selectedPerson
+          ? `${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`
+          : 'No selected person'}
+      </h1>
 
-      <div className="dropdown-menu" role="menu">
-        {isFocused && (
-          <div className="dropdown-content">
-            {currentPeople.length
-              ? currentPeople.map((person) => (
-                <div
-                  key={person.name}
-                  className="dropdown-item"
-                >
-                  <a
-                    href="/"
-                    className={classNames('button is-light', {
-                      'has-text-link': person.sex === 'm',
-                      'has-text-danger': person.sex === 'f',
-                    })}
-                    onClick={(event) => handleButtonClick(event, person)}
+      <div className="dropdown is-active">
+        <div className="dropdown-trigger">
+          <input
+            type="text"
+            placeholder="Enter a part of the name"
+            value={query}
+            onChange={handleCurrentChange}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            className="input"
+          />
+        </div>
+
+        <div className="dropdown-menu" role="menu">
+          {isFocused && (
+            <div className="dropdown-content">
+              {currentPeople.length
+                ? currentPeople.map((person) => (
+                  <div
+                    key={person.name}
+                    className="dropdown-item"
                   >
-                    {person.name}
-                  </a>
-                </div>
-              ))
-              : 'No matching suggestions'}
-          </div>
-        )}
+                    <a
+                      href="/"
+                      className={classNames('button is-light', {
+                        'has-text-link': person.sex === 'm',
+                        'has-text-danger': person.sex === 'f',
+                      })}
+                      onMouseDown={(event) => handleButtonClick(event, person)}
+                    >
+                      {person.name}
+                    </a>
+                  </div>
+                ))
+                : 'No matching suggestions'}
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </main>
+
   );
 }));
