66e7ff108c5754f2388925a04a4731ce34644803
Artem Sydun <sydun2003@gmail.com>
2023-11-28 00:56:18 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,30 @@
-import React, { useState, useMemo } from 'react';
-import cn from 'classnames';
+import React, { useState } from 'react';
+
 import './App.scss';
 import { peopleFromServer } from './data/people';
 import { Person } from './types/Person';
-import { PeopleList } from './components/PeopleList';
+import { Autocomplete } from './components/Autocomplete';
 
 export const App: React.FC = () => {
-  const [isFocused, setIsFocused] = useState(false);
-  const [inputQuery, setInputQuery] = useState('');
   const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
-
-  const filterQuery = useMemo(() => {
-    return peopleFromServer.filter(person => person.name.toLowerCase()
-      .includes(inputQuery.toLowerCase()));
-  }, [inputQuery]);
-
-  const onSelect = (person: Person) => {
-    setInputQuery(person.name);
-    setSelectedPerson(person);
-    setIsFocused(false);
-  };
 
   return (
     <main className="section">
+
       <h1 className="title">
         {selectedPerson ? (
           `${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`
         ) : (
-          'No matches found'
+          'No selected person'
         )}
 
       </h1>
 
-      <div className={cn('dropdown', {
-        'is-active': isFocused,
-      })}
-      >
-        <div className="dropdown-trigger">
-          <input
-            type="text"
-            placeholder="Enter a part of the name"
-            className="input"
-            value={inputQuery}
-            onFocus={() => setIsFocused(true)}
-            onChange={(event) => setInputQuery(event.target.value)}
-          />
-        </div>
-
-        <PeopleList
-          personList={filterQuery}
-          onSelect={onSelect}
-        />
-      </div>
-    </main>
+      <Autocomplete
+        personList={peopleFromServer}
+        onSelect={setSelectedPerson}
+        delay={1000}
+      />
+    </main >
   );
 };
