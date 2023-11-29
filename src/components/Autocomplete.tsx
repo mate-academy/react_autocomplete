79409c3dbc032e271f649730ab223f7e170ff8ad
Dmytro Krasnoholovenko <errisi.v@gmail.com>
2023-11-29 22:14:18 +0200

    result

---
+++ src/components/Autocomplete.tsx
@@ -0,0 +1,108 @@
+import cn from 'classnames';
+import debounce from 'lodash.debounce';
+import React, { useCallback, useMemo, useState } from 'react';
+
+import { peopleFromServer } from '../data/people';
+import { Person } from '../types/Person';
+
+type Props = {
+  onSelect: (person: Person) => void,
+  delay: number,
+};
+
+export const Autocomplete: React.FC<Props> = ({
+  onSelect,
+  delay,
+}) => {
+  const [visibleQuery, setVisibleQuery] = useState('');
+  const [inputQuery, setInputQuery] = useState('');
+  const [isListVisible, setIsListVisible] = useState(false);
+
+  const currentInputQuery = useCallback(
+    debounce((value) => setInputQuery(value), delay),
+    [setInputQuery, delay],
+  );
+
+  const filteredPeople = useMemo(() => {
+    return peopleFromServer.filter((person: Person) => (
+      person.name.toLowerCase().includes(inputQuery.trim().toLowerCase())
+    ));
+  }, [inputQuery]);
+
+  const handleCurrentPerson = (
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+    person: Person,
+  ) => {
+    event.preventDefault();
+    setVisibleQuery(person.name);
+    onSelect(person);
+    setIsListVisible(false);
+  };
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setVisibleQuery(e.target.value);
+    currentInputQuery(e.target.value);
+    setIsListVisible(true);
+  };
+
+  const handleOnFocus = () => {
+    if (!visibleQuery) {
+      setIsListVisible(true);
+    }
+  };
+
+  const handleOnBlur = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (visibleQuery || inputQuery) {
+      setVisibleQuery('');
+      setInputQuery(e.target.value);
+      currentInputQuery('');
+    } else {
+      setTimeout(() => {
+        setIsListVisible(false);
+      }, 100);
+    }
+  };
+
+  return (
+    <div className="dropdown is-active">
+      <div className="dropdown-trigger">
+        <input
+          type="text"
+          placeholder="Enter a part of the name"
+          className="input"
+          value={visibleQuery}
+          onChange={handleOnChange}
+          onFocus={handleOnFocus}
+          onBlur={handleOnBlur}
+        />
+      </div>
+
+      {isListVisible && (
+        <div className="dropdown-menu" role="menu">
+          <div className="dropdown-content">
+
+            {!filteredPeople.length && (
+              <p className="dropdown-item has-text-primary">
+                No matching suggestions
+              </p>
+            )}
+
+            {filteredPeople.map(person => (
+              <div className="dropdown-item">
+                <a
+                  key={person.slug}
+                  onClick={(e) => handleCurrentPerson(e, person)}
+                  className={cn('has-text-link', {
+                    'has-text-danger': person.sex === 'f',
+                  })}
+                >
+                  {person.name}
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
