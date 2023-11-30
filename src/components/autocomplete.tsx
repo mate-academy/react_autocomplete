3e7f2d0d895e8a10c7e0bdebaf8292cacf666deb
Yaroslav Prisyazhniy <prisyazhniy2002@gmail.com>
2023-11-30 20:57:46 +0200

    errors fixed

--- src/components/autocomplete.tsx
+++ src/components/autocomplete.tsx
@@ -1,10 +1,11 @@
-import { useMemo, useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import classNames from 'classnames';
 import debounce from 'lodash.debounce';
 import { Person } from '../types/Person';
+import './autocomplete.scss';
 
 type Props = {
-  people: Person[],
+  people: Person[];
   setPerson?: (person: Person) => void;
   delay: number;
 };
@@ -17,9 +18,14 @@ export const Autocomplete: React.FC<Props> = ({
   const [dropdownActive, setDropdownActive] = useState(false);
   const [query, setQuery] = useState('');
   const [choosePeople, setChoosePeople] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout | null>(null);
 
-  const applyQuery = useMemo(
-    () => debounce(setQuery, delay), [delay],
+  const applyFilter = useMemo(
+    () => debounce((filter: string) => {
+      setQuery(filter);
+    }, delay),
+    [delay],
   );
 
   const selectPerson = (
@@ -37,23 +43,64 @@ export const Autocomplete: React.FC<Props> = ({
     setDropdownActive(true);
   };
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setChoosePeople(e.target.value);
-    applyQuery(e.target.value);
+  const handleTimeout = () => {
+    if (choosePeople === query) {
+      applyFilter(choosePeople);
+    }
   };
 
+  const handleInputChange
+    = ({ target: { value } }: React.ChangeEvent<HTMLInputElement>) => {
+      setChoosePeople(value);
+      setIsLoading(true);
+
+      applyFilter(value);
+
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+
+      const id = setTimeout(() => {
+        setIsLoading(false);
+        handleTimeout();
+      }, delay);
+
+      setTimeoutId(id);
+    };
+
   const filteredPeople = useMemo(() => {
-    return people
-      .filter(person => person.name.toLocaleLowerCase()
-        .includes(query.toLocaleLowerCase()));
+    return people.filter((person) => person.name
+      .toLocaleLowerCase().includes(query.toLocaleLowerCase()));
   }, [query, people]);
 
+  let content;
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  } else if (filteredPeople.length) {
+    content = filteredPeople.map((person) => (
+      <a
+        className="dropdown-item"
+        key={person.slug}
+        href="/#"
+        onClick={(event) => selectPerson(event, person)}
+      >
+        <p className="has-text-link">
+          <span className="username">{person.name}</span>
+        </p>
+      </a>
+    ));
+  } else {
+    content = 'No matching suggestions';
+  }
+
   return (
-    <div className={classNames('dropdown', {
-      'is-active': dropdownActive,
-    })}
+    <div
+      className={classNames('dropdown', {
+        'is-active': dropdownActive,
+      })}
     >
-      <div className="dropdown-trigger ">
+      <div className="dropdown-trigger">
         <input
           type="text"
           value={choosePeople}
@@ -66,25 +113,10 @@ export const Autocomplete: React.FC<Props> = ({
       </div>
 
       <div className="dropdown-menu" role="menu">
-        <div
-          className="dropdown-content"
-        >
-          {filteredPeople.length
-            ? filteredPeople.map(person => (
-              <a
-                className="dropdown-item"
-                key={person.name}
-                href="/#"
-                onClick={event => selectPerson(event, person)}
-              >
-                <p
-                  className="has-text-link"
-                >
-                  {person.name}
-                </p>
-              </a>
-            ))
-            : 'No matching suggestions'}
+        <div className="dropdown-content">
+          <div className="user-list">
+            {content}
+          </div>
         </div>
       </div>
     </div>
