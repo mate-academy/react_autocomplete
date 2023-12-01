7a8559fc9200bb95f8314b911e9a9c7ce8c90d92
SerhiiUnhurian <sergeyunguryan322@gmail.com>
2023-12-01 16:53:13 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
+import debounce from 'lodash.debounce';
 import './App.scss';
 import { peopleFromServer } from './data/people';
+import { Person } from './types/Person';
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+  const [showDropDownMenu, setShowDropDownMenu] = useState(false);
+
+  const applyQuery = useCallback(debounce(setAppliedQuery, 1000), []);
+
+  const filteredPeople = useMemo(
+    () => peopleFromServer.filter(
+      person => person.name.toLowerCase().includes(appliedQuery.toLowerCase()),
+    ), [appliedQuery],
+  );
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+    applyQuery(event.target.value);
+  };
+
+  const handleItemClick = (person: Person) => {
+    setSelectedPerson(person);
+    setQuery(person.name);
+  };
 
   return (
     <main className="section">
-      <h1 className="title">
+      {selectedPerson && (
+        <h1 className="title">
+          {`${selectedPerson.name} (${selectedPerson.born} = ${selectedPerson.died})`}
+        </h1>
+      )}
+      {/* <h1 className="title">
         {`${name} (${born} = ${died})`}
-      </h1>
+      </h1> */}
 
       <div className="dropdown is-active">
         <div className="dropdown-trigger">
@@ -17,40 +45,52 @@ export const App: React.FC = () => {
             type="text"
             placeholder="Enter a part of the name"
             className="input"
+            value={query}
+            onChange={handleQueryChange}
+            onFocus={() => setShowDropDownMenu(true)}
+            onBlur={() => setShowDropDownMenu(false)}
           />
         </div>
 
-        <div className="dropdown-menu" role="menu">
-          <div className="dropdown-content">
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Bernard Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Antone Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Petronella de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Hercke</p>
+        {showDropDownMenu && (
+          <div className="dropdown-menu" role="menu">
+            <div className="dropdown-content">
+              {filteredPeople.length
+                ? filteredPeople.map(person => (
+                  <div
+                    className="dropdown-item"
+                    key={person.slug}
+                    onClick={() => handleItemClick(person)}
+                  >
+                    <p className="has-text-link">{person.name}</p>
+                  </div>
+                ))
+                : (
+                  <div className="dropdown-item">
+                    <p>
+                      No matching suggestions
+                    </p>
+                  </div>
+                )}
             </div>
           </div>
-        </div>
+        )}
+
+        {/* <div className="dropdown-menu" role="menu">
+          <div className="dropdown-content">
+
+            {suggestions.map(person => (
+              <div
+                className="dropdown-item"
+                key={person.slug}
+                onClick={() => handleItemClick(person)}
+              >
+                <p className="has-text-link">{person.name}</p>
+              </div>
+            ))}
+
+          </div>
+        </div> */}
       </div>
     </main>
   );
