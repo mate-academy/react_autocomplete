85f19f9fbbbef8f5ba80c65004c0efe48ac034a4
Maksym Svoboda <maksim4ik71@gmail.com>
2023-12-02 19:02:52 +0200

    made solution to autocomplete

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,88 @@
-import React from 'react';
+import cn from 'classnames';
+import React, { useMemo, useState } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
+import { debounce } from './helpers';
+import { Person } from './types/Person';
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState(query);
+  const [inputIsFocused, setInputIsFocused] = useState(false);
+  const [queryIsProcessed, setQueryIsProcessed] = useState(true);
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+
+  const peopleToView = useMemo(() => {
+    return peopleFromServer.filter(person => person.name.toLowerCase()
+      .includes(appliedQuery.toLowerCase()));
+  }, [appliedQuery]);
+
+  const applyQuery = useMemo(
+    () => debounce((newQuery: string) => {
+      setAppliedQuery(newQuery);
+      setQueryIsProcessed(true);
+    }, 1000),
+    [],
+  );
+
+  const handleChangeQuery = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQueryIsProcessed(false);
+    setQuery(event.target.value);
+    applyQuery(event.target.value);
+  };
+
+  const handlePersonClicked = (person: Person) => {
+    setSelectedPerson(person);
+    setInputIsFocused(false);
+    setQuery(person.name);
+    setAppliedQuery(person.name);
+  };
 
   return (
     <main className="section">
       <h1 className="title">
-        {`${name} (${born} = ${died})`}
+        {selectedPerson
+          ? `${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`
+          : 'No selected person'}
       </h1>
 
-      <div className="dropdown is-active">
+      <div
+        className={cn('dropdown', {
+          'is-active': inputIsFocused && queryIsProcessed,
+        })}
+      >
         <div className="dropdown-trigger">
           <input
             type="text"
             placeholder="Enter a part of the name"
             className="input"
+            value={query}
+            onChange={handleChangeQuery}
+            onFocus={() => setInputIsFocused(true)}
+            onBlur={() => setTimeout(() => setInputIsFocused(false), 200)}
           />
         </div>
 
         <div className="dropdown-menu" role="menu">
           <div className="dropdown-content">
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Bernard Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Antone Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Petronella de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Hercke</p>
-            </div>
+            {peopleToView.length === 0 ? (
+              <div className="dropdown-item">
+                <p>No matching suggestions</p>
+              </div>
+            ) : (
+              peopleToView.map((person: Person) => (
+                <div
+                  aria-hidden
+                  className="dropdown-item"
+                  key={person.slug}
+                  onClick={() => handlePersonClicked(person)}
+                >
+                  <p className={`${person.sex === 'm' ? 'has-text-link' : 'has-text-danger'}`}>
+                    {person.name}
+                  </p>
+                </div>
+              ))
+            )}
           </div>
         </div>
       </div>
