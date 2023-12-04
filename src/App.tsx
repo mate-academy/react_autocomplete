823e95153cbfbb5833afb1d75ea4498eddf43980
Shostyy <anatoliy.shostal@gmail.com>
2023-12-05 00:03:28 +0200

    Fixed task solution: moved logic from App to DropDown list, fix useCallback warning, fix div warning, etc.

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,16 @@
-import React, { useState, useMemo, useCallback } from 'react';
-import { debounce } from 'lodash';
+import React, { useState } from 'react';
 
 import './App.scss';
 import Information from './components/Information/Information';
-import DropDownList from './components/DropDownList/DropDownList';
+import { DropDownList } from './components/DropDownList/DropDownList';
 import { Person } from './types/Person';
 
 import { peopleFromServer } from './data/people';
 
+const delay = 1000;
+
 export const App: React.FC = () => {
   const [currentPerson, setCurrentPerson] = useState<Person | null>(null);
-  const [hasNoResultError, setHasNoResultError] = useState(false);
-  const [appliedQuery, setAppliedQuery] = useState('');
-  const delay = 1000;
-
-  const applyQuery = useCallback(
-    debounce((value: string) => {
-      setAppliedQuery(value);
-    }, delay),
-    [setAppliedQuery],
-  );
-
-  const filteredPeople = useMemo(() => {
-    setHasNoResultError(false);
-
-    const arrayOfPeople = peopleFromServer.filter((item: Person) => {
-      return item.name.toLowerCase().includes(appliedQuery.toLowerCase());
-    });
-
-    if (arrayOfPeople.length === 0) {
-      setHasNoResultError(true);
-    }
-
-    return arrayOfPeople;
-  }, [appliedQuery]);
-
-  const handleQueryChange = (name: string) => {
-    applyQuery(name);
-  };
 
   return (
     <main className="section">
@@ -46,10 +19,9 @@ export const App: React.FC = () => {
       />
 
       <DropDownList
-        filteredPeople={filteredPeople}
-        handleQueryChange={handleQueryChange}
-        hasNoResultError={hasNoResultError}
+        peopleList={peopleFromServer}
         setCurrentPerson={(person) => setCurrentPerson(person)}
+        delay={delay}
       />
     </main>
   );
--- src/components/DropDownList/DropDownList.tsx
+++ src/components/DropDownList/DropDownList.tsx
@@ -1,27 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo, useCallback } from 'react';
+import { debounce } from 'lodash';
+
 import { Person } from '../../types/Person';
 
 interface People {
-  filteredPeople: Person[];
-  handleQueryChange: (suggest: string) => void;
-  hasNoResultError: boolean;
+  peopleList: Person[];
   setCurrentPerson: (person: Person) => void;
+  delay: number;
 }
 
-const DropDownList: React.FC<People> = ({
-  filteredPeople,
-  handleQueryChange,
-  hasNoResultError,
+export const DropDownList: React.FC<People> = ({
+  peopleList,
   setCurrentPerson,
+  delay,
 }) => {
   const [isFocused, setIsFocused] = useState(false);
   const [inputText, setInputText] = useState('');
   const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+  const [appliedQuery, setAppliedQuery] = useState('');
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const filteredPeople = useMemo(() => {
+    const arrayOfPeople = peopleList.filter((item: Person) => {
+      return item.name.toLowerCase().includes(appliedQuery.toLowerCase());
+    });
+
+    return arrayOfPeople;
+  }, [appliedQuery, peopleList]);
+
+  const debounceHandler = useMemo(() => debounce((
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
     setInputText(event.target.value);
-    handleQueryChange(event.target.value);
-  };
+    setAppliedQuery(event.target.value);
+  }, delay),
+  [delay, setAppliedQuery, setInputText]);
+
+  const handleInputChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      debounceHandler(event);
+    },
+    [debounceHandler],
+  );
 
   const handleInputFocus = () => {
     setIsFocused(true);
@@ -35,17 +54,12 @@ const DropDownList: React.FC<People> = ({
     }
   };
 
-  const handleItemClick = (
-    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
-    person: Person,
-  ) => {
-    event.preventDefault();
-
+  const handleItemClick = (person: Person) => {
     setSelectedPerson(person);
     setCurrentPerson(person);
     setInputText('');
     setIsFocused(false);
-    handleQueryChange('');
+    setAppliedQuery('');
   };
 
   return (
@@ -65,17 +79,24 @@ const DropDownList: React.FC<People> = ({
       {isFocused && (
         <div className="dropdown-menu" role="menu">
           <div className="dropdown-content">
-            {filteredPeople.map((person: Person) => (
-              <a
-                href="/"
-                className={`dropdown-item ${selectedPerson === person ? 'selected' : ''}`}
-                key={person.name}
-                onClick={(event) => handleItemClick(event, person)}
-              >
-                <p className="has-text-link">{person.name}</p>
-              </a>
-            ))}
-            {hasNoResultError && (
+            {filteredPeople.length !== 0 ? (
+              filteredPeople.map((person: Person) => (
+                <div
+                  className={`dropdown-item ${selectedPerson === person ? 'selected' : ''}`}
+                  key={person.name}
+                  onClick={() => handleItemClick(person)}
+                  onKeyDown={(e) => {
+                    if (e.key === 'Enter') {
+                      handleItemClick(person);
+                    }
+                  }}
+                  role="menuitem"
+                  tabIndex={0}
+                >
+                  <p className="has-text-link">{person.name}</p>
+                </div>
+              ))
+            ) : (
               <div className="dropdown-item">
                 <p className="has-text-link">No matching suggestions</p>
               </div>
@@ -86,5 +107,3 @@ const DropDownList: React.FC<People> = ({
     </div>
   );
 };
-
-export default DropDownList;
