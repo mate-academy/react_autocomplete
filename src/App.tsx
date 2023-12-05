f10245ef584b93e860dd5cf84b4716d94324bc6f
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2023-12-06 00:02:40 +0200

    Revert "autocomplete lodash"
    This reverts commit 6a7c643189807818b4708a81622881aca0907018.

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,55 @@
-import React, { useState, useCallback, useMemo } from 'react';
+import React from 'react';
 import './App.scss';
-import classNames from 'classnames';
-import debounce from 'lodash.debounce';
 import { peopleFromServer } from './data/people';
-import { PeopleDropdown } from './components/PeopleDropdown';
-import { Person } from './types/Person';
-
-// You can change delay here
-const delay = 700;
 
 export const App: React.FC = () => {
-  const [query, setQuery] = useState('');
-  const [appliedQuery, setAppliedQuery] = useState('');
-  const [selectPerson, setSelectPerson] = useState<Person | null>(null);
-
-  const people = useMemo(() => (
-    peopleFromServer.filter(person => (
-      person.name.toLowerCase().includes(appliedQuery.toLowerCase())))
-  ), [appliedQuery]);
-
-  const resetQuery = useCallback(() => {
-    setAppliedQuery('');
-    setQuery('');
-  }, []);
-
-  const applyQuery = useCallback(
-    debounce(setAppliedQuery, delay),
-    [appliedQuery],
-  );
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setQuery(e.target.value);
-    applyQuery(e.target.value);
-  };
-
-  const handleSelect = useCallback((person: Person) => {
-    setSelectPerson(person);
-    resetQuery();
-  }, []);
+  const { name, born, died } = peopleFromServer[0];
 
   return (
     <main className="section">
       <h1 className="title">
-        {selectPerson
-          ? `${selectPerson.name} (${selectPerson.born} - ${selectPerson.died})`
-          : 'No selected person'}
+        {`${name} (${born} = ${died})`}
       </h1>
 
-      <div className={classNames('dropdown',
-        { 'is-active': appliedQuery })}
-      >
+      <div className="dropdown is-active">
         <div className="dropdown-trigger">
           <input
             type="text"
-            value={query}
             placeholder="Enter a part of the name"
             className="input"
-            onChange={handleChange}
           />
         </div>
 
         <div className="dropdown-menu" role="menu">
-          <PeopleDropdown people={people} onSelect={handleSelect} />
+          <div className="dropdown-content">
+            <div className="dropdown-item">
+              <p className="has-text-link">Pieter Haverbeke</p>
+            </div>
+
+            <div className="dropdown-item">
+              <p className="has-text-link">Pieter Bernard Haverbeke</p>
+            </div>
+
+            <div className="dropdown-item">
+              <p className="has-text-link">Pieter Antone Haverbeke</p>
+            </div>
+
+            <div className="dropdown-item">
+              <p className="has-text-danger">Elisabeth Haverbeke</p>
+            </div>
+
+            <div className="dropdown-item">
+              <p className="has-text-link">Pieter de Decker</p>
+            </div>
+
+            <div className="dropdown-item">
+              <p className="has-text-danger">Petronella de Decker</p>
+            </div>
+
+            <div className="dropdown-item">
+              <p className="has-text-danger">Elisabeth Hercke</p>
+            </div>
+          </div>
         </div>
       </div>
     </main>
