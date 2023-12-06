d028f745a1dadb28a87eedd26d2d310f88f1ed6c
Your NameNatalia <you@example.comnetzelnatalia@gmail.com>
2023-12-06 18:57:14 +0100

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
+import { Dropdown } from './components/Dropdown';
 import { peopleFromServer } from './data/people';
+import { Person } from './types/Person';
+
+const TIMER_FILTER_DELAY = 1000;
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
 
   return (
     <main className="section">
       <h1 className="title">
-        {`${name} (${born} = ${died})`}
+        {selectedPerson
+          ? (
+            <p>
+              {`${selectedPerson.name} (${selectedPerson.born} = ${selectedPerson.died})`}
+            </p>
+          )
+          : <p>No selected person.</p>}
       </h1>
 
-      <div className="dropdown is-active">
-        <div className="dropdown-trigger">
-          <input
-            type="text"
-            placeholder="Enter a part of the name"
-            className="input"
-          />
-        </div>
-
-        <div className="dropdown-menu" role="menu">
-          <div className="dropdown-content">
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Bernard Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Antone Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Petronella de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Hercke</p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Dropdown
+        onSelect={setSelectedPerson}
+        people={peopleFromServer}
+        delay={TIMER_FILTER_DELAY}
+      />
     </main>
   );
 };
