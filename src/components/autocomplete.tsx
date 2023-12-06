ed6a15556dbe00d21ff7f7d923b21714014f3a7b
katya sahun <katerinasagun@Katerinas-MacBook-Air.local>
2023-12-06 21:49:30 +0200

    solution

--- src/components/autocomplete.tsx
+++ src/components/autocomplete.tsx
@@ -4,101 +4,98 @@ import debounce from 'lodash.debounce';
 import { Person } from '../types/Person';
 
 type Props = {
-    people: Person[],
-    onSelected?: (person: Person | string) => void;
+  people: Person[];
+  onSelected?: (person: Person | string) => void;
 };
 export const Autocomplete: React.FC<Props> = ({
-    people,
-    onSelected = () => { },
+  people,
+  onSelected = () => {},
 }) => {
-    const [query, setQuery] = useState('');
-    const [appliedQuery, setAppliedQuery] = useState('');
-    const [showDropdown, setShowDropdown] = useState(false);
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
+  const [showDropdown, setShowDropdown] = useState(false);
 
-    const applyQuery = useCallback(debounce(setAppliedQuery, 1000), []);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const applyQuery = useCallback(debounce(setAppliedQuery, 1000), []);
 
-    const filteredPeople = useMemo(() => {
-        return people.filter(({ name }) => {
-            return name.toLowerCase().includes(appliedQuery.toLowerCase().trim());
-        });
-    }, [appliedQuery, people]);
+  const filteredPeople = useMemo(() => {
+    return people.filter(({ name }) => {
+      return name.toLowerCase().includes(appliedQuery.toLowerCase().trim());
+    });
+  }, [appliedQuery, people]);
 
-    const handlePersonSelect = (
-        event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
-    ) => {
-        event.preventDefault();
+  const handlePersonSelect = (
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) => {
+    event.preventDefault();
 
-        const person = filteredPeople.find(personItem => {
-            return personItem.name === event.currentTarget.textContent || '';
-        });
+    const person = filteredPeople.find((personItem) => {
+      return personItem.name === event.currentTarget.textContent || '';
+    });
 
-        if (person) {
-            onSelected(person);
-        }
-
-        setQuery(event.currentTarget.textContent || '');
-        applyQuery(event.currentTarget.textContent || '');
-
-        setShowDropdown(false);
+    if (person) {
+      onSelected(person);
     }
 
-    const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setQuery(event.target.value);
-        applyQuery(event.target.value);
+    setQuery(event.currentTarget.textContent || '');
+    applyQuery(event.currentTarget.textContent || '');
 
-        if (event.target.value.length === 0) {
-            onSelected('');
-        }
-    };
+    setShowDropdown(false);
+  };
 
-    return (
-        <div className="dropdown is-active">
-            <div className="dropdown-trigger">
-                <input
-                    type="text"
-                    placeholder="Enter a part of the name"
-                    className="input"
-                    value={query}
-                    onChange={handleQueryChange}
-                    onFocus={() => setShowDropdown(true)}
-                    onBlur={() => setShowDropdown(false)}
-                />
-            </div>
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+    applyQuery(event.target.value);
 
-            {showDropdown && (
-                <div className="dropdown-menu" role="menu">
-                    <div className="dropdown-content">
-                        {filteredPeople.length ? filteredPeople.map(person => {
-                            const { slug, name, sex } = person;
-                            const isWoman = sex === 'f';
+    if (event.target.value.length === 0) {
+      onSelected('');
+    }
+  };
 
-                            return (
-                                <div
-                                    className="dropdown-item"
-                                    key={slug}
-                                >
-                                    <a
-                                        className={classNames(
-                                            isWoman
-                                                ? 'has-text-danger'
-                                                : 'has-text-link',
-                                        )}
-                                        href="/"
-                                        onMouseDown={handlePersonSelect}
-                                    >
-                                        {name}
-                                    </a>
-                                </div>
-                            )
-                        })
-                            : (
-                                <div className="dropdown-item">
-                                    <p>No matching suggestion</p>
-                                </div>
-                            )}
-                    </div>
-                </div>
+  return (
+    <div className="dropdown is-active">
+      <div className="dropdown-trigger">
+        <input
+          type="text"
+          placeholder="Enter a part of the name"
+          className="input"
+          value={query}
+          onChange={handleQueryChange}
+          onFocus={() => setShowDropdown(true)}
+          onBlur={() => setShowDropdown(false)}
+        />
+      </div>
+
+      {showDropdown && (
+        <div className="dropdown-menu" role="menu">
+          <div className="dropdown-content">
+            {filteredPeople.length ? (
+              filteredPeople.map((person) => {
+                const { slug, name, sex } = person;
+                const isWoman = sex === 'f';
+
+                return (
+                  <div className="dropdown-item" key={slug}>
+                    <a
+                      className={classNames(
+                        isWoman ? 'has-text-danger' : 'has-text-link',
+                      )}
+                      href="/"
+                      onMouseDown={handlePersonSelect}
+                    >
+                      {name}
+                    </a>
+                  </div>
+                );
+              })
+            ) : (
+              <div className="dropdown-item">
+                <p>No matching suggestion</p>
+              </div>
             )}
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 };
