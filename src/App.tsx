193dce3c12622e66d119a34c0997b5a911e9c53a
jacekkosiba <jac.kosiba@gmail.com>
2023-12-15 18:09:18 +0100

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,84 @@
-import React from 'react';
+import {
+  FC, FocusEventHandler, MouseEvent, useEffect, useState,
+} from 'react';
+import cn from 'classnames';
 import './App.scss';
 import { peopleFromServer } from './data/people';
+import { Person } from './types/Person';
+import { AppProps } from './types/AppProps';
 
-export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+export const App: FC<AppProps> = ({ queryDelay = 1000 }) => {
+  const [query, setQuery] = useState('');
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [visiblePeople, setVisiblePeople] = useState(peopleFromServer);
+  const [selectedPerson, setSelectedPerson] = useState<Person>({
+    name: '',
+    sex: 'm',
+    born: 0,
+    died: 0,
+    fatherName: '',
+    motherName: '',
+    slug: '',
+  });
+
+  const { name, born, died } = selectedPerson;
+
+  const handleInputBlur: FocusEventHandler<HTMLInputElement> = (event) => {
+    if (event.relatedTarget?.className === 'dropdown-item') {
+      return;
+    }
+
+    setShowDropdown(false);
+  };
+
+  const onSelected
+  = (event: MouseEvent<HTMLAnchorElement>, person: Person) => {
+    event.preventDefault();
+    setSelectedPerson(person);
+    setShowDropdown(false);
+    setQuery('');
+  };
+
+  useEffect(() => {
+    if (query) {
+      setShowDropdown(false);
+    }
+
+    const timeoutId = setTimeout(() => {
+      let filteredPeople = [...peopleFromServer];
+
+      if (query) {
+        filteredPeople = filteredPeople
+          .filter(person => person.name.toLocaleLowerCase()
+            .includes(query.toLocaleLowerCase()));
+      }
+
+      setVisiblePeople(filteredPeople);
+      if (query) {
+        setShowDropdown(true);
+      }
+    }, queryDelay);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [query, queryDelay]);
 
   return (
     <main className="section">
-      <h1 className="title">
-        {`${name} (${born} = ${died})`}
-      </h1>
+      {
+        selectedPerson.name
+          ? (
+            <h1 className="title">
+              {`${name} (${born} = ${died})`}
+            </h1>
+          )
+          : (
+            <h1 className="title">
+              No selected person
+            </h1>
+          )
+      }
 
       <div className="dropdown is-active">
         <div className="dropdown-trigger">
@@ -17,38 +86,45 @@ export const App: React.FC = () => {
             type="text"
             placeholder="Enter a part of the name"
             className="input"
+            value={query}
+            onChange={(e) => setQuery(e.target.value.trimStart())}
+            onBlur={handleInputBlur}
+            onClick={() => setShowDropdown(prev => !prev)}
           />
         </div>
 
         <div className="dropdown-menu" role="menu">
-          <div className="dropdown-content">
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Bernard Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Antone Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Haverbeke</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Petronella de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Hercke</p>
-            </div>
+          <div
+            className={cn('dropdown-content', {
+              'is-hidden': !showDropdown,
+            })}
+          >
+            {
+              !visiblePeople.length
+                ? (
+                  <span className="has-text-grey-light">
+                    No matching suggestions
+                  </span>
+                )
+                : (
+                  visiblePeople.map(person => (
+                    <a
+                      key={person.slug}
+                      href={`#${person.slug}`}
+                      className="dropdown-item"
+                      onClick={(e) => onSelected(e, person)}
+                    >
+                      <p
+                        className={cn({
+                          'has-text-danger': person.sex === 'f',
+                          'has-text-info': person.sex === 'm',
+                        })}
+                      >
+                        {person.name}
+                      </p>
+                    </a>
+                  )))
+            }
           </div>
         </div>
       </div>
