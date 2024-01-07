90e92a259e9c54e5857ed617b549ddadd778ec92
boytrend0108 <boytrend@gmail.com>
2024-01-07 21:43:43 +0200

    handle dropdoow

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
 import { Autocomplete } from './Autocomplete/Autocomplete';
@@ -7,6 +7,13 @@ export const App: React.FC = () => {
   const { name, born, died } = peopleFromServer[0];
 
   const [query, setQuery] = useState('');
+  const [isFocus, setIsFocus] = useState(false);
+
+  const closeDropdown = useCallback(() => {
+    setTimeout(() => {
+      setIsFocus(false);
+    }, 200);
+  }, []);
 
   return (
     <main className="section">
@@ -19,10 +26,17 @@ export const App: React.FC = () => {
           type="text"
           placeholder="Enter a part of the name"
           className="input"
+          value={query}
           onChange={event => setQuery(event.target.value)}
+          onFocus={() => setIsFocus(true)}
+          onBlur={closeDropdown}
         />
 
-        <Autocomplete query={query} />
+        <Autocomplete
+          query={query}
+          isFocus={isFocus}
+          onSelect={(value) => setQuery(value)}
+        />
       </div>
     </main>
   );
--- src/Autocomplete/Autocomplete.tsx
+++ src/Autocomplete/Autocomplete.tsx
@@ -1,35 +1,44 @@
+import React from 'react';
 import cn from 'classnames';
+import './Autocomplete.scss';
+
+import { peopleFromServer } from '../data/people';
+import { Person } from '../types/Person';
 
 type Props = {
   query: string;
+  isFocus: boolean,
+  onSelect: (name: string) => void
 };
 
-export const Autocomplete: React.FC<Props> = ({ query }) => {
+export const Autocomplete: React.FC<Props> = React.memo(({
+  // query,
+  isFocus,
+  onSelect,
+}) => {
   return (
     <div className={cn('dropdown', {
-      'is-active': query,
+      'is-active': isFocus,
     })}
     >
-      <div className="dropdown-menu" id="dropdown-menu" role="menu">
+      <div
+        className="dropdown-menu"
+        id="dropdown-menu"
+        role="menu"
+      >
         <div className="dropdown-content">
-          <p className="dropdown-item">
-            Dropdown item
-          </p>
-          <p className="dropdown-item">
-            Other dropdown item
-          </p>
-          <p className="dropdown-item is-active">
-            Active dropdown item
-          </p>
-          <p className="dropdown-item">
-            Other dropdown item
-          </p>
-          <hr className="dropdown-divider" />
-          <p className="dropdown-item">
-            With a divider
-          </p>
+          {peopleFromServer.map((el: Person) => (
+            <button
+              type="button"
+              className="dropdown-item"
+              key={el.slug}
+              onClick={() => onSelect(el.name)}
+            >
+              {el.name}
+            </button>
+          ))}
         </div>
       </div>
     </div>
   );
-};
+});
