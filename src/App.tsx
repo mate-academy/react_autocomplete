f18292d28369352e765aaa599fdef286c9f0c9a6
boytrend0108 <boytrend@gmail.com>
2024-01-08 12:15:54 +0200

    complete task

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,10 @@
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
 import './App.scss';
 import { Autocomplete } from './Autocomplete/Autocomplete';
 import { Person } from './types/Person';
 
-// eslint-disable-next-line @typescript-eslint/ban-types
-const debounce = (callback: Function, delay: number) => {
-  let timerId = 0;
-
-  return (...args: any) => {
-    window.clearTimeout(timerId);
-
-    timerId = window.setTimeout(() => {
-      callback(...args);
-    }, delay);
-  };
-};
-
 export const App: React.FC = () => {
   const [person, setPerson] = useState<Person | null>(null);
-  const [isFocus, setIsFocus] = useState(false);
-  const [query, setQuery] = useState('');
-  const [appliedQuery, setAppliedQuery] = useState('');
-
-  const closeDropdown = useCallback(() => {
-    setTimeout(() => {
-      setIsFocus(false);
-    }, 200);
-  }, []);
-
-  const applyQuery = useCallback(debounce(setAppliedQuery, 1000), []);
-
-  const handlerOnSelect = (selectedPerson: Person) => {
-    setPerson(selectedPerson);
-    setQuery(selectedPerson.name);
-  };
 
   return (
     <main className="section">
@@ -45,26 +16,10 @@ export const App: React.FC = () => {
         <p className="title">No selected person</p>
       )}
 
-      <div className="dropdown">
-        <input
-          type="text"
-          placeholder="Enter a part of the name"
-          className="input"
-          value={query}
-          onChange={event => {
-            setQuery(event.target.value);
-            applyQuery(event.target.value);
-          }}
-          onFocus={() => setIsFocus(true)}
-          onBlur={closeDropdown}
-        />
-
-        <Autocomplete
-          query={appliedQuery}
-          isFocus={isFocus}
-          onSelect={handlerOnSelect}
-        />
-      </div>
+      <Autocomplete
+        delay={1000}
+        onSelect={setPerson}
+      />
     </main>
   );
 };
