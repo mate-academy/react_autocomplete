fc2c3a83df079a409d0ce15bc1ba82fe8dd931a9
Sasha <sashavjun22@gmail.com>
2024-01-15 10:41:23 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,107 @@
-import React from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import './App.scss';
+import cn from 'classnames';
 import { peopleFromServer } from './data/people';
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const people = peopleFromServer;
+  const [appliedQuery, setAppliedQuery] = useState('');
+  const [dropdownActive, setDropdownActive] = useState(false);
+  const [hoverActive, setHoverActive] = useState('');
+  const [personPicked, setPersonPicked] = useState('');
+  const [header, setHeader] = useState('No selected person');
+
+  function debounce(f: (...args: string[]) => void, delay = 500) {
+    let timer: number;
+
+    return (...args: string[]) => {
+      clearTimeout(timer);
+      timer = window.setTimeout(f, delay, ...args);
+    };
+  }
+
+  /* eslint-disable */
+
+  const applyQuery = useCallback(
+    debounce(
+      setAppliedQuery,
+    ),
+    [],
+  );
+
+  const getVisiblePeople = () => {
+    return people.filter((person) => person
+      .name
+      .toLowerCase()
+      .includes(appliedQuery.toLowerCase()));
+  };
+
+  const visiblePeople = useMemo(
+    getVisiblePeople,
+    [people, appliedQuery],
+  );
+
+  const onSelected = (
+    personName: string,
+    dateBorn: number,
+    dateDied: number,
+  ) => {
+    setPersonPicked(personName);
+    setHeader(`${personName} (${dateBorn} - ${dateDied})`);
+    setPersonPicked('');
+    setDropdownActive(false);
+  };
+
+  const onChange = (event:React.ChangeEvent<HTMLInputElement>) => {
+    setPersonPicked(event.target.value);
+    applyQuery(event.target.value);
+  };
 
   return (
     <main className="section">
       <h1 className="title">
-        {`${name} (${born} = ${died})`}
+        {header}
       </h1>
 
-      <div className="dropdown is-active">
+      <div className={cn('dropdown', { 'is-active': dropdownActive })}>
         <div className="dropdown-trigger">
           <input
             type="text"
             placeholder="Enter a part of the name"
             className="input"
+            value={personPicked}
+            onFocus={() => setDropdownActive(!dropdownActive)}
+            onChange={onChange}
+            onBlur={() => setDropdownActive(false)}
           />
         </div>
 
         <div className="dropdown-menu" role="menu">
           <div className="dropdown-content">
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Haverbeke</p>
-            </div>
 
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Bernard Haverbeke</p>
-            </div>
+            {visiblePeople.map((person) => (
+              <div
+                key={person.name}
+                className={cn('dropdown-item',
+                  { 'has-background-light': hoverActive === person.name })}
+                onMouseEnter={() => setHoverActive(person.name)}
+                onMouseDown={() => onSelected(person.name,
+                  person.born,
+                  person.died)}
 
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter Antone Haverbeke</p>
-            </div>
+                /* eslint-disable */
 
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Haverbeke</p>
-            </div>
+                aria-hidden="true"
+              >
+                <p className="has-text-link">{person.name}</p>
+              </div>
+            ))}
 
-            <div className="dropdown-item">
-              <p className="has-text-link">Pieter de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Petronella de Decker</p>
-            </div>
-
-            <div className="dropdown-item">
-              <p className="has-text-danger">Elisabeth Hercke</p>
-            </div>
+            {!visiblePeople.length
+            /* eslint-disable */
+                && <p className="message has-text-link">
+                  No matching suggestions
+                   </p>}
           </div>
         </div>
       </div>
