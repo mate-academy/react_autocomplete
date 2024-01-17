4923400a49cf011b787ce50a6dfab606f9ec3a81
Alina <afesh4ak@gmail.com>
2024-01-17 19:30:11 +0100

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import classNames from 'classnames';
 import './App.scss';
 import { peopleFromServer } from './data/people';
@@ -9,6 +9,7 @@ export const App: React.FC = () => {
   const [suggestions, setSuggestions] = useState<Person[]>([]);
   const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
   const [inputFocused, setInputFocused] = useState(false);
+  const inputRef = useRef<HTMLInputElement | null>(null);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newText = event.target.value;
@@ -20,29 +21,44 @@ export const App: React.FC = () => {
 
   const handleInputFocus = () => {
     setInputFocused(true);
-    setSuggestions([]);
+  };
+
+  const handleInputBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    setTimeout(() => {
+      if (
+        e.relatedTarget
+        && e.currentTarget.contains(e.relatedTarget as Node)
+      ) {
+        return;
+      }
+
+      setInputFocused(false);
+      setSuggestions([]);
+    }, 200);
   };
 
   useEffect(() => {
-    let debounceTimeout: NodeJS.Timeout;
+    const fetchSuggestions = () => {
+      const filteredSuggestions: Person[] = peopleFromServer.filter(
+        (person) => person.name.toLowerCase().includes(inputText.toLowerCase()),
+      );
 
-    if (inputFocused) {
-      debounceTimeout = setTimeout(() => {
-        const filteredSuggestions: Person[] = peopleFromServer.filter(
-          (person) => person.name
-            .toLowerCase().includes(inputText.toLowerCase()),
-        );
+      setSuggestions(filteredSuggestions);
+    };
 
-        setSuggestions(filteredSuggestions);
-      }, 1000);
-    }
+    const timeoutId = setTimeout(() => {
+      if (inputFocused && inputRef.current) {
+        fetchSuggestions();
+      }
+    }, 1000);
 
-    return () => clearTimeout(debounceTimeout);
+    return () => clearTimeout(timeoutId);
   }, [inputText, inputFocused]);
 
   const handleSuggestionClick = (person: Person) => {
     setSelectedPerson(person);
     setInputText(person.name);
+    setSuggestions([]);
   };
 
   const handleKeyDown = (
@@ -67,41 +83,39 @@ export const App: React.FC = () => {
       <div className="dropdown is-active" role="listbox">
         <div className="dropdown-trigger">
           <input
+            ref={inputRef}
             type="text"
             placeholder="Enter a part of the name"
             className="input"
             value={inputText}
             onChange={handleInputChange}
             onFocus={handleInputFocus}
+            onBlur={e => handleInputBlur(e)}
           />
         </div>
 
-        <div className="dropdown-menu" role="menu">
-          <div className="dropdown-content">
-            {!suggestions.length && inputText && (
-              <div className="dropdown-item">
-                <p className="has-text-danger">No matching suggestions</p>
-              </div>
-            )}
-
-            {suggestions.map((person) => (
-              <div
-                key={person.name}
-                className={classNames('dropdown-item', {
-                  'has-text-link': person === selectedPerson,
-                  'cursor-pointer': true,
-                })}
-                role="option"
-                aria-selected={person === selectedPerson ? 'true' : 'false'}
-                tabIndex={0}
-                onClick={() => handleSuggestionClick(person)}
-                onKeyDown={(e) => handleKeyDown(e, person)}
-              >
-                <p>{person.name}</p>
-              </div>
-            ))}
+        {inputFocused && suggestions.length > 0 && (
+          <div className="dropdown-menu" role="menu">
+            <div className="dropdown-content">
+              {suggestions.map((person) => (
+                <div
+                  key={person.name}
+                  className={classNames('dropdown-item', {
+                    'has-text-link': person === selectedPerson,
+                    'cursor-pointer': true,
+                  })}
+                  role="option"
+                  aria-selected={person === selectedPerson ? 'true' : 'false'}
+                  tabIndex={0}
+                  onClick={() => handleSuggestionClick(person)}
+                  onKeyDown={(e) => handleKeyDown(e, person)}
+                >
+                  <p>{person.name}</p>
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </main>
   );
