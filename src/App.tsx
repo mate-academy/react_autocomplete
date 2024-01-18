cabf8959ea4f4b0c4941b1bb653c034d713ca64b
Artem <artemmalikov742@gmail.com>
2024-01-18 21:21:33 +0200

    add no selected person option

--- src/App.tsx
+++ src/App.tsx
@@ -32,11 +32,13 @@ export const App: React.FC = () => {
 
   return (
     <main className="section">
-      {selectedPerson && (
-        <h1 className="title">
-          {`${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`}
-        </h1>
-      )}
+      <h1 className="title">
+        {
+          selectedPerson
+            ? `${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`
+            : 'No selected person'
+        }
+      </h1>
 
       <PeopleDropdown
         key={selectedPerson?.slug}
