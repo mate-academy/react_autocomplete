6cae40362c70f691b698cc502dc89a8665a7294b
Hovera-Liubomyr <ljusuk@gmail.com>
2024-01-22 12:44:28 +0200

    react_autocomplete-11

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,100 @@
-import React, { useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import './App.scss';
+import debounce from 'lodash.debounce';
+import cn from 'classnames';
 import { peopleFromServer } from './data/people';
+import { Person } from './types/Person';
 
 export const App: React.FC = () => {
-  const peoples = peopleFromServer;
-  const [persons, setPersons] = useState('');
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
+  const [isfocused, setIsfocused] = useState(false);
+  const [selectedPerson, setSelectedPerson] = useState<Person>();
+  const [isListVisible, setIsListVisible] = useState(true);
 
-  const handleChangePerson = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPersons(event.target.value);
+  const filteredPeople = useMemo(() => {
+    return peopleFromServer
+      .filter(person => person.name.toLowerCase().includes(
+        appliedQuery.toLowerCase(),
+      ));
+  }, [appliedQuery]);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const applyQuery = useCallback(debounce((value) => {
+    setAppliedQuery(value);
+    setIsListVisible(true);
+  }, 1000),
+  []);
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+    setIsListVisible(false);
+    applyQuery(event.target.value);
   };
 
-  const selectedPerson = peoples.find(people => people.name === persons);
+  const handleClick = (name: string) => {
+    setQuery('');
+    setAppliedQuery('');
+
+    const filterdPerson: Person | undefined
+      = peopleFromServer.find(person => person.name === name);
+
+    setSelectedPerson(filterdPerson);
+  };
+
+  const handleMouseDown = (
+    (event: React.KeyboardEvent<HTMLDivElement>, name: string) => {
+      if (event.key === 'Enter') {
+        handleClick(name);
+      }
+    });
 
   return (
     <main className="section">
       <h1 className="title">
-        {selectedPerson ? `${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})` : 'No selected person'}
+        {selectedPerson
+          ? `${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`
+          : 'No selected person'}
       </h1>
-
-      <div className="dropdown is-active">
+      <div className={cn('dropdown', { 'is-active': isfocused })}>
         <div className="dropdown-trigger">
           <input
-            value={persons}
+            value={query}
             type="text"
             placeholder="Enter a part of the name"
             className="input"
-            list="select"
-            onChange={handleChangePerson}
+            onChange={handleQueryChange}
+            onFocus={() => setIsfocused(true)}
+            onBlur={() => setIsfocused(false)}
           />
-          <datalist id="select">
-            {peoples.map((people) => (
-              // eslint-disable-next-line jsx-a11y/control-has-associated-label
-              <option key={people.name} value={people.name} />
-            ))}
-          </datalist>
+        </div>
+
+        <div className="dropdown-menu" role="menu">
+          <ul className="dropdown-content">
+
+            {isListVisible && (
+              filteredPeople.length > 0
+                ? (filteredPeople.map(person => (
+                  <li
+                    className="dropdown-item"
+                    key={person.name}
+                  >
+                    <div
+                      onMouseDown={() => handleClick(person.name)}
+                      onKeyDown={(event) => handleMouseDown(event, person.name)}
+                      role="button"
+                      tabIndex={0}
+                    >
+                      <p className="has-text-link">{person.name}</p>
+                    </div>
+                  </li>
+                )))
+                : (
+                  <li className="dropdown-item">
+                    <p>No matching suggestions</p>
+                  </li>
+                ))}
+          </ul>
         </div>
       </div>
     </main>
