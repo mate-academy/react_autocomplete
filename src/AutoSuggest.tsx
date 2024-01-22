e45a6c17518627ff580d51091ed51df68d263a0f
VolodymyrKolisnichenko <90705859+vk-workshop@users.noreply.github.com>
2024-01-22 23:08:41 +0100

    fixed issue

--- src/AutoSuggest.tsx
+++ src/AutoSuggest.tsx
@@ -39,42 +39,42 @@ export const AutoSuggest: React.FC<Props> = ({
   const handleSuggestionClick = (selectedPerson: Person) => {
     setQuery(selectedPerson.name);
     onSelected(selectedPerson);
+    setAppliedQuery(selectedPerson.name);
   };
 
   return (
-    <>
-      <div className={cn('dropdown', { 'is-active': isInputFocused })}>
-        <div className="dropdown-trigger">
-          <input
-            type="text"
-            placeholder="Enter a part of the name"
-            className="input"
-            value={query}
-            onChange={handleQueryChange}
-            onBlur={() => setIsInputFocused(false)}
-            onFocus={() => setIsInputFocused(true)}
-          />
-        </div>
+    <div className={cn('dropdown', { 'is-active': isInputFocused })}>
+      <div className="dropdown-trigger">
+        <input
+          type="text"
+          placeholder="Enter a part of the name"
+          className="input"
+          value={query}
+          onChange={handleQueryChange}
+          onBlur={() => setIsInputFocused(false)}
+          onFocus={() => setIsInputFocused(true)}
+        />
+      </div>
 
+      {isInputFocused && (
         <div className="dropdown-menu" role="menu">
           <div className="dropdown-content">
-            {filteredPeople.length ? (
-              filteredPeople.map((person) => (
-                <a
-                  className="dropdown-item"
-                  key={person.name}
-                  href="/"
-                  onMouseDown={() => handleSuggestionClick(person)}
-                >
-                  <p className="has-text-link">{person.name}</p>
-                </a>
-              ))
-            ) : (
+            {filteredPeople.map((person) => (
+              <a
+                className="dropdown-item"
+                key={person.name}
+                href="/"
+                onMouseDown={() => handleSuggestionClick(person)}
+              >
+                <p className="has-text-link">{person.name}</p>
+              </a>
+            ))}
+            {filteredPeople.length === 0 && (
               <p className="dropdown-item">No matching suggestions</p>
             )}
           </div>
         </div>
-      </div>
-    </>
+      )}
+    </div>
   );
 };
