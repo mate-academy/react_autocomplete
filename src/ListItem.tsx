e849871b71e20df12b93f43578fc982ec9422337
Yaroslav Samoilenko <yasik2255@live.com>
2024-01-23 21:33:31 +0200

    add solution

---
+++ src/ListItem.tsx
@@ -0,0 +1,35 @@
+import cn from 'classnames';
+import { Person } from './types/Person';
+
+interface Props {
+  person: Person;
+  onSelect: (p: Person) => void;
+}
+
+export const ListItem = (props: Props) => {
+  const { person, onSelect } = props;
+
+  const handleClick = (
+    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ):void => {
+    e.preventDefault();
+    onSelect(person);
+  };
+
+  return (
+    <a
+      href="/#"
+      className="dropdown-item"
+      onClick={handleClick}
+    >
+      <p
+        className={cn({
+          'has-text-link': person.sex === 'm',
+          'has-text-danger': person.sex === 'f',
+        })}
+      >
+        {person.name}
+      </p>
+    </a>
+  );
+};
