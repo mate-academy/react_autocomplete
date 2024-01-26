eb6009e886262ed7d97d3e18d645b5aed8e7d588
Bohdan Liutyi <liutyibohdan13@gmail.com>
2024-01-26 21:34:35 +0200

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,127 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import './App.scss';
+import classNames from 'classnames';
+import debounce from 'lodash.debounce';
+import { peopleFromServer } from './data/people';
+
 import { Person } from './types/Person';
-import { AutoSuggest } from './AutoSuggest';
 
 export const App: React.FC = () => {
-  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+  const [query, setQuery] = useState('');
+  const [apliedQuery, setApliedQuery] = useState('');
+  const [visible, setVisible] = useState(false);
+  const [choosedPerson, setchoosedPerson] = useState<Person | null>(null);
 
-  const handleSelected = (person: Person | null) => {
-    setSelectedPerson(person);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const apllyQuery = useCallback(
+    debounce((value: string) => setApliedQuery(value), 1000),
+    [],
+  );
+
+  const handleOnClick
+    = (eventOnClick: React.MouseEvent, person: Person) => {
+      eventOnClick.preventDefault();
+
+      setQuery(person.name);
+      setApliedQuery(person.name);
+      setVisible(false);
+      setchoosedPerson(person);
+    };
+
+  const preparedList = (people: Person[], queryFilter: string) => {
+    const queryNormalized = queryFilter.toLocaleLowerCase();
+
+    return people.filter(person => person.name.toLocaleLowerCase()
+      .includes(queryNormalized));
   };
+
+  const delayOnBlur = () => {
+    setTimeout(() => setVisible(false), 100);
+  };
+
+  const handleReset = () => {
+    setchoosedPerson(null);
+    setApliedQuery('');
+    setQuery('');
+  };
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+    apllyQuery(event.target.value);
+  };
+
+  const preparedPeopleList = preparedList(peopleFromServer, apliedQuery);
 
   return (
     <main className="section">
-      <h1 className="title">
-        {selectedPerson
-          ? `${selectedPerson.name} (${selectedPerson.born} = ${selectedPerson.died})`
-          : 'No selected person'}
-      </h1>
+      {choosedPerson ? (
+        <h1 className="title">
+          {`${choosedPerson.name} (${choosedPerson.born} = ${choosedPerson.died})`}
+        </h1>
+      ) : (
+        <h1 className="title">
+          No selected person
+        </h1>
+      )}
 
-      <AutoSuggest
-        delay={1000}
-        onSelected={handleSelected}
-      />
+      <div className="dropdown is-active">
+        <div className="dropdown-trigger control has-icons-right">
+          <input
+            onBlur={delayOnBlur}
+            onFocus={() => setVisible(true)}
+            onChange={event => handleOnChange(event)}
+            type="text"
+            placeholder="Enter a part of the name"
+            className="input"
+            value={query}
+          />
+          {choosedPerson && (
+            <span className="icon is-small is-right">
+              <button
+                onClick={handleReset}
+                type="button"
+                className="delete is-small"
+              >
+                x
+              </button>
+            </span>
+          )}
+        </div>
+
+        <div
+          className="dropdown-menu"
+          role="menu"
+          style={{
+            display: visible ? 'block' : 'none',
+          }}
+        >
+          <div className="dropdown-content">
+            {!preparedPeopleList.length ? (
+              <div className="dropdown-item">
+                <p>No matching suggestions</p>
+              </div>
+            ) : (
+              preparedPeopleList.map(person => (
+                <a
+                  href="/"
+                  onClick={event => handleOnClick(event, person)}
+                  className="dropdown-item"
+                  key={person.slug}
+                >
+                  <p
+                    className={classNames({
+                      'has-text-link': person.sex === 'm',
+                      'has-text-danger': person.sex === 'f',
+                    })}
+                  >
+                    {person.name}
+                  </p>
+                </a>
+              ))
+            )}
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
