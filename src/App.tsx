9cac35282024bc89fd5286e024883db785fe9b33
mykyta01 <mykyta.romansihyn@gmail.com>
2024-01-27 18:13:15 +0100

    add task solution 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,97 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo, MouseEvent } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
+import { DropItems } from './components/DropdownItems';
+import { Person } from './types/Person';
 
 export const App: React.FC = () => {
-  const peoples = peopleFromServer;
-  const [persons, setPersons] = useState('');
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
 
-  const handelChangePerson = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPersons(event.target.value);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const [query, setQuery] = React.useState('');
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
   };
 
-  const selectedPerson = peoples.find(people => people.name === persons);
+  const handleFocus = () => {
+    setIsOpen(true);
+  };
+
+  const handleBlur = () => {
+    setTimeout(() => setIsOpen(false), 100);
+  };
+
+  const handleReset = () => {
+    setSelectedPerson(null);
+    setQuery('');
+  };
+
+  const handleDropItemClick = (event: MouseEvent, person: Person) => {
+    event.preventDefault();
+
+    setSelectedPerson(person);
+    setQuery(person.name);
+  };
+
+  const sortedPeoples = useMemo(() => {
+    return peopleFromServer.filter(person => {
+      return person.name.toLowerCase().includes(query.toLowerCase());
+    });
+  }, [query]);
 
   return (
     <main className="section">
       <h1 className="title">
-        {selectedPerson ? `${selectedPerson.name} (${selectedPerson.born} = ${selectedPerson.died})` : 'No selected person'}
+        {selectedPerson ? (
+          `${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`
+        ) : ('No one selected')}
       </h1>
 
       <div className="dropdown is-active">
-        <div className="dropdown-trigger">
+        <div className="dropdown-trigger control has-icons-right">
           <input
-            value={persons}
             type="text"
             placeholder="Enter a part of the name"
             className="input"
-            list="select"
-            onChange={handelChangePerson}
+            value={query}
+            onChange={handleQueryChange}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
           />
-          <datalist id="select">
-            {peoples.map((people) => (
-              // eslint-disable-next-line jsx-a11y/control-has-associated-label
-              <option key={people.name} value={people.name} />
-            ))}
-          </datalist>
+          {query && (
+            <span className="icon is-small is-right">
+              <button
+                onClick={handleReset}
+                type="button"
+                className="delete is-small"
+              >
+                x
+              </button>
+            </span>
+          )}
         </div>
+
+        {isOpen ? (
+          <div className="dropdown-menu" role="menu">
+            <div className="dropdown-content">
+              {sortedPeoples.length ? (
+                sortedPeoples.map(person => (
+                  <DropItems
+                    person={person}
+                    key={person.slug}
+                    handleOnClick={handleDropItemClick}
+                  />
+                ))
+              ) : (
+                <div className="dropdown-item">
+                  <p>No matching suggestions</p>
+                </div>
+              )}
+            </div>
+          </div>
+        ) : ''}
       </div>
     </main>
   );
