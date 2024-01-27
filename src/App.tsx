03e2f7d105e31a84e7ff9357df0e15e5faa13b33
Bohdan Liutyi <liutyibohdan13@gmail.com>
2024-01-27 21:16:02 +0200

    list died

--- src/App.tsx
+++ src/App.tsx
@@ -1,127 +1,27 @@
-import React, { useCallback, useState } from 'react';
-import './App.scss';
-import classNames from 'classnames';
-import debounce from 'lodash.debounce';
-import { peopleFromServer } from './data/people';
+import React, { useState } from 'react';
 
+import './App.scss';
+import { peopleFromServer } from './data/people';
 import { Person } from './types/Person';
+import { AutoComplete } from './AutoComplete';
 
 export const App: React.FC = () => {
-  const [query, setQuery] = useState('');
-  const [apliedQuery, setApliedQuery] = useState('');
-  const [visible, setVisible] = useState(false);
-  const [choosedPerson, setchoosedPerson] = useState<Person | null>(null);
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const apllyQuery = useCallback(
-    debounce((value: string) => setApliedQuery(value), 1000),
-    [],
-  );
-
-  const handleOnClick
-    = (eventOnClick: React.MouseEvent, person: Person) => {
-      eventOnClick.preventDefault();
-
-      setQuery(person.name);
-      setApliedQuery(person.name);
-      setVisible(false);
-      setchoosedPerson(person);
-    };
-
-  const preparedList = (people: Person[], queryFilter: string) => {
-    const queryNormalized = queryFilter.toLocaleLowerCase();
-
-    return people.filter(person => person.name.toLocaleLowerCase()
-      .includes(queryNormalized));
-  };
-
-  const delayOnBlur = () => {
-    setTimeout(() => setVisible(false), 100);
-  };
-
-  const handleReset = () => {
-    setchoosedPerson(null);
-    setApliedQuery('');
-    setQuery('');
-  };
-
-  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setQuery(event.target.value);
-    apllyQuery(event.target.value);
-  };
-
-  const preparedPeopleList = preparedList(peopleFromServer, apliedQuery);
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+  const { name, born, died } = selectedPerson || {};
 
   return (
     <main className="section">
-      {choosedPerson ? (
-        <h1 className="title">
-          {`${choosedPerson.name} (${choosedPerson.born} = ${choosedPerson.died})`}
-        </h1>
-      ) : (
-        <h1 className="title">
-          No selected person
-        </h1>
-      )}
-
-      <div className="dropdown is-active">
-        <div className="dropdown-trigger control has-icons-right">
-          <input
-            onBlur={delayOnBlur}
-            onFocus={() => setVisible(true)}
-            onChange={event => handleOnChange(event)}
-            type="text"
-            placeholder="Enter a part of the name"
-            className="input"
-            value={query}
-          />
-          {choosedPerson && (
-            <span className="icon is-small is-right">
-              <button
-                onClick={handleReset}
-                type="button"
-                className="delete is-small"
-              >
-                x
-              </button>
-            </span>
-          )}
-        </div>
-
-        <div
-          className="dropdown-menu"
-          role="menu"
-          style={{
-            display: visible ? 'block' : 'none',
-          }}
-        >
-          <div className="dropdown-content">
-            {!preparedPeopleList.length ? (
-              <div className="dropdown-item">
-                <p>No matching suggestions</p>
-              </div>
-            ) : (
-              preparedPeopleList.map(person => (
-                <a
-                  href="/"
-                  onClick={event => handleOnClick(event, person)}
-                  className="dropdown-item"
-                  key={person.slug}
-                >
-                  <p
-                    className={classNames({
-                      'has-text-link': person.sex === 'm',
-                      'has-text-primary': person.sex === 'f',
-                    })}
-                  >
-                    {person.name}
-                  </p>
-                </a>
-              ))
-            )}
-          </div>
-        </div>
-      </div>
+      <h1 className="title">
+        {selectedPerson ? (
+          `${name} (${born} - ${died})`
+        ) : 'No selected person'}
+      </h1>
+      <AutoComplete
+        people={peopleFromServer}
+        timer={1000}
+        onSelect={setSelectedPerson}
+        selectPerson={name}
+      />
     </main>
   );
 };
---
+++ src/AutoComplete.tsx
@@ -0,0 +1,122 @@
+import React, { useCallback, useMemo, useState } from 'react';
+import classNames from 'classnames';
+import debounce from 'lodash.debounce';
+import { Person } from './types/Person';
+
+interface Props {
+  people: Person[],
+  timer?: number,
+  onSelect?: (person: Person | null) => void,
+  selectPerson?: string,
+}
+
+export const AutoComplete: React.FC<Props> = ({
+  people,
+  timer = 1000,
+  onSelect = () => { },
+  selectPerson,
+}) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const applyQuery = useCallback(
+    debounce(setAppliedQuery, timer),
+    [timer],
+  );
+
+  const filteredPeople = useMemo(() => {
+    return people.filter(p => {
+      return p.name.toLowerCase().includes(appliedQuery.toLowerCase());
+    });
+  }, [people, appliedQuery]);
+
+  const handlePersonSelect = (person: Person | null) => {
+    onSelect(person);
+    if (person) {
+      setQuery(person.name);
+    }
+  };
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value;
+
+    setIsFocused(false);
+    setQuery(inputValue);
+    applyQuery(inputValue);
+    setTimeout(() => {
+      setIsFocused(true);
+    }, timer);
+    if (inputValue.length === 0) {
+      handlePersonSelect(null);
+    }
+  };
+
+  const handleOnBlur = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (query || appliedQuery) {
+      setQuery('');
+      applyQuery('');
+      setAppliedQuery(event.target.value);
+      setTimeout(() => {
+        setIsFocused(false);
+      });
+    } else {
+      setTimeout(() => {
+        setIsFocused(false);
+      });
+    }
+  };
+
+  return (
+    <div className={classNames('dropdown', {
+      'is-active': isFocused,
+    })}
+    >
+      <div className="dropdown-trigger">
+        <input
+          type="text"
+          placeholder="Enter a part of the name"
+          className="input"
+          value={(query && query) || selectPerson}
+          onFocus={() => setIsFocused(true)}
+          onBlur={handleOnBlur}
+          onChange={handleQueryChange}
+        />
+      </div>
+
+      <div className="dropdown-menu" role="menu">
+        <div className="dropdown-content">
+          {filteredPeople.length
+            ? filteredPeople.map(person => {
+              const isWoman = person.sex === 'f';
+              const { name } = person;
+
+              return (
+                <a
+                  href="/#"
+                  className="dropdown-item"
+                  key={person.name}
+                  onMouseDown={() => handlePersonSelect(person)}
+                >
+                  <p className={classNames(
+                    isWoman
+                      ? 'has-text-primary'
+                      : 'has-text-link',
+                  )}
+                  >
+                    {name}
+                  </p>
+                </a>
+              );
+            })
+            : (
+              <div className="dropdown-item">
+                <p>No matching suggestions</p>
+              </div>
+            )}
+        </div>
+      </div>
+    </div>
+  );
+};
