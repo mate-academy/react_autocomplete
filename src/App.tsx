884194694577c412e936da3a836f2ea8b599e0e7
Inna Sheremet <innsheremet88@gmail.com>
2024-02-03 18:00:20 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,34 @@ import React, { useState } from 'react';
 import 'bulma';
 import { peopleFromServer } from './data/people';
 import { Autocomplete } from './components/Autocomplete';
+import { Person } from './types/Person';
 
 export const App: React.FC = () => {
   const [selectedPerson, setSelectedPerson] = useState<string | null>(null);
 
+  const findPerson = (people: Person[], selectPerson: string | null) => {
+    return people.find((person: Person) => person.name === selectPerson);
+  };
+
+  const renderPersonInfo = () => {
+    const foundPerson = findPerson(peopleFromServer, selectedPerson);
+
+    if (foundPerson) {
+      const personInfo = `${foundPerson.name} (${foundPerson.born} - ${foundPerson.died})`;
+
+      return (
+        <h1 className="title" key={selectedPerson}>
+          {personInfo}
+        </h1>
+      );
+    }
+
+    return null;
+  };
+
   return (
     <main className="section">
-      {selectedPerson && (
-        (() => {
-          const foundPerson = peopleFromServer.find(
-            (person) => person.name === selectedPerson,
-          );
-
-          if (foundPerson) {
-            return (
-              <h1 className="title" key={selectedPerson}>
-                {`${foundPerson.name} (${foundPerson.born} - ${foundPerson.died})`}
-              </h1>
-            );
-          }
-
-          return null;
-        })()
-      )}
+      {selectedPerson && renderPersonInfo()}
 
       {!selectedPerson && (
         <h1 className="title">
