a66a396d17664d0a2d5445bd81df75b1d2866f82
unknown <bohdan.lesyk.kn.2021@lpnu.ua>
2024-02-07 00:20:31 +0200

    like this??

--- src/App.tsx
+++ src/App.tsx
@@ -1,120 +1,15 @@
-import React, {
-  useState,
-  useRef,
-  useMemo,
-  useCallback,
-  useEffect,
-} from 'react';
-import { debounce } from 'lodash';
 import './App.scss';
-import { Person } from './types/Person';
-import { PeopleList } from './components/Autocomplete/Autocomplete';
+import React from 'react';
 import { peopleFromServer } from './data/people';
+import { Autocomplete } from './components/Autocomplete/Autocomplete';
 
 export const App: React.FC = () => {
-  const [query, setQuery] = useState('');
-  const [appliedQuery, setAppliedQuery] = useState('');
-  const [isVisible, setIsVisible] = useState(false);
-  const [title, setTitle] = useState('No selected person');
-  const [loading, setLoading] = useState(false);
-
-  const dropdownRef = useRef<HTMLDivElement>(null);
-
-  const filteredPeople = useMemo(() => {
-    return peopleFromServer
-      .filter(person => person?.name?.toLowerCase()
-        .includes(appliedQuery.toLowerCase()));
-  }, [appliedQuery]);
-
-  const delayQuery = useMemo(() => debounce((value: string) => {
-    setAppliedQuery(value.trim());
-  }, 1000), []);
-
-  const handleInputChange = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) => {
-      const value = event.target.value.trim();
-
-      setQuery(value);
-      setTitle('No selected person');
-      setLoading(true);
-      delayQuery(value);
-    }, [delayQuery],
-  );
-
-  const handleOnBlur = useCallback(() => {
-    setTimeout(() => {
-      if (dropdownRef.current
-        && !dropdownRef.current.contains(document.activeElement as Node)) {
-        setIsVisible(false);
-        setLoading(false);
-      }
-    }, 100);
-  }, []);
-
-  const handlePersonSelect = useCallback((person: Person | null) => {
-    if (person) {
-      setQuery(person.name);
-      setTitle(`${person.name} (${person.born} - ${person.died})`);
-    } else {
-      setQuery('');
-      setTitle('No selected person');
-    }
-
-    setIsVisible(false);
-    setLoading(false);
-  }, []);
-
-  useEffect(() => {
-    return () => {
-      delayQuery.cancel();
-    };
-  }, [delayQuery]);
-
-  useEffect(() => {
-    const fetchData = () => {
-      setAppliedQuery(query);
-      setLoading(true);
-      setTimeout(() => {
-        setLoading(false);
-      }, 1000);
-    };
-
-    fetchData();
-  }, [query]);
-
   return (
     <main className="section is-flex is-flex-direction-column">
-      <h1 className="title" data-cy="title">
-        {title}
-      </h1>
-
-      <div className="dropdown is-active" ref={dropdownRef}>
-        <div className="dropdown-trigger">
-          <input
-            type="text"
-            placeholder="Enter a part of the name"
-            className="input"
-            data-cy="search-input"
-            value={query}
-            onChange={handleInputChange}
-            onFocus={() => setIsVisible(true)}
-            onBlur={handleOnBlur}
-          />
-        </div>
-
-        {isVisible && !loading && (
-          <div
-            className="dropdown-menu"
-            role="menu"
-            data-cy="suggestions-list"
-          >
-            <PeopleList
-              onSelect={handlePersonSelect}
-              people={filteredPeople}
-            />
-          </div>
-        )}
-      </div>
+      <Autocomplete
+        people={peopleFromServer}
+        delay={1000}
+      />
     </main>
   );
 };
--- src/components/Autocomplete/Autocomplete.tsx
+++ src/components/Autocomplete/Autocomplete.tsx
@@ -1,59 +1,161 @@
-import { useState } from 'react';
+import React, {
+  useState,
+  useRef,
+  useMemo,
+  useCallback,
+  useEffect,
+} from 'react';
+import { debounce } from 'lodash';
 import clsx from 'clsx';
-import './Autocomplete.scss';
 import { Person } from '../../types/Person';
+import './Autocomplete.scss';
 
 type Props = {
   people: Person[];
-  onSelect: (person: Person | null) => void;
+  delay?: number;
 };
 
-export const PeopleList: React.FC<Props> = ({ people, onSelect }) => {
+export const Autocomplete: React.FC<Props> = ({ people, delay }) => {
   const [hoveredPerson, setHoveredPerson] = useState<Person | null>(null);
 
-  const handlePersonClick = (person: Person) => {
-    onSelect(person);
-  };
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
+  const [isVisible, setIsVisible] = useState(false);
+  const [title, setTitle] = useState('No selected person');
+  const [loading, setLoading] = useState(false);
+
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  const filteredPeople = useMemo(() => {
+    return people
+      .filter(person => person?.name?.toLowerCase()
+        .includes(appliedQuery.toLowerCase()));
+  }, [appliedQuery, people]);
+
+  const delayQuery = useMemo(() => debounce((value: string) => {
+    setAppliedQuery(value.trim());
+  }, delay), [delay]);
+
+  const handleInputChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const value = event.target.value.trim();
+
+      setQuery(value);
+      setTitle('No selected person');
+      setLoading(true);
+      delayQuery(value);
+    }, [delayQuery],
+  );
+
+  const handleOnBlur = useCallback(() => {
+    setTimeout(() => {
+      if (dropdownRef.current
+        && !dropdownRef.current.contains(document.activeElement as Node)) {
+        setIsVisible(false);
+        setLoading(false);
+      }
+    }, 100);
+  }, []);
+
+  const handlePersonSelect = useCallback((person: Person | null) => {
+    if (person) {
+      setQuery(person.name);
+      setTitle(`${person.name} (${person.born} - ${person.died})`);
+    } else {
+      setQuery('');
+      setTitle('No selected person');
+    }
+
+    setIsVisible(false);
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      delayQuery.cancel();
+    };
+  }, [delayQuery]);
+
+  useEffect(() => {
+    const fetchData = () => {
+      setAppliedQuery(query);
+      setLoading(true);
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000);
+    };
+
+    fetchData();
+  }, [query]);
 
   return (
-    <div className="dropdown-content">
-      {people.length === 0 ? (
-        <div
-          className="
+    <>
+      <h1 className="title" data-cy="title">
+        {title}
+      </h1>
+
+      <div className="dropdown is-active" ref={dropdownRef}>
+        <div className="dropdown-trigger">
+          <input
+            type="text"
+            placeholder="Enter a part of the name"
+            className="input"
+            data-cy="search-input"
+            value={query}
+            onChange={handleInputChange}
+            onFocus={() => setIsVisible(true)}
+            onBlur={handleOnBlur}
+          />
+        </div>
+
+        {isVisible && !loading && (
+          <div
+            className="dropdown-menu"
+            role="menu"
+            data-cy="suggestions-list"
+          >
+            <div className="dropdown-content">
+              {filteredPeople.length === 0 ? (
+                <div
+                  className="
             notification
             is-danger
             is-light
             mt-3
             is-align-self-flex-start
           "
-          role="alert"
-          data-cy="no-suggestions-message"
-        >
-          <p className="has-text-danger">No matching suggestions</p>
-        </div>
-      ) : (
-        people.map((person) => (
-          <div
-            key={person.slug}
-            className={clsx('dropdown-item', {
-              'hovered': hoveredPerson === person,
-            })}
-            data-cy="suggestion-item"
-            role="button"
-            tabIndex={0}
-            onClick={() => handlePersonClick(person)}
-            onMouseEnter={() => setHoveredPerson(person)}
-            onMouseLeave={() => setHoveredPerson(null)}
-            onKeyDown={(event) => {
-              if (event.key === 'Enter' || event.key === ' ') {
-                handlePersonClick(person);
-              }
-            }}
-          >
-            <p className="has-text-link">{person.name}</p>
+                  role="alert"
+                  data-cy="no-suggestions-message"
+                >
+                  <p className="has-text-danger">No matching suggestions</p>
+                </div>
+              ) : (
+                filteredPeople.map((person) => (
+                  <div
+                    key={person.slug}
+                    className={clsx('dropdown-item', {
+                      hovered: hoveredPerson === person,
+                    })}
+                    data-cy="suggestion-item"
+                    role="button"
+                    tabIndex={0}
+                    onClick={() => handlePersonSelect(person)}
+                    onMouseEnter={() => setHoveredPerson(person)}
+                    onMouseLeave={() => setHoveredPerson(null)}
+                    onKeyDown={(event) => {
+                      if (event.key === 'Enter' || event.key === ' ') {
+                        handlePersonSelect(person);
+                      }
+                    }}
+                  >
+                    <p className="has-text-link">{person.name}</p>
+                  </div>
+                ))
+              )}
+            </div>
           </div>
-        ))
-      )}
-    </div>
+        )}
+      </div>
+    </>
   );
 };
