d0636b78ab7e823e3aa2beea5a75d16d86b8e09c
Dmytro Pivkopa <alien_dima@yahoo.com>
2024-02-10 11:53:55 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,54 @@
-import React from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import './App.scss';
+import { PeopleList } from './Components/PeopleList/PeopleList';
 import { peopleFromServer } from './data/people';
+// import { debounce } from 'cypress/types/lodash';
+
+function debounce(
+  callback: React.Dispatch<React.SetStateAction<string>>,
+  delay: number,
+) {
+  let timerId = 0;
+
+  return (args: string) => {
+    window.clearTimeout(timerId);
+
+    timerId = window.setTimeout(() => {
+      callback(args);
+    }, delay);
+  };
+}
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [query, setQuery] = useState('');
+  const [selectedPerson, setSelectedPerson] = useState(peopleFromServer[0]);
+  const [showList, setShowList] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  const [isSelected, setIsSelected] = useState(false);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const applyQuery = useCallback(
+    debounce(setQuery, 300), [],
+  );
+
+  const handleQueryChange = (personName: string) => {
+    applyQuery(personName);
+    setInputValue(personName);
+  };
+
+  const machedPeople = useMemo(() => {
+    return peopleFromServer.filter((person) => {
+      return person.name.includes(query);
+    });
+  }, [query]);
 
   return (
     <div className="container">
       <main className="section is-flex is-flex-direction-column">
         <h1 className="title" data-cy="title">
-          {`${name} (${born} - ${died})`}
+          {isSelected ? (
+            `${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`
+          ) : 'No selected person'}
         </h1>
 
         <div className="dropdown is-active">
@@ -19,80 +58,46 @@ export const App: React.FC = () => {
               placeholder="Enter a part of the name"
               className="input"
               data-cy="search-input"
+              onChange={(event) => {
+                handleQueryChange(event.target.value);
+                setIsSelected(false);
+              }}
+              onFocus={() => {
+                setShowList(true);
+              }}
+              onBlur={() => {
+                setTimeout(() => {
+                  setShowList(false);
+                }, 100);
+              }}
+              value={inputValue}
             />
           </div>
 
-          <div
-            className="dropdown-menu"
-            role="menu"
-            data-cy="suggestions-list"
-          >
-            <div className="dropdown-content">
-              <div
-                className="dropdown-item"
-                data-cy="suggestion-item"
-              >
-                <p className="has-text-link">Pieter Haverbeke</p>
-              </div>
-
-              <div
-                className="dropdown-item"
-                data-cy="suggestion-item"
-              >
-                <p className="has-text-link">Pieter Bernard Haverbeke</p>
-              </div>
-
-              <div
-                className="dropdown-item"
-                data-cy="suggestion-item"
-              >
-                <p className="has-text-link">Pieter Antone Haverbeke</p>
-              </div>
-
-              <div
-                className="dropdown-item"
-                data-cy="suggestion-item"
-              >
-                <p className="has-text-danger">Elisabeth Haverbeke</p>
-              </div>
-
-              <div
-                className="dropdown-item"
-                data-cy="suggestion-item"
-              >
-                <p className="has-text-link">Pieter de Decker</p>
-              </div>
-
-              <div
-                className="dropdown-item"
-                data-cy="suggestion-item"
-              >
-                <p className="has-text-danger">Petronella de Decker</p>
-              </div>
-
-              <div
-                className="dropdown-item"
-                data-cy="suggestion-item"
-              >
-                <p className="has-text-danger">Elisabeth Hercke</p>
-              </div>
-            </div>
-          </div>
+          {showList && machedPeople.length > 0 ? (
+            <PeopleList
+              setSelectedPerson={setSelectedPerson}
+              setInputValue={setInputValue}
+              setIsSelected={setIsSelected}
+            />
+          ) : ''}
         </div>
 
-        <div
-          className="
+        {machedPeople.length === 0 ? (
+          <div
+            className="
             notification
             is-danger
             is-light
             mt-3
             is-align-self-flex-start
           "
-          role="alert"
-          data-cy="no-suggestions-message"
-        >
-          <p className="has-text-danger">No matching suggestions</p>
-        </div>
+            role="alert"
+            data-cy="no-suggestions-message"
+          >
+            <p className="has-text-danger">No matching suggestions</p>
+          </div>
+        ) : ('')}
       </main>
     </div>
   );
