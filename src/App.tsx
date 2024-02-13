88d39100f4fd01cbcecab0291729887c74f58364
mykhalenych <mykhalenych@gmail.com>
2024-02-13 15:19:59 +0200

    feature/prettier added prettier, updated packages

--- src/App.tsx
+++ src/App.tsx
@@ -22,58 +22,33 @@ export const App: React.FC = () => {
             />
           </div>
 
-          <div
-            className="dropdown-menu"
-            role="menu"
-            data-cy="suggestions-list"
-          >
+          <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
             <div className="dropdown-content">
-              <div
-                className="dropdown-item"
-                data-cy="suggestion-item"
-              >
+              <div className="dropdown-item" data-cy="suggestion-item">
                 <p className="has-text-link">Pieter Haverbeke</p>
               </div>
 
-              <div
-                className="dropdown-item"
-                data-cy="suggestion-item"
-              >
+              <div className="dropdown-item" data-cy="suggestion-item">
                 <p className="has-text-link">Pieter Bernard Haverbeke</p>
               </div>
 
-              <div
-                className="dropdown-item"
-                data-cy="suggestion-item"
-              >
+              <div className="dropdown-item" data-cy="suggestion-item">
                 <p className="has-text-link">Pieter Antone Haverbeke</p>
               </div>
 
-              <div
-                className="dropdown-item"
-                data-cy="suggestion-item"
-              >
+              <div className="dropdown-item" data-cy="suggestion-item">
                 <p className="has-text-danger">Elisabeth Haverbeke</p>
               </div>
 
-              <div
-                className="dropdown-item"
-                data-cy="suggestion-item"
-              >
+              <div className="dropdown-item" data-cy="suggestion-item">
                 <p className="has-text-link">Pieter de Decker</p>
               </div>
 
-              <div
-                className="dropdown-item"
-                data-cy="suggestion-item"
-              >
+              <div className="dropdown-item" data-cy="suggestion-item">
                 <p className="has-text-danger">Petronella de Decker</p>
               </div>
 
-              <div
-                className="dropdown-item"
-                data-cy="suggestion-item"
-              >
+              <div className="dropdown-item" data-cy="suggestion-item">
                 <p className="has-text-danger">Elisabeth Hercke</p>
               </div>
             </div>
