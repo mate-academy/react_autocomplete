1e4cd28c5d80a4ffcdb7f8623d3fc4307b07f8e6
alona <alionazhv@gmail.com>
2024-02-22 07:52:53 +0100

    The App component was fized

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React, { useState } from 'react';
 import './App.scss';
+import { useState } from 'react';
 import { peopleFromServer } from './data/people';
 import { Person } from './types/Person';
-import { Autocomplete } from './Autocomplete';
+import { AutoComplete } from './Components/Autocomplete/Autocomplete';
 
 export const App: React.FC = () => {
   const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
-  const handleSelect = (person: Person | null) => {
+
+  const { name, born, died } = selectedPerson || peopleFromServer[0];
+
+  const handleSelectedPerson = (person: Person | null) => {
     setSelectedPerson(person);
   };
 
-  const pageTitle = selectedPerson
-    ? `${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`
+  const selectPerson = selectedPerson
+    ? `${name} (${born} - ${died})`
     : 'No selected person';
 
   return (
     <div className="container">
       <main className="section is-flex is-flex-direction-column">
         <h1 className="title" data-cy="title">
-          {pageTitle}
+          {selectPerson}
         </h1>
-        <Autocomplete onSelect={handleSelect} people={peopleFromServer} />
+
+        <AutoComplete onSelected={handleSelectedPerson} />
       </main>
     </div>
   );
