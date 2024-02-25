aa509e918a638cea54cc057f5d927eb436d85eed
Mykhailo Hubko <mixelios@gmail.com>
2024-02-25 17:24:44 +0100

    i need help

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+import debounce from 'lodash.debounce';
+import classNames from 'classnames';
 import './App.scss';
 import { peopleFromServer } from './data/people';
 
+type Person = {
+  name: string;
+  sex: string;
+  born: number;
+  died: number;
+  fatherName: string | null;
+  motherName: string | null;
+  slug: string;
+};
+
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const people = [...peopleFromServer];
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
+  const [isFocused, setIsFocused] = useState(false);
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+
+  const handleSelect = (item: Person) => {
+    setSelectedPerson(item);
+    setQuery('');
+  };
+
+  const handleKeyPress = (event: React.KeyboardEvent, item: Person) => {
+    if (event.key === 'Enter' || event.key === ' ') {
+      handleSelect(item);
+    }
+  };
+
+  const handleFocused = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    setTimeout(() => {
+      setIsFocused(false);
+    }, 300);
+  };
+
+  const applyQuery = debounce(setAppliedQuery, 300);
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+    applyQuery(event.target.value);
+  };
+
+  const getFilteredPeople = (): Person[] => {
+    const filteredPeople: Person[] = people.filter(item => {
+      return item.name.includes(appliedQuery);
+    });
+
+    return filteredPeople;
+  };
+
+  const preperedPeople: Person[] = getFilteredPeople();
 
   return (
     <div className="container">
       <main className="section is-flex is-flex-direction-column">
-        <h1 className="title" data-cy="title">
-          {`${name} (${born} - ${died})`}
-        </h1>
+        {selectedPerson ? (
+          <h1 className="title" data-cy="title">
+            {`${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`}
+          </h1>
+        ) : (
+          <h1 className="title" data-cy="title">
+            No selected person
+          </h1>
+        )}
 
         <div className="dropdown is-active">
           <div className="dropdown-trigger">
@@ -19,50 +79,62 @@ export const App: React.FC = () => {
               placeholder="Enter a part of the name"
               className="input"
               data-cy="search-input"
+              value={query}
+              onFocus={handleFocused}
+              onBlur={handleBlur}
+              onChange={handleQueryChange}
             />
           </div>
 
-          <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
-            <div className="dropdown-content">
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Bernard Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Antone Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Petronella de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Hercke</p>
-              </div>
+          <div
+            className={classNames('dropdown-menu', {
+              'is-hidden': !isFocused || preperedPeople.length === 0,
+            })}
+            role="menu"
+            data-cy="suggestions-list"
+          >
+            <div
+              className={classNames('dropdown-content', {
+                'is-hidden': !isFocused,
+              })}
+            >
+              {preperedPeople.map((item, index) => {
+                return (
+                  <div
+                    role="button"
+                    className="dropdown-item"
+                    data-cy="suggestion-item"
+                    key={item.slug}
+                    tabIndex={index}
+                    onKeyDown={(event: React.KeyboardEvent) => {
+                      handleKeyPress(event, item);
+                    }}
+                    onClick={() => {
+                      handleSelect(item);
+                    }}
+                  >
+                    <p
+                      className={classNames('text', {
+                        'has-text-link': item.sex === 'm',
+                        'has-text-danger': item.sex === 'f',
+                      })}
+                    >
+                      {item.name}
+                    </p>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
 
         <div
-          className="
-            notification
-            is-danger
-            is-light
-            mt-3
-            is-align-self-flex-start
-          "
+          className={classNames(
+            'notification is-danger is-light mt-3 is-align-self-flex-start',
+            {
+              'is-hidden': preperedPeople.length !== 0,
+            },
+          )}
           role="alert"
           data-cy="no-suggestions-message"
         >
