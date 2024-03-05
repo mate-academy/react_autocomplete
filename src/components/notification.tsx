77fa7f0c91f20eb290ba179598464595182788c5
Roman Tarasov <rtarasov1k90@gmail.com>
2024-03-05 10:32:45 +0200

    spaces

--- src/components/notification.tsx
+++ src/components/notification.tsx
@@ -1,12 +1,12 @@
 export const Notifications: React.FC = () => (
   <div
     className="
-  notification
-  is-danger
-  is-light
-  mt-3
-  is-align-self-flex-start
-"
+      notification
+      is-danger
+      is-light
+      mt-3
+      is-align-self-flex-start
+    "
     role="alert"
     data-cy="no-suggestions-message"
   >
