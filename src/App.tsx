055d6352d41537dea53a4bb1dee30fa2ea9cb7d7
mrsvolodya <vovamursky@gmail.com>
2024-03-10 14:33:15 +0200

    autocomplete-dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,108 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import debaunce from 'lodash.debounce';
 import './App.scss';
 import { peopleFromServer } from './data/people';
+import { Person } from './types/Person';
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [person, setPerson] = useState<Person>();
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
+  const [focused, setFocused] = useState(false);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const appliedQueryDebounce = useCallback(debaunce(setAppliedQuery, 300), []);
+
+  const filteredInputs = peopleFromServer.filter(p => {
+    return (
+      p.name.toLowerCase().includes(appliedQuery.toLowerCase()) &&
+      p.name.length !== appliedQuery.length
+    );
+  });
+  const noMatching = filteredInputs.length === 0;
+
+  const handlerQueryInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+    appliedQueryDebounce(e.target.value);
+    const chosenPeople = peopleFromServer.find(p => p.name === e.target.value);
+
+    setPerson(chosenPeople);
+  };
+
+  const handlerPointQuery = (user: Person) => {
+    setQuery(user.name);
+    appliedQueryDebounce(user.name);
+    const chosenPeople = peopleFromServer.find(p => p.name === user.name);
+
+    setPerson(chosenPeople);
+  };
 
   return (
     <div className="container">
       <main className="section is-flex is-flex-direction-column">
         <h1 className="title" data-cy="title">
-          {`${name} (${born} - ${died})`}
+          {!person
+            ? 'No selected person'
+            : ` ${person?.name} (${person?.born} - ${person?.died})`}
         </h1>
 
-        <div className="dropdown is-active">
+        <div className="dropdown is-active is-hoverable">
           <div className="dropdown-trigger">
             <input
+              onFocus={() => setFocused(true)}
               type="text"
               placeholder="Enter a part of the name"
               className="input"
               data-cy="search-input"
+              value={query}
+              onChange={handlerQueryInput}
             />
           </div>
-
-          <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
-            <div className="dropdown-content">
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Bernard Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Antone Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Petronella de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Hercke</p>
+          {focused && (
+            <div
+              className="dropdown-menu"
+              role="menu"
+              data-cy="suggestions-list"
+            >
+              <div className="dropdown-content">
+                {filteredInputs.map(user => (
+                  <div
+                    className="dropdown-item"
+                    data-cy="suggestion-item"
+                    key={user.name}
+                  >
+                    <p
+                      className="has-text-link"
+                      onKeyDown={() => {
+                        handlerPointQuery(user);
+                      }}
+                      onClick={() => {
+                        handlerPointQuery(user);
+                      }}
+                    >
+                      {user.name}
+                    </p>
+                  </div>
+                ))}
               </div>
             </div>
-          </div>
+          )}
         </div>
 
-        <div
-          className="
+        {noMatching && (
+          <div
+            className="
             notification
             is-danger
             is-light
             mt-3
             is-align-self-flex-start
           "
-          role="alert"
-          data-cy="no-suggestions-message"
-        >
-          <p className="has-text-danger">No matching suggestions</p>
-        </div>
+            role="alert"
+            data-cy="no-suggestions-message"
+          >
+            <p className="has-text-danger">No matching suggestions</p>
+          </div>
+        )}
       </main>
     </div>
   );
