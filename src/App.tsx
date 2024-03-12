79945647dff63fa912a5c1fe97fa0988faf29bb6
volodymyr <volodiayurovych@gmail.com>
2024-03-12 23:36:38 +0100

    check

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,92 @@
-import React from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import './App.scss';
+import classNames from 'classnames';
+import debounce from 'lodash.debounce';
 import { peopleFromServer } from './data/people';
+import { Person } from './types/Person';
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const notSelectedPerson: Person = {
+    name: '',
+    sex: 'm',
+    born: 0,
+    died: 0,
+    fatherName: '',
+    motherName: '',
+    slug: '',
+  };
+  const [onSelected, setOnSelected] = useState(notSelectedPerson);
+  const { name, born, died } = onSelected;
+  const [isShowed, setIsShowed] = useState(false);
+  const [currentInput, setCurrentInput] = useState('');
+  const [instantInoutValue, setInstantInoutValue] = useState('');
+  const [delay] = useState(300);
+
+  const applyCurrentInput = useCallback(debounce(setCurrentInput, delay), []);
+
+  const handleInputValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+    applyCurrentInput(event.target.value);
+    setInstantInoutValue(event.target.value);
+    setOnSelected(notSelectedPerson);
+  };
+
+  const handleSelectedPerson = (person: Person) => {
+    setInstantInoutValue(person.name);
+    setOnSelected(person);
+  };
+
+  const filteredPeople = useMemo(() => {
+    return peopleFromServer.filter(person =>
+      person.name.toLowerCase().includes(currentInput.toLowerCase()),
+    );
+  }, [currentInput]);
 
   return (
     <div className="container">
       <main className="section is-flex is-flex-direction-column">
         <h1 className="title" data-cy="title">
-          {`${name} (${born} - ${died})`}
+          {name === '' ? 'No selected person' : `${name} (${born} - ${died})`}
         </h1>
 
-        <div className="dropdown is-active">
+        <div className={classNames('dropdown', isShowed && 'is-active')}>
           <div className="dropdown-trigger">
             <input
+              value={instantInoutValue}
               type="text"
               placeholder="Enter a part of the name"
               className="input"
               data-cy="search-input"
+              onChange={handleInputValue}
+              onFocus={() => setIsShowed(true)}
+              onBlur={() => {
+                setTimeout(() => {
+                  setIsShowed(false);
+                }, 100);
+              }}
             />
           </div>
 
           <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
             <div className="dropdown-content">
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Bernard Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Antone Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Petronella de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Hercke</p>
-              </div>
+              {filteredPeople.map(person => (
+                <div
+                  key={person.name}
+                  className="dropdown-item"
+                  data-cy="suggestion-item"
+                  data-person={person}
+                  onClick={() => handleSelectedPerson(person)}
+                  aria-hidden="true"
+                  style={{ cursor: 'pointer' }}
+                >
+                  <p
+                    className={
+                      person.sex === 'm' ? 'has-text-link' : 'has-text-danger'
+                    }
+                  >
+                    {person.name}
+                  </p>
+                </div>
+              ))}
             </div>
           </div>
         </div>
@@ -66,7 +102,9 @@ export const App: React.FC = () => {
           role="alert"
           data-cy="no-suggestions-message"
         >
-          <p className="has-text-danger">No matching suggestions</p>
+          <p className="has-text-danger">
+            {filteredPeople.length === 0 && 'No matching suggestions'}
+          </p>
         </div>
       </main>
     </div>
