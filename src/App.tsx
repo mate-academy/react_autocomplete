4a030358ec1be2496faf6b8b6e65826ddbd044d4
Olha Shvets <lv160888soi@gmail.com>
2024-03-13 16:11:24 +0200

    fixed h1

--- src/App.tsx
+++ src/App.tsx
@@ -14,13 +14,11 @@ export const App: React.FC = () => {
   return (
     <div className="container">
       <main className="section is-flex is-flex-direction-column">
-        {selectedPerson && (
-          <h1 className="title" data-cy="title">
-            {selectedPerson
-              ? `${selectedPerson?.name} (${selectedPerson?.born} - ${selectedPerson?.died})`
-              : 'No selected person'}
-          </h1>
-        )}
+        <h1 className="title" data-cy="title">
+          {selectedPerson
+            ? `${selectedPerson?.name} (${selectedPerson?.born} - ${selectedPerson?.died})`
+            : 'No selected person'}
+        </h1>
 
         <Autocomplete people={peopleFromServer} onSelect={onSelected} />
       </main>
