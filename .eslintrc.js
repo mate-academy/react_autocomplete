ff7b08b187f80d3dbdb6239baee881b1fa3d92a8
Misha Grinko <glukym@gmail.com>
2024-03-15 18:50:01 +0200

    fix node14 deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,9 @@
 module.exports = {
-  extends: ['@mate-academy/eslint-config-react-typescript', 'plugin:cypress/recommended'],
+  extends: [
+    '@mate-academy/eslint-config-react-typescript',
+    'plugin:cypress/recommended',
+  ],
   rules: {
-    'max-len': ['error', {
-      ignoreTemplateLiterals: true,
-      ignoreComments: true,
-    }],
-    'jsx-a11y/label-has-associated-control': ["error", {
-      assert: "either",
-    }],
+    'react/display-name': 0
   },
 };
