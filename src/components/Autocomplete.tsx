5352dde2d8a9b7ab2085763f92c24ec378fed69f
Denis Hurskiy <denis.gurskiy@gmail.com>
2024-03-15 22:00:22 +0100

    Implement Autocomplete

---
+++ src/components/Autocomplete.tsx
@@ -0,0 +1,117 @@
+import { useCallback, useRef, useState } from 'react';
+import debounce from 'lodash.debounce';
+
+import './Autocomplete.scss';
+import { Person } from '../types/Person';
+
+type Props = {
+  people: Person[];
+  onSelect?: (person: Person | null) => void;
+  delay: number;
+};
+
+export const Autocomplete: React.FC<Props> = ({
+  people,
+  onSelect = () => {},
+  delay,
+}) => {
+  const queryField = useRef<HTMLInputElement | null>(null);
+
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
+  const [focusInput, setFocusInput] = useState(false);
+
+  const applyQuery = useCallback(debounce(setAppliedQuery, delay), []);
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    onSelect(null);
+    setQuery(event.target.value);
+    applyQuery(event.target.value);
+  };
+
+  const filteredPeople = people.filter(person => {
+    return person.name.toLowerCase().includes(appliedQuery.toLowerCase());
+  });
+
+  const handlerSelectPerson = (person: Person) => {
+    setQuery(person.name);
+    onSelect(person);
+    setFocusInput(false);
+  };
+
+  const handlerClearQuery = () => {
+    onSelect(null);
+    setQuery('');
+    setAppliedQuery('');
+    if (queryField.current) {
+      queryField.current.focus();
+    }
+  };
+
+  return (
+    <div className="dropdown is-active">
+      <div className="dropdown-trigger">
+        <input
+          data-cy="search-input"
+          ref={queryField}
+          type="text"
+          placeholder="Enter a part of the name"
+          className="input"
+          value={query}
+          onChange={handleQueryChange}
+          onFocus={() => setFocusInput(true)}
+        />
+        {query && (
+          <button
+            type="button"
+            className="delete is-medium dropdown-close"
+            onClick={handlerClearQuery}
+          >
+            x
+          </button>
+        )}
+      </div>
+
+      {focusInput && (
+        <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
+          <div className="dropdown-content">
+            {filteredPeople.length !== 0 ? (
+              filteredPeople.map((person: Person) => (
+                <div
+                  role="button"
+                  tabIndex={0}
+                  key={person.name}
+                  className="dropdown-item"
+                  data-cy="suggestion-item"
+                  onClick={() => handlerSelectPerson(person)}
+                  onKeyDown={event => {
+                    if (event.key === 'Enter' || event.key === ' ') {
+                      event.preventDefault();
+                      handlerSelectPerson(person);
+                    }
+                  }}
+                >
+                  <p className="has-text-link">{person.name}</p>
+                </div>
+              ))
+            ) : (
+              <div
+                className="
+                  notification
+                  is-danger
+                  is-light
+                  mt-3
+                  is-align-self-flex-start
+                "
+                role="alert"
+                data-cy="no-suggestions-message"
+              >
+                <p className="has-text-grey-lighter">No matching suggestions</p>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
