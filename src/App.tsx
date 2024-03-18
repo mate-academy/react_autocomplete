89bd224499165e24309680b42241f85272b9f2c1
Andrew Rich <andy.rich3a@gmail.com>
2024-03-18 23:03:07 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,43 @@
-import React from 'react';
-import './App.scss';
+import React, { useMemo, useState } from 'react';
+import debounce from 'lodash.debounce';
 import { peopleFromServer } from './data/people';
+import { Person } from './types/Person';
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+  const [query, setQuery] = useState('');
+  const [isDropdownActive, setIsDropdownActive] = useState(false);
+
+  const filteredPeople = useMemo(() => {
+    const normalisedQuery = query.toLowerCase().trim();
+
+    return peopleFromServer.filter(person => {
+      const normalisedPerson = person.name.toLowerCase();
+
+      return normalisedPerson.includes(normalisedQuery);
+    });
+  }, [query]);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+    setSelectedPerson(null);
+    setIsDropdownActive(!!event.target.value || event.type === 'focus');
+    debounce(() => setIsDropdownActive(true), 300)();
+  };
+
+  const handleSuggestionClick = (person: Person) => {
+    setQuery(person.name);
+    setSelectedPerson(person);
+    setIsDropdownActive(false);
+  };
 
   return (
     <div className="container">
       <main className="section is-flex is-flex-direction-column">
         <h1 className="title" data-cy="title">
-          {`${name} (${born} - ${died})`}
+          {selectedPerson
+            ? `${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`
+            : 'No selected person'}
         </h1>
 
         <div className="dropdown is-active">
@@ -19,56 +47,54 @@ export const App: React.FC = () => {
               placeholder="Enter a part of the name"
               className="input"
               data-cy="search-input"
+              value={query}
+              onChange={handleInputChange}
+              onFocus={handleInputChange}
             />
           </div>
 
           <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
-            <div className="dropdown-content">
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Haverbeke</p>
+            {isDropdownActive && (
+              <div className="dropdown-content">
+                {filteredPeople.map(person => (
+                  <div
+                    className="dropdown-item"
+                    data-cy="suggestion-item"
+                    key={person.name}
+                    role="menuitem"
+                    tabIndex={0}
+                    onClick={() => handleSuggestionClick(person)}
+                    onKeyDown={e => {
+                      if (e.key === 'Enter' || e.key === ' ') {
+                        handleSuggestionClick(person);
+                      }
+                    }}
+                  >
+                    <p className="has-text-link">{person.name}</p>
+                  </div>
+                ))}
+                {!filteredPeople.length && (
+                  <div
+                    className="
+                      notification
+                      is-danger
+                      is-light
+                      mt-3
+                      is-align-self-flex-start
+                    "
+                    role="alert"
+                    data-cy="no-suggestions-message"
+                  >
+                    <p className="has-text-danger">No matching suggestions</p>
+                  </div>
+                )}
               </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Bernard Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Antone Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Petronella de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Hercke</p>
-              </div>
-            </div>
+            )}
           </div>
-        </div>
-
-        <div
-          className="
-            notification
-            is-danger
-            is-light
-            mt-3
-            is-align-self-flex-start
-          "
-          role="alert"
-          data-cy="no-suggestions-message"
-        >
-          <p className="has-text-danger">No matching suggestions</p>
         </div>
       </main>
     </div>
   );
 };
+
+export default App;
