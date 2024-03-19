0f6a8299aa042f60dab66fde29d165f0ff84f01f
Anastasiia Vasylieva <anastasia.vasylieva97@gmail.com>
2024-03-19 14:03:07 +0100

    autocomplition done

---
+++ src/Dropdown.tsx
@@ -0,0 +1,108 @@
+import { useMemo, useState } from 'react';
+import { Person } from './types/Person';
+import { debounce } from './debounce';
+import classNames from 'classnames';
+
+interface DropdownType {
+  people: Person[];
+  onSelected: (person: Person | null) => void;
+  delay?: number;
+}
+
+export const Dropdown: React.FC<DropdownType> = ({
+  people,
+  onSelected,
+  delay = 300,
+}) => {
+  const [query, setQuery] = useState('');
+  const [isInputShown, setIsInputShown] = useState(false);
+
+  const filteredPeople = useMemo(() => {
+    return people.filter(person => {
+      const normalizedQuery = query.toLowerCase().trim();
+
+      return person.name.toLowerCase().includes(normalizedQuery);
+    });
+  }, [people, query]);
+
+  const applyName = useMemo(
+    () => debounce(() => setIsInputShown(true), delay),
+    [delay],
+  );
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    applyName();
+
+    setQuery(event.target.value);
+    setIsInputShown(false);
+    onSelected(null);
+  };
+
+  const handleOnBlurChange = () => {
+    setTimeout(() => {
+      setIsInputShown(false);
+    }, delay);
+  };
+
+  const handleSelectPerson = (person: Person) => {
+    onSelected(person);
+    setQuery(person.name);
+  };
+
+  return (
+    <>
+      <div
+        className={classNames('dropdown', {
+          'is-active': isInputShown,
+        })}
+      >
+        <div className="dropdown-trigger">
+          <input
+            type="text"
+            placeholder="Enter a part of the name"
+            className="input"
+            data-cy="search-input"
+            value={query}
+            onChange={handleOnChange}
+            onFocus={() => setIsInputShown(true)}
+            onBlur={handleOnBlurChange}
+          />
+        </div>
+
+        <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
+          {filteredPeople.length > 0 && (
+            <div className="dropdown-content">
+              {filteredPeople.map(person => (
+                <button
+                  key={person.slug}
+                  className="dropdown-item button is-white"
+                  type="button"
+                  data-cy="suggestion-item"
+                  onClick={() => handleSelectPerson(person)}
+                >
+                  <p className="has-text-link">{person.name}</p>
+                </button>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+
+      {filteredPeople.length === 0 && (
+        <div
+          className="
+            notification
+            is-danger
+            is-light
+            mt-3
+            is-align-self-flex-start
+          "
+          role="alert"
+          data-cy="no-suggestions-message"
+        >
+          <p className="has-text-danger">No matching suggestions</p>
+        </div>
+      )}
+    </>
+  );
+};
---
+++ src/debounce.ts
@@ -0,0 +1,11 @@
+export function debounce<T extends (...args: any[]) => void>(callback: T, delay: number) {
+  let timerId = 0;
+
+  return (...args: Parameters<T>) => {
+    window.clearTimeout(timerId);
+
+    timerId = window.setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+}
