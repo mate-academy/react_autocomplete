6209257be90c767ec1279e638e203a292d72649d
TetianaBukhenko <tania04bukhenko@gmail.com>
2024-03-20 23:58:41 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,65 @@
-import React from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
+import { Dropdowns } from './Components/Dropdowns';
+import { Person } from './types/Person';
+
+function debounce(
+  callback: React.Dispatch<React.SetStateAction<string>>,
+  delay: number
+) {
+  let timerId = 0;
+
+  return (query: string) => {
+    window.clearTimeout(timerId);
+
+    timerId = window.setTimeout(() => {
+      callback(query);
+    }, delay);
+  };
+}
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+  const [displayOptions, setDisplayOptions] = useState(false);
+
+  const applyQuerty = useCallback(debounce(setAppliedQuery, 1000), []);
+
+  const handleQuertyChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+    applyQuerty(event.target.value);
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    handleQuertyChange(event); setSelectedPerson(null)
+  };
+
+
+  const displayedSuggestions = useMemo(() => {
+    return peopleFromServer.filter(person => {
+      const normalizedName = person.name.toLowerCase();
+      const normalizedQuery = appliedQuery.toLowerCase();
+
+      return normalizedName.includes(normalizedQuery);
+    });
+  }, [appliedQuery]);
+
+  const hasSuggestions = displayedSuggestions.length >= 1;
 
   return (
     <div className="container">
       <main className="section is-flex is-flex-direction-column">
-        <h1 className="title" data-cy="title">
-          {`${name} (${born} - ${died})`}
-        </h1>
+        {selectedPerson ? (
+          <h1 className="title" data-cy="title">
+            {`${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`}
+          </h1>
+        ) : (
+          <h1 className="title" data-cy="title">
+            No selected person
+          </h1>
+        )}
 
         <div className="dropdown is-active">
           <div className="dropdown-trigger">
@@ -19,54 +68,35 @@ export const App: React.FC = () => {
               placeholder="Enter a part of the name"
               className="input"
               data-cy="search-input"
+              value={selectedPerson?.name || query}
+              onChange={handleInputChange}
+              onFocus={() => setDisplayOptions(true)}
             />
           </div>
 
           <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
-            <div className="dropdown-content">
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Bernard Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Antone Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Petronella de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Hercke</p>
-              </div>
-            </div>
+            {displayOptions &&
+              (hasSuggestions ? (
+                <Dropdowns
+                  suggestions={displayedSuggestions}
+                  setSelectedPerson={setSelectedPerson}
+                />
+              ) : (
+                <div
+                  className="
+                    notification
+                    is-danger
+                    is-light
+                    mt-3
+                    is-align-self-flex-start
+                    "
+                  role="alert"
+                  data-cy="no-suggestions-message"
+                >
+                  <p className="has-text-danger">No matching suggestions</p>
+                </div>
+              ))}
           </div>
-        </div>
-
-        <div
-          className="
-            notification
-            is-danger
-            is-light
-            mt-3
-            is-align-self-flex-start
-          "
-          role="alert"
-          data-cy="no-suggestions-message"
-        >
-          <p className="has-text-danger">No matching suggestions</p>
         </div>
       </main>
     </div>
