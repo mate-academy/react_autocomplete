9e3a76ff56ba0c2d8af1dbd580298defd66b7561
TetianaBukhenko <tania04bukhenko@gmail.com>
2024-03-21 00:31:41 +0200

    improved task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,9 @@
 import React, { useCallback, useMemo, useState } from 'react';
 import './App.scss';
-import { peopleFromServer } from './data/people';
 import { Dropdowns } from './Components/Dropdowns';
 import { Person } from './types/Person';
-
-function debounce(
-  callback: React.Dispatch<React.SetStateAction<string>>,
-  delay: number
-) {
-  let timerId = 0;
-
-  return (query: string) => {
-    window.clearTimeout(timerId);
-
-    timerId = window.setTimeout(() => {
-      callback(query);
-    }, delay);
-  };
-}
+import { ErrorMessage } from './Components/ErrorMessage';
+import { debounce, filterPeople } from './functions';
 
 export const App: React.FC = () => {
   const [query, setQuery] = useState('');
@@ -27,23 +13,20 @@ export const App: React.FC = () => {
 
   const applyQuerty = useCallback(debounce(setAppliedQuery, 1000), []);
 
+  //#region handleChange
   const handleQuertyChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(event.target.value);
     applyQuerty(event.target.value);
   };
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    handleQuertyChange(event); setSelectedPerson(null)
+    handleQuertyChange(event);
+    setSelectedPerson(null);
   };
-
+  //#endregion
 
   const displayedSuggestions = useMemo(() => {
-    return peopleFromServer.filter(person => {
-      const normalizedName = person.name.toLowerCase();
-      const normalizedQuery = appliedQuery.toLowerCase();
-
-      return normalizedName.includes(normalizedQuery);
-    });
+    return filterPeople(appliedQuery);
   }, [appliedQuery]);
 
   const hasSuggestions = displayedSuggestions.length >= 1;
@@ -76,25 +59,14 @@ export const App: React.FC = () => {
 
           <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
             {displayOptions &&
+              !selectedPerson &&
               (hasSuggestions ? (
                 <Dropdowns
                   suggestions={displayedSuggestions}
                   setSelectedPerson={setSelectedPerson}
                 />
               ) : (
-                <div
-                  className="
-                    notification
-                    is-danger
-                    is-light
-                    mt-3
-                    is-align-self-flex-start
-                    "
-                  role="alert"
-                  data-cy="no-suggestions-message"
-                >
-                  <p className="has-text-danger">No matching suggestions</p>
-                </div>
+                <ErrorMessage />
               ))}
           </div>
         </div>
