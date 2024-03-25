9bee6cb527ad75327a4a72857dfb036c2e037f8d
Ruslan Ogerenko <ruslanogerenko@gmail.com>
2024-03-26 01:51:45 +0200

    solved the task

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,61 @@
-import React from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
+import cn from 'classnames';
+import { Person } from './types/Person';
+import debounce from 'lodash.debounce';
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+
+  const [hoveredItem, setHoveredItem] = useState<string | null>(null);
+
+  const [listIsVisible, setListIsVisible] = useState(false);
+
+  const [newPeople, setNewPeople] = useState<Person[]>([]);
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
+
+  const filteredPeople = useMemo(() => {
+    return newPeople.filter(people =>
+      people.name.toLowerCase().includes(appliedQuery.toLowerCase().trim()),
+    );
+  }, [appliedQuery, newPeople]);
+
+  const applyQuery = useCallback(debounce(setAppliedQuery, 300), []);
+
+  const handleOnQuery = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+    applyQuery(event.target.value);
+    setSelectedPerson(null);
+  };
+
+  const handleOnFocus = () => {
+    setNewPeople(peopleFromServer);
+    setListIsVisible(true);
+  };
+
+  const handleMouseEnter = (itemName: string) => {
+    setHoveredItem(itemName);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredItem(null);
+  };
+
+  const handleOnSelected = (person: Person) => {
+    setSelectedPerson(person);
+    setQuery(person.name);
+    setListIsVisible(false);
+  };
 
   return (
     <div className="container">
-      <main className="section is-flex is-flex-direction-column">
+      <main className="dropdown section is-flex is-flex-direction-column">
         <h1 className="title" data-cy="title">
-          {`${name} (${born} - ${died})`}
+          {selectedPerson
+            ? `${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`
+            : 'No selected person'}
         </h1>
 
         <div className="dropdown is-active">
@@ -19,54 +65,64 @@ export const App: React.FC = () => {
               placeholder="Enter a part of the name"
               className="input"
               data-cy="search-input"
+              value={query}
+              onChange={handleOnQuery}
+              onFocus={handleOnFocus}
             />
           </div>
 
-          <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
-            <div className="dropdown-content">
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Bernard Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Antone Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Petronella de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Hercke</p>
+          {listIsVisible && (
+            <div
+              className="dropdown-menu"
+              role="menu"
+              data-cy="suggestions-list"
+            >
+              <div className="dropdown-content">
+                {filteredPeople.map(person => (
+                  <div
+                    key={person.name}
+                    className="dropdown-item"
+                    data-cy="suggestion-item"
+                    onMouseEnter={() => handleMouseEnter(person.name)}
+                    onMouseLeave={handleMouseLeave}
+                    style={{
+                      backgroundColor:
+                        hoveredItem === person.name ? 'lightblue' : 'white',
+                    }}
+                    onClick={() => handleOnSelected(person)}
+                  >
+                    <p
+                      className={cn(
+                        {
+                          'has-text-link': person.sex === 'm',
+                        },
+                        {
+                          'has-text-danger': person.sex === 'f',
+                        },
+                      )}
+                    >
+                      {person.name}
+                    </p>
+                  </div>
+                ))}
+                {filteredPeople.length === 0 && (
+                  <div
+                    className="
+                      notification
+                      is-danger
+                      is-light
+                      mt-3
+                      is-align-self-flex-start
+                      "
+                    role="alert"
+                    data-cy="no-suggestions-message"
+                  >
+                    <p className="has-text-danger">No matching suggestions</p>
+                  </div>
+                )}
               </div>
             </div>
-          </div>
-        </div>
-
-        <div
-          className="
-            notification
-            is-danger
-            is-light
-            mt-3
-            is-align-self-flex-start
-          "
-          role="alert"
-          data-cy="no-suggestions-message"
-        >
-          <p className="has-text-danger">No matching suggestions</p>
+          )}
         </div>
       </main>
     </div>
