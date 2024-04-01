832e38de32af2f1c84a436d5e059590c509adece
Hlib Vakhrameiev <g.vakhrameev@icloud.com>
2024-04-01 17:22:34 +0300

    add autocomplete component

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,13 @@
-import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
-import debounce from 'lodash.debounce';
-import classNames from 'classnames';
 import { Person } from './types/Person';
+import { Autocomplete } from './component/Autocomplete';
+
+const DELAY = 300;
 
 export const App: React.FC = () => {
   const [selectedPerson, setSelectedPeople] = useState<Person | null>(null);
-  const [openList, setOpenList] = useState(false);
-
-  const [search, setSearch] = useState('');
-  const [request, setRequest] = useState('');
-  const delay = 300;
-
-  const showPeople = useMemo(() => {
-    return peopleFromServer.filter(people =>
-      people.name.toLowerCase().includes(request.toLowerCase().trim()),
-    );
-  }, [request]);
-
-  const applySearch = useMemo(() => debounce(setRequest, delay), [setRequest]);
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-    applySearch(e.target.value);
-    setOpenList(true);
-    setSelectedPeople(null);
-  };
-
-  const handleSelectedPeople = (people: Person) => {
-    setSelectedPeople(people);
-    setSearch(people.name);
-    setOpenList(false);
-  };
-
-  const handleCancelValue = () => {
-    setSearch('');
-    setSelectedPeople(null);
-  };
 
   return (
     <div className="container">
@@ -48,68 +18,12 @@ export const App: React.FC = () => {
             : 'No selected person'}
         </h1>
 
-        <div
-          className={classNames('dropdown', {
-            'is-active': openList,
-          })}
-        >
-          <div className="dropdown-trigger">
-            <input
-              type="text"
-              value={search}
-              onChange={handleChange}
-              placeholder="Enter a part of the name"
-              className="input"
-              data-cy="search-input"
-              onFocus={() => setOpenList(true)}
-            />
-            {search === selectedPerson?.name && (
-              <button
-                className="delete is-small"
-                onClick={() => handleCancelValue()}
-              ></button>
-            )}
-          </div>
-
-          <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
-            <div className="dropdown-content">
-              {showPeople.map(people => (
-                <div
-                  className="dropdown-item"
-                  data-cy="suggestion-item"
-                  key={people.name}
-                  onClick={() => handleSelectedPeople(people)}
-                >
-                  <p
-                    className={classNames('person', {
-                      'has-text-link': people.sex === 'm',
-                      'has-text-danger': people.sex === 'f',
-                      'is-selected': people.name === selectedPerson?.name,
-                    })}
-                  >
-                    {people.name}
-                  </p>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-
-        {showPeople.length === 0 && (
-          <div
-            className="
-            notification
-            is-danger
-            is-light
-            mt-3
-            is-align-self-flex-start
-          "
-            role="alert"
-            data-cy="no-suggestions-message"
-          >
-            <p className="has-text-danger">No matching suggestions</p>
-          </div>
-        )}
+        <Autocomplete
+          onSelected={setSelectedPeople}
+          delay={DELAY}
+          people={peopleFromServer}
+          selectedPerson={selectedPerson}
+        />
       </main>
     </div>
   );
---
+++ src/component/Autocomplete.tsx
@@ -0,0 +1,121 @@
+import React, { useMemo, useState } from 'react';
+import debounce from 'lodash.debounce';
+import classNames from 'classnames';
+import './Autocomplete.scss';
+import { Person } from '../types/Person';
+
+interface AutocompleteType {
+  people: Person[];
+  onSelected: (person: Person | null) => void;
+  delay?: number;
+  selectedPerson: Person | null;
+}
+
+export const Autocomplete: React.FC<AutocompleteType> = ({
+  onSelected,
+  people,
+  delay,
+  selectedPerson,
+}) => {
+  const [openList, setOpenList] = useState(false);
+
+  const [search, setSearch] = useState('');
+  const [request, setRequest] = useState('');
+
+  const shownPeople = useMemo(() => {
+    return people.filter(person =>
+      person.name.toLowerCase().includes(request.toLowerCase().trim()),
+    );
+  }, [request, people]);
+
+  const applySearch = useMemo(
+    () => debounce(setRequest, delay),
+    [setRequest, delay],
+  );
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+    applySearch(e.target.value);
+    setOpenList(true);
+    onSelected(null);
+  };
+
+  const handleSelectedPeople = (person: Person) => {
+    setSearch(person.name);
+    onSelected(person);
+    setOpenList(false);
+  };
+
+  const handleCancelValue = () => {
+    setSearch('');
+    applySearch('');
+    onSelected(null);
+  };
+
+  return (
+    <>
+      <div
+        className={classNames('dropdown', {
+          'is-active': openList,
+        })}
+      >
+        <div className="dropdown-trigger">
+          <input
+            type="text"
+            value={search}
+            onChange={handleChange}
+            placeholder="Enter a part of the name"
+            className="input"
+            data-cy="search-input"
+            onFocus={() => setOpenList(true)}
+            onBlur={() => setOpenList(false)}
+          />
+          {search === selectedPerson?.name && (
+            <button
+              className="delete is-small"
+              onClick={() => handleCancelValue()}
+            ></button>
+          )}
+        </div>
+
+        <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
+          <div className={classNames({ 'dropdown-content': false })}>
+            {shownPeople.map(person => (
+              <div
+                className="dropdown-item"
+                data-cy="suggestion-item"
+                key={person.name}
+                onMouseDown={() => handleSelectedPeople(person)}
+              >
+                <p
+                  className={classNames('person', {
+                    'has-text-link': person.sex === 'm',
+                    'has-text-danger': person.sex === 'f',
+                    'is-selected': person.name === selectedPerson?.name,
+                  })}
+                >
+                  {person.name}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      {shownPeople.length === 0 && (
+        <div
+          className="
+    notification
+    is-danger
+    is-light
+    mt-3
+    is-align-self-flex-start
+  "
+          role="alert"
+          data-cy="no-suggestions-message"
+        >
+          <p className="has-text-danger">No matching suggestions</p>
+        </div>
+      )}
+    </>
+  );
+};
