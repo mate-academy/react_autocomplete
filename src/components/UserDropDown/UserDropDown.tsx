278afe7d9b41cb640b77f8eb81d2aa33a473ffd9
Denys209818 <deniskravcuk63@gmail.com>
2024-04-02 14:50:22 +0300

    add small fixes

--- src/components/UserDropDown/UserDropDown.tsx
+++ src/components/UserDropDown/UserDropDown.tsx
@@ -9,28 +9,32 @@ interface DropDownType {
 export const UserDropDown: React.FC<DropDownType> = React.memo(
   ({ users, onSelected = () => {} }) => {
     return (
-      <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
-        <div className="dropdown-content">
-          {users.map(item => {
-            return (
-              <a
-                key={item.name + Math.round(Math.random() * 1000) / 1000}
-                className="dropdown-item"
-                onClick={(
-                  e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
-                ) => {
-                  e.preventDefault();
-                  e.stopPropagation();
-                  onSelected(item);
-                }}
-                data-cy="suggestion-item"
-              >
-                {item.name}
-              </a>
-            );
-          })}
-        </div>
-      </div>
+      <>
+        {users && users.length > 0 && (
+          <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
+            <div className="dropdown-content">
+              {users.map(item => {
+                return (
+                  <a
+                    key={item.name + Math.round(Math.random() * 1000) / 1000}
+                    className="dropdown-item"
+                    onClick={(
+                      e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+                    ) => {
+                      e.preventDefault();
+                      e.stopPropagation();
+                      onSelected(item);
+                    }}
+                    data-cy="suggestion-item"
+                  >
+                    {item.name}
+                  </a>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </>
     );
   },
 );
