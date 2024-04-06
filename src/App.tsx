74722f51beae073ee6609a36a4b4500e1ae73b71
MaximAV <atanasovmaksim56@gmail.com>
2024-04-06 17:22:34 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,60 @@
-import React from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
+import classNames from 'classnames';
+import debounce from 'lodash.debounce';
+import { Person } from './types/Person';
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const noUser = {
+    name: 'No selected person',
+    born: null,
+    died: null,
+  };
+
+  const [people, setPeople] = useState(peopleFromServer);
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+  const { name, born, died } = selectedPerson || noUser;
+  const [query, setQuery] = useState('');
+  const [timedQuery, setTimedQuery] = useState('');
+  const [isFocused, setIsFocused] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const timeOutQuery = useCallback(debounce(setTimedQuery, 1000), []);
+
+  const onChangeHandle = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setQuery(event.target.value);
+      timeOutQuery(event.target.value);
+    },
+    [],
+  );
+
+  useMemo(() => {
+    const sortedPeople = peopleFromServer.filter(person =>
+      person.name.toLowerCase().includes(timedQuery.toLowerCase().trim()),
+    );
+
+    setPeople(sortedPeople);
+  }, [timedQuery]);
+
+  useEffect(() => {
+    if (people.length === 0) {
+      setIsError(true);
+    } else {
+      setIsError(false);
+    }
+  }, [timedQuery]);
+
+  // debounce();
 
   return (
     <div className="container">
       <main className="section is-flex is-flex-direction-column">
         <h1 className="title" data-cy="title">
-          {`${name} (${born} - ${died})`}
+          {born === null && died === null
+            ? `${name}`
+            : `${name} (${born} - ${died})`}
         </h1>
 
         <div className="dropdown is-active">
@@ -19,50 +64,45 @@ export const App: React.FC = () => {
               placeholder="Enter a part of the name"
               className="input"
               data-cy="search-input"
+              onFocus={() => setIsFocused(true)}
+              onBlur={() => setIsFocused(false)}
+              onChange={onChangeHandle}
+              value={query}
             />
           </div>
 
-          <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
+          <div
+            className={classNames('dropdown-menu', {
+              'display-none': !isFocused || isError,
+            })}
+            role="menu"
+            data-cy="suggestions-list"
+          >
             <div className="dropdown-content">
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Haverbeke</p>
-              </div>
+              {people.map(person => (
+                <div
+                  style={{ cursor: 'pointer' }}
+                  key={`${person.name}-${Math.round(Math.random() * 100) / 100}`}
+                  className="dropdown-item"
+                  data-cy="suggestion-item"
+                  onMouseEnter={() => setSelectedPerson(person)}
+                >
+                  <p className="has-text-link">{person.name}</p>
+                </div>
+              ))}
 
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Bernard Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Antone Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Petronella de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
+              {/* <div className="dropdown-item" data-cy="suggestion-item">
                 <p className="has-text-danger">Elisabeth Hercke</p>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
 
         <div
-          className="
-            notification
-            is-danger
-            is-light
-            mt-3
-            is-align-self-flex-start
-          "
+          className={classNames(
+            'notification is-danger is-light mt-3 is-align-self-flex-start',
+            { 'display-none': !isError },
+          )}
           role="alert"
           data-cy="no-suggestions-message"
         >
