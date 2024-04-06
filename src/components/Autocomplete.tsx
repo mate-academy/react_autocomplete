ddf74345c94d7c7f6fde8f3076495a270c9be92e
Dmytro Kravchenko <dmytro318@gmail.com>
2024-04-06 22:02:43 +0300

    initial commot

---
+++ src/components/Autocomplete.tsx
@@ -0,0 +1,111 @@
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { Person } from '../types/Person';
+import { peopleFromServer } from '../data/people';
+import debounce from 'lodash.debounce';
+
+type Props = {
+  onSelected: (person: Person | null) => void;
+  delay?: number;
+};
+
+export const Autocomplete: React.FC<Props> = ({ onSelected, delay = 300 }) => {
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
+  const [listVisibility, setListVisibility] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
+
+  const applyDebouncedQuery = useCallback(
+    debounce((value: string) => {
+      setAppliedQuery(value);
+      setListVisibility(true);
+    }, delay),
+    [],
+  );
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    applyDebouncedQuery(event.target.value);
+    setQuery(event.target.value);
+    setListVisibility(false);
+  };
+
+  const handleListClick = (selectedPerson: Person) => {
+    setAppliedQuery(selectedPerson.name);
+    setQuery(selectedPerson.name);
+    onSelected(selectedPerson);
+    setListVisibility(false);
+  };
+
+  const handleOutsideClick = (event: MouseEvent) => {
+    if (
+      dropdownRef.current &&
+      !dropdownRef.current.contains(event.target as Node)
+    ) {
+      setListVisibility(false);
+    }
+  };
+
+  useEffect(() => {
+    if (listVisibility) {
+      document.addEventListener('mousedown', handleOutsideClick);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, [listVisibility]);
+
+  const filteredPeople = useMemo(() => {
+    return peopleFromServer.filter(person =>
+      person.name.toLowerCase().includes(appliedQuery.toLowerCase().trim()),
+    );
+  }, [appliedQuery]);
+
+  return (
+    <div className="dropdown is-active" ref={dropdownRef}>
+      <div className="dropdown-trigger">
+        <input
+          type="text"
+          placeholder="Enter a part of the name"
+          className="input"
+          data-cy="search-input"
+          value={query}
+          onChange={handleInputChange}
+          onFocus={() => setListVisibility(true)}
+        />
+      </div>
+
+      <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
+        {listVisibility && (
+          <div className="dropdown-content">
+            {filteredPeople.length ? (
+              filteredPeople.map((person: Person) => (
+                <div
+                  className="dropdown-item"
+                  data-cy="suggestion-item"
+                  key={person.name}
+                  onClick={() => handleListClick(person)}
+                >
+                  <p className="has-text-link">{person.name}</p>
+                </div>
+              ))
+            ) : (
+              <div
+                className="
+                  notification
+                  is-danger
+                  is-light
+                  mt-3
+                  is-align-self-flex-start
+                "
+                role="alert"
+                data-cy="no-suggestions-message"
+              >
+                <p className="has-text-danger">No matching suggestions</p>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
