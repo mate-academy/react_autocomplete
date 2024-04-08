4bc874d1040d17cafb1232b89751b5fb8169f45f
Max Petrashishin <rikogjdefolt@gmail.com>
2024-04-08 12:43:51 +0300

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,83 +1,47 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
-import { Person } from './types/Person';
 import { Autocomplete } from './components/Autocomplete';
-
-// eslint-disable-next-line @typescript-eslint/ban-types
-function debounse(callback: Function, delay: number) {
-  let timerId = 0;
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return (...args: any) => {
-    window.clearTimeout(timerId);
-
-    timerId = window.setTimeout(() => {
-      callback(...args);
-    }, delay);
-  };
-}
 
 export const App: React.FC = () => {
   const delay = 300;
-  const clearPerson = {
+
+  const [activePerson, setActivePerson] = useState({
     name: '',
     born: 0,
     died: 0,
-  };
-  const [people] = useState(peopleFromServer);
-  const [searchPeople, setSearchPeople] = useState('');
-  const [apliedPeople, setApliedPeople] = useState('');
-  const [usePeople, setUsePeople] = useState(false);
-  const [activePerson, setActivePerson] = useState(clearPerson);
-  const { name, born, died } = activePerson;
-  const handleUsePeople = () => {
-    setUsePeople(true);
-  };
+  });
 
-  const handleClickUser = (person: Person) => {
-    setUsePeople(false);
-    setSearchPeople(person.name);
-    setApliedPeople(person.name);
+  const [personIsActive, setPersonIsActive] = useState(false);
+
+  const handleSetPerson = (n: string, b: number, d: number) => {
     setActivePerson({
-      name: person.name,
-      born: person.born,
-      died: person.died,
+      name: n,
+      born: b,
+      died: d,
     });
-  };
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const applyPeople = useCallback(debounse(setApliedPeople, delay), []);
-
-  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchPeople(e.target.value);
-    applyPeople(e.target.value);
-
-    if (e.target.value !== name) {
-      setActivePerson(clearPerson);
+    if (!n) {
+      setPersonIsActive(false);
+    } else {
+      setPersonIsActive(true);
     }
   };
 
-  const peopleFilter = useMemo(() => {
-    return people.filter(it =>
-      it.name.toLowerCase().includes(apliedPeople.toLowerCase()),
-    );
-  }, [people, apliedPeople]);
+  const { name, born, died } = activePerson;
 
   return (
     <div className="container">
       <main className="section is-flex is-flex-direction-column">
         <h1 className="title" data-cy="title">
-          {name ? `${name} (${born} - ${died})` : 'No selected person'}
+          {personIsActive
+            ? `${name} (${born} - ${died})`
+            : 'No selected person'}
         </h1>
 
         <Autocomplete
-          searchPeople={searchPeople}
-          handleSearchChange={handleSearchChange}
-          handleUsePeople={handleUsePeople}
-          usePeople={usePeople}
-          peopleFilter={peopleFilter}
-          handleClickUser={handleClickUser}
+          people={peopleFromServer}
+          delay={delay}
+          onSelect={handleSetPerson}
         />
       </main>
     </div>
--- src/components/Autocomplete.tsx
+++ src/components/Autocomplete.tsx
@@ -1,23 +1,62 @@
-import React from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import { Person } from '../types/Person';
 
+// eslint-disable-next-line @typescript-eslint/ban-types
+function debounse(callback: Function, delay: number) {
+  let timerId = 0;
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return (...args: any) => {
+    window.clearTimeout(timerId);
+
+    timerId = window.setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+}
+
 type Props = {
-  searchPeople: string;
-  handleSearchChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  handleUsePeople: () => void;
-  usePeople: boolean;
-  peopleFilter: Person[];
-  handleClickUser: (person: Person) => void;
+  people: Person[];
+  delay: number;
+  onSelect: (n: string, b: number, d: number) => void;
 };
 
-export const Autocomplete: React.FC<Props> = ({
-  searchPeople,
-  handleSearchChange,
-  handleUsePeople,
-  usePeople,
-  peopleFilter,
-  handleClickUser,
-}) => {
+export const Autocomplete: React.FC<Props> = ({ people, delay, onSelect }) => {
+  const [searchPeople, setSearchPeople] = useState('');
+  const [apliedPeople, setApliedPeople] = useState('');
+  const [usePeople, setUsePeople] = useState(false);
+  let name = '';
+
+  const handleUsePeople = () => {
+    setUsePeople(true);
+  };
+
+  const handleClickUser = (person: Person) => {
+    setUsePeople(false);
+    setSearchPeople(person.name);
+    setApliedPeople(person.name);
+    onSelect(person.name, person.born, person.died);
+    name = person.name;
+  };
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const applyPeople = useCallback(debounse(setApliedPeople, delay), []);
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchPeople(e.target.value);
+    applyPeople(e.target.value);
+
+    if (e.target.value !== name) {
+      onSelect('', 0, 0);
+    }
+  };
+
+  const peopleFilter = useMemo(() => {
+    return people.filter(it =>
+      it.name.toLowerCase().includes(apliedPeople.toLowerCase()),
+    );
+  }, [apliedPeople, people]);
+
   return (
     <div className="dropdown is-active">
       <div className="dropdown-trigger">
