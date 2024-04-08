8b291a60e475d69bbc2f5fcfa1c55d036df1a676
Bohdan Shyrii <shyrii11bohdan@gmail.com>
2024-04-08 13:46:54 +0200

    cope

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,22 @@ import './App.scss';
 import { peopleFromServer } from './data/people';
 import { Person } from './types/Person';
 
-export const App: React.FC = () => {
-  const [query, setQuery] = React.useState('');
-  const [isDropedDown, setIsDropedDown] = React.useState(false);
-  const [selectedPerson, setSelectedPerson] = React.useState<Person | null>(
-    null,
-  );
+type State = {
+  query: string;
+  selectedPerson: Person | null;
+  isDropedDown: boolean;
+};
 
-  const { name, born, died } = selectedPerson || {};
+export class App extends React.Component {
+  state: State = {
+    query: '',
+    selectedPerson: null,
+    isDropedDown: false,
+  };
 
-  const getFilterPeople = (): Person[] => {
+  getFilterPeople = (): Person[] => {
+    const { query } = this.state;
+
     if (!query) {
       return peopleFromServer;
     }
@@ -22,120 +28,126 @@ export const App: React.FC = () => {
     );
   };
 
-  const handlerBlur = () => {
+  handlerBlur = () => {
     setTimeout(() => {
-      setIsDropedDown(false);
+      this.setState({ isDropedDown: false });
     }, 300);
   };
 
-  const handlerOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  handlerOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
 
-    setQuery(value);
+    this.setState({ query: value });
 
     if (!value) {
-      setSelectedPerson(null);
+      this.setState({ selectedPerson: null });
     }
   };
 
-  const handlerClear = () => {
-    setSelectedPerson(null);
-    setQuery('');
+  handlerClear = () => {
+    this.setState({
+      selectedPerson: null,
+      query: '',
+    });
   };
 
-  return (
-    <div className="container">
-      <main className="section is-flex is-flex-direction-column">
-        {name ? (
-          <h1 className="title" data-cy="title">
-            {`${name} (${born} - ${died})`}
-          </h1>
-        ) : (
-          <h1 className="title" data-cy="title">
-            No selected person
-          </h1>
-        )}
+  render() {
+    const { query, selectedPerson } = this.state;
+    const { name, born, died } = selectedPerson || {};
 
-        <div className="dropdown is-active">
-          <div className="dropdown-trigger">
-            <input
-              type="text"
-              placeholder="Enter a part of the name"
-              className="input"
-              data-cy="search-input"
-              onFocus={() => setIsDropedDown(true)}
-              onBlur={handlerBlur}
-              value={query}
-              onChange={handlerOnChange}
-            />
-          </div>
+    return (
+      <div className="container">
+        <main className="section is-flex is-flex-direction-column">
+          {name ? (
+            <h1 className="title" data-cy="title">
+              {`${name} (${born} - ${died})`}
+            </h1>
+          ) : (
+            <h1 className="title" data-cy="title">
+              No selected person
+            </h1>
+          )}
 
-          <div className="control">
-            <button className="button is-info" onClick={handlerClear}>
-              Clear
-            </button>
-          </div>
+          <div className="dropdown is-active">
+            <div className="dropdown-trigger">
+              <input
+                type="text"
+                placeholder="Enter a part of the name"
+                className="input"
+                data-cy="search-input"
+                onFocus={() => this.setState({ isDropedDown: true })}
+                onBlur={this.handlerBlur}
+                value={query}
+                onChange={this.handlerOnChange}
+              />
+            </div>
 
-          <div
-            className="dropdown-menu custom-dropdown-menu"
-            role="menu"
-            data-cy="suggestions-list"
-          >
-            {isDropedDown &&
-              getFilterPeople().map(person => (
-                <div
-                  className="
-                  dropdown-content
-                  custom-dropdown-item
-                  "
-                  key={person.name}
-                  onClick={() => {
-                    setQuery(person.name);
-                    setIsDropedDown(false);
-                    setSelectedPerson(person);
-                  }}
-                  style={{
-                    cursor: 'pointer',
-                  }}
-                >
-                  <div className="dropdown-item" data-cy="suggestion-item">
-                    <p className="has-text-link">{person.name}</p>
+            <div className="control">
+              <button className="button is-info" onClick={this.handlerClear}>
+                Clear
+              </button>
+            </div>
+
+            <div
+              className="dropdown-menu custom-dropdown-menu"
+              role="menu"
+              data-cy="suggestions-list"
+            >
+              {this.state.isDropedDown &&
+                this.getFilterPeople().map(person => (
+                  <div
+                    className="dropdown-content custom-dropdown-item"
+                    key={person.name}
+                    onClick={() => {
+                      this.setState({
+                        query: person.name,
+                        isDropedDown: false,
+                        selectedPerson: person,
+                      });
+                    }}
+                    style={{
+                      cursor: 'pointer',
+                    }}
+                  >
+                    <div className="dropdown-item" data-cy="suggestion-item">
+                      <p className="has-text-link">{person.name}</p>
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
+            </div>
           </div>
-        </div>
 
-        {getFilterPeople().length === 0 && (
-          <div
-            className="
+          {this.getFilterPeople().length === 0 && (
+            <div
+              className="
               notification
               is-danger
               is-light
               mt-3
               is-align-self-flex-start"
-            role="alert"
-            data-cy="no-suggestions-message"
-          >
-            <p className="has-text-danger">No matching suggestions</p>
-          </div>
-        )}
+              role="alert"
+              data-cy="no-suggestions-message"
+            >
+              <p className="has-text-danger">No matching suggestions</p>
+            </div>
+          )}
 
-        {selectedPerson && selectedPerson && getFilterPeople().length > 0 && (
-          <div
-            className="
-                  notification
-                  is-info
-                  is-light
-                  mt-3
-                  is-align-self-flex-start"
-            role="alert"
-            data-cy="no-suggestions-message"
-          >
-            <p className="has-text-info">Person founded</p>
-          </div>
-        )}
-      </main>
-    </div>
-  );
-};
+          {selectedPerson && this.getFilterPeople().length > 0 && (
+            <div
+              className="
+              notification
+              is-info
+              is-light
+              mt-3
+              is-align-self-flex-start"
+              role="alert"
+              data-cy="no-suggestions-message"
+            >
+              <p className="has-text-info">Person founded</p>
+            </div>
+          )}
+        </main>
+      </div>
+    );
+  }
+}
