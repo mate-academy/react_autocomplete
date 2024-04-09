36e0ea0d4fe034660296e8bbe3bf9c9c7622b6d9
Viktoria Moroz <viktoriaandriivna25@gmail.com>
2024-04-09 09:40:12 +0300

    task need to fix2

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,25 @@
-import React, { useCallback } from 'react';
 import './App.scss';
-import { peopleFromServer } from './data/people';
-import { useState, useMemo } from 'react';
 import { Dropdown } from './components/dropdown_menu/Dropdown-menu';
+import { Person } from './types/Person';
+import { useState } from 'react';
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
 
-  const [query, setQuery] = useState('');
-  const [appliedQuery, setAppliedQuery] = useState('');
-
-  const filterPeople = useMemo(() => {
-    return peopleFromServer.filter(person =>
-      person.name.toLowerCase().includes(appliedQuery),
-    );
-  }, [appliedQuery]);
-
-  const debounce = (
-    callback: (...args: React.ChangeEvent<HTMLInputElement>[]) => void,
-    delay = 300,
-  ) => {
-    let timerId = 0;
-
-    return (...args: React.ChangeEvent<HTMLInputElement>[]) => {
-      window.clearTimeout(timerId);
-
-      timerId = window.setTimeout(() => {
-        callback(...args);
-      }, delay);
-    };
-  };
-
-  const applyQuery = debounce(setAppliedQuery, 1000);
-
-  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    applyQuery(event.target.value);
-    setQuery(event.target.value);
-  };
+  const title = selectedPerson
+    ? `${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`
+    : 'No selected person';
 
   return (
     <div className="container">
       <main className="section is-flex is-flex-direction-column">
         <h1 className="title" data-cy="title">
-          {`${name} (${born} - ${died})`}
+          {title}
         </h1>
         <Dropdown
-          debounce={debounce}
-          filterPeople={filterPeople}
-          query={query}
-          handleQueryChange={handleQueryChange}
+          onSelected={setSelectedPerson}
+          selectedPerson={selectedPerson}
         />
-        <div
-          className="
-            notification
-            is-danger
-            is-light
-            mt-3
-            is-align-self-flex-start
-          "
-          role="alert"
-          data-cy="no-suggestions-message"
-        >
-          <p className="has-text-danger">No matching suggestions</p>
-        </div>
       </main>
     </div>
   );
--- src/components/dropdown_menu/Dropdown-menu.tsx
+++ src/components/dropdown_menu/Dropdown-menu.tsx
@@ -1,58 +1,93 @@
 import { Person } from '../../types/Person';
-import { useState } from 'react';
+import { useCallback, useState, useMemo } from 'react';
+import { peopleFromServer } from '../../data/people';
 
 type Props = {
-  filterPeople: Person[];
-  query: string;
-  handleQueryChange: React.ChangeEventHandler<HTMLInputElement>;
-  delay?: number;
-  debounce: (
-    callback: (...args: React.ChangeEvent<HTMLInputElement>[]) => void,
-    delay: number,
-  ) => () => void;
+  onSelected: (person: Person | null) => void;
+  selectedPerson: Person | null;
 };
 
-export const Dropdown: React.FC<Props> = ({
-  filterPeople,
-  query,
-  handleQueryChange,
-  debounce,
-  //delay = 300,
-}) => {
+export const Dropdown: React.FC<Props> = ({ onSelected, selectedPerson }) => {
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
   const [isOpen, setIsOpen] = useState(false);
 
-  const handleInputFocus = () => {
-    setIsOpen(true);
+  const filterPeople = useMemo(() => {
+    return peopleFromServer.filter(person =>
+      person.name.toLowerCase().includes(appliedQuery.toLowerCase().trim()),
+    );
+  }, [appliedQuery]);
+
+  const debounce = (callback: (args: string) => void, delay = 300) => {
+    let timerId = 0;
+
+    return (args: string) => {
+      window.clearTimeout(timerId);
+
+      timerId = window.setTimeout(() => {
+        callback(args);
+      }, delay);
+    };
   };
 
-  const handleInputBlur = () => {
-    setIsOpen(false);
-  };
+  const applyQuery = useCallback(debounce(setAppliedQuery, 1000), []);
 
-  const handleDebounce = debounce(handleQueryChange, 1000);
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    applyQuery(event.target.value);
+    setQuery(event.target.value);
+    onSelected(null);
+  };
 
   return (
     <div className="dropdown is-active">
       <div className="dropdown-trigger">
         <input
-          value={query}
-          onChange={handleDebounce}
-          onFocus={handleInputFocus}
-          onBlur={handleInputBlur}
+          value={selectedPerson ? selectedPerson.name : query}
+          onChange={handleQueryChange}
+          onFocus={() => setIsOpen(true)}
+          onBlur={() => setIsOpen(false)}
           type="text"
           placeholder="Enter a part of the name"
           className="input"
           data-cy="search-input"
         />
+        {!filterPeople.length && (
+          <div
+            className="
+            notification
+            is-danger
+            is-light
+            mt-3
+            is-align-self-flex-start
+          "
+            role="alert"
+            data-cy="no-suggestions-message"
+          >
+            <p className="has-text-danger">No matching suggestions</p>
+          </div>
+        )}
       </div>
+
       {isOpen && (
-        <div className="dropdown-menu" id="dropdown-menu2" role="menu">
+        <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
           <div className="dropdown-content">
-            <div className="dropdown-item">
-              {filterPeople.map(person => (
-                <p key={person.born}>{person.name} </p>
-              ))}
-            </div>
+            {filterPeople.map(person => (
+              <div
+                className="dropdown-item"
+                key={person.born}
+                data-cy="suggestions-item"
+                role="button"
+                tabIndex={0}
+                onMouseDown={() => onSelected(person)}
+                onKeyDown={event => {
+                  if (event.key === 'Enter') {
+                    onSelected(person);
+                  }
+                }}
+              >
+                <p className="has-text-link">{person.name}</p>
+              </div>
+            ))}
           </div>
         </div>
       )}
