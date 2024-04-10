bc49c438dee621bd4a45e4f8051af2fc91468d89
Yuliia Kucheriavenko <kucherisvenko@gmail.com>
2024-04-10 16:02:06 +0300

    fixed code

--- src/service/debounce.ts
+++ src/service/debounce.ts
@@ -12,14 +12,3 @@ export function debounce<T extends (...args: any[]) => void>(
     }, delay);
   };
 }
-// export function debounce(callback: Function, delay: number) {
-//   let timerId = 0;
-
-//   return (...args: any) => {
-//     window.clearTimeout(timerId);
-
-//     timerId = window.setTimeout(() => {
-//       callback(...args);
-//     }, delay);
-//   };
-// }
