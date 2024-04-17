b117785da5462fb92e9ebd0f1f5b9b66b3819727
KatsuboDmytro <dmitriyofficial03@gmaik.com>
2024-04-17 13:56:11 +0300

    autocomplete

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,37 @@
 import React, { useCallback, useMemo, useState } from 'react';
 import debounce from 'lodash.debounce';
+import classNames from 'classnames';
 import { peopleFromServer } from './data/people';
 import { Person } from './types/Person';
 import './App.scss';
-import classNames from 'classnames';
 
 export const App: React.FC = () => {
   const [query, setQuery] = useState('');
   const [appliedQuery, setAppliedQuery] = useState('');
   const [isFocusedQuery, setIsFocusedQuery] = useState(false);
-  const [highlightedPerson, setHighlightedPerson] = useState<Person | null>();
+  const [highlightedPerson, setHighlightedPerson] = useState<Person|null>(null);
   const { name, born, died } = highlightedPerson || {};
+  const clickedPerson =
+    highlightedPerson && query.length !== 0
+      ? `${name} (${born} - ${died})`
+      : 'No selected person';
+
   const applyQuery = useCallback(debounce(setAppliedQuery, 300), []);
 
   const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(event.target.value);
     applyQuery(event.target.value);
 
-    if (query.length === 0) {
-      setHighlightedPerson(undefined);
+    if (event.target.value.length === 0) {
+      setHighlightedPerson(null);
     }
   };
 
   const filteredNames = useMemo(() => {
     return peopleFromServer.filter(
-      (person) => person.slug.includes(appliedQuery.trim().toLowerCase())
+      (person) => person.name.toLowerCase().includes(
+        appliedQuery.trim().toLowerCase()
+      )
     );
   }, [appliedQuery]);
 
@@ -38,10 +45,7 @@ export const App: React.FC = () => {
     <div className="container">
       <main className="section is-flex is-flex-direction-column">
         <h1 className="title" data-cy="title">
-          {highlightedPerson && query.length !== 0
-            ? `${name} (${born} - ${died})`
-            : 'No selected person'
-          }
+          {clickedPerson}
         </h1>
 
         <div className="dropdown is-active">
@@ -58,7 +62,7 @@ export const App: React.FC = () => {
             />
           </div>
 
-          {(filteredNames.length !== 0) && (
+          {filteredNames.length !== 0 && (
             isFocusedQuery && (
               <div
                 className="dropdown-menu"
@@ -94,7 +98,7 @@ export const App: React.FC = () => {
           )}
         </div>
 
-        {filteredNames.length === 0 && (
+        {!filteredNames.length && (
           <div
             className="
               notification
