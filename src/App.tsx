29ab13f4154c3daed8f6430bd86985cc313ba405
Medvid Oleksandr <samaster010604@gmail.com>
2024-04-24 13:26:00 +0300

    Autocomplite

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,72 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
+import { Autocomplete } from './components/Autocomplite/Autocomplete';
+import { Person } from './types/Person';
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [autocompleteKey, setAutocompleteKey] = useState(0);
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+  const [filteredPeople, setFilteredPeople] =
+    useState<Person[]>(peopleFromServer);
+
+  const { name, born, died, slug } = selectedPerson || {};
+
+  const filterPeople = useCallback((query: string) => {
+    const filteredPeopleFromServer = peopleFromServer.filter(currentPerson => {
+      const personName = currentPerson.name.toLowerCase();
+      const queryLower = query.toLowerCase();
+
+      return personName.includes(queryLower);
+    });
+
+    setFilteredPeople(filteredPeopleFromServer);
+  }, []);
+
+  const onPersonSelect = (person: Person | null) => {
+    if (person) {
+      setAutocompleteKey(currentKey => currentKey + 1);
+    }
+
+    setSelectedPerson(person);
+  };
 
   return (
     <div className="container">
       <main className="section is-flex is-flex-direction-column">
-        <h1 className="title" data-cy="title">
-          {`${name} (${born} - ${died})`}
-        </h1>
+        {selectedPerson ? (
+          <h1 key={slug} className="title" data-cy="title">
+            {`${name} (${born} - ${died})`}
+          </h1>
+        ) : (
+          <h1 className="title" data-cy="title">
+            No selected person
+          </h1>
+        )}
 
-        <div className="dropdown is-active">
-          <div className="dropdown-trigger">
-            <input
-              type="text"
-              placeholder="Enter a part of the name"
-              className="input"
-              data-cy="search-input"
-            />
+        <Autocomplete
+          key={autocompleteKey}
+          onPersonSelect={onPersonSelect}
+          people={filteredPeople}
+          selectedPerson={selectedPerson}
+          onQuery={filterPeople}
+        />
+
+        {!filteredPeople.length && (
+          <div
+            className="
+              notification
+              is-danger
+              is-light
+              mt-3
+              is-align-self-flex-start
+            "
+            role="alert"
+            data-cy="no-suggestions-message"
+          >
+            <p className="has-text-danger">No matching suggestions</p>
           </div>
-
-          <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
-            <div className="dropdown-content">
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Bernard Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Antone Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Petronella de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Hercke</p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div
-          className="
-            notification
-            is-danger
-            is-light
-            mt-3
-            is-align-self-flex-start
-          "
-          role="alert"
-          data-cy="no-suggestions-message"
-        >
-          <p className="has-text-danger">No matching suggestions</p>
-        </div>
+        )}
       </main>
     </div>
   );
