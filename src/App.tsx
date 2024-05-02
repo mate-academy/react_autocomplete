bfe4f6a7d35031583fcfc91651db99f51af49b36
Yana Momot <yanamomot2004@gmail.com>
2024-05-02 17:31:10 +0300

    add small fix

--- src/App.tsx
+++ src/App.tsx
@@ -73,7 +73,7 @@ export const App: React.FC = () => {
           )}
         </div>
 
-        {!filter && (
+        {!selectUser && dropDown && (
           <div
             className="
             notification
