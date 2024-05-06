e9f8c3efa567d2942e7434c56d8b4100514dbb7b
Valentyn Sobol <valiksobol14@gmail.com>
2024-05-06 23:48:57 +0300

    add task solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import { createRoot } from 'react-dom/client';
 
 import 'bulma/css/bulma.css';
+import 'bulma';
+
 import { App } from './App';
 
 createRoot(document.getElementById('root') as HTMLDivElement).render(<App />);
