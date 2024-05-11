08f9314fa1e067ab15d7a016ad6ccae8cc2356fc
Andrii Dudko <Annbpiu@gmail.com>
2024-05-11 22:01:47 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,109 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
+import { Person } from './types/Person';
+import debounce from 'lodash.debounce';
+import cn from 'classnames';
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [inputText, setInputText] = useState('');
+  const [selectedPerson, setSelectedPerson] = useState('No selected person');
+  const [titleSelectPerson, setTitleSelectPerson] = useState('');
+  const [hasErrorInput, setHasErrorInput] = useState(true);
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+
+  const applyInput = useCallback(debounce(setInputText, 300), []);
+
+  const filteredPeople = peopleFromServer.filter(person =>
+    person.name.toLowerCase().includes(inputText.toLowerCase()),
+  );
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    applyInput(event.target.value);
+    setInputText(event.target.value);
+
+    const hasMatch = peopleFromServer.some(person =>
+      person.name
+        .toLowerCase()
+        .includes(event.target.value.trim().toLowerCase()),
+    );
+
+    setHasErrorInput(hasMatch);
+  };
+
+  const suggesName = (person: Person) => {
+    setSelectedPerson(`${person.name} (${person.born} - ${person.died})`);
+
+    setInputText(person.name);
+    setTitleSelectPerson(person.name);
+    setDropdownVisible(false);
+  };
+
+  useEffect(() => {
+    if (inputText !== titleSelectPerson) {
+      setDropdownVisible(false);
+      setSelectedPerson('No selected person');
+    }
+
+    if (selectedPerson === 'No selected person') {
+      setDropdownVisible(true);
+    }
+  });
 
   return (
     <div className="container">
       <main className="section is-flex is-flex-direction-column">
         <h1 className="title" data-cy="title">
-          {`${name} (${born} - ${died})`}
+          {selectedPerson}
         </h1>
 
-        <div className="dropdown is-active">
+        <div
+          className={cn('dropdown', {
+            'is-active': dropdownVisible,
+          })}
+        >
           <div className="dropdown-trigger">
             <input
               type="text"
               placeholder="Enter a part of the name"
               className="input"
               data-cy="search-input"
+              value={inputText}
+              onChange={handleInputChange}
             />
           </div>
 
           <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
             <div className="dropdown-content">
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Bernard Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Antone Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Petronella de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Hercke</p>
-              </div>
+              {filteredPeople.map((people, index) => (
+                <div
+                  className="dropdown-item"
+                  key={index}
+                  data-cy="suggestion-item"
+                  onClick={() => suggesName(people)}
+                >
+                  <p className="has-text-link">{people.name}</p>
+                </div>
+              ))}
             </div>
           </div>
         </div>
 
-        <div
-          className="
+        {!hasErrorInput && (
+          <div
+            className="
             notification
             is-danger
             is-light
             mt-3
             is-align-self-flex-start
           "
-          role="alert"
-          data-cy="no-suggestions-message"
-        >
-          <p className="has-text-danger">No matching suggestions</p>
-        </div>
+            role="alert"
+            data-cy="no-suggestions-message"
+          >
+            <p className="has-text-danger">No matching suggestions</p>
+          </div>
+        )}
       </main>
     </div>
   );
