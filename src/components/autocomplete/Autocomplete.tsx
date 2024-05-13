20c46c8dd454745dc0064695fda0d054aded9165
Mykhailo Lapchynskyi <mykhailolapchynskyi@Mykhailos-MacBook-Air.local>
2024-05-13 18:55:02 +0200

    task solution

---
+++ src/components/autocomplete/Autocomplete.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Person } from '../../types/Person';
+
+type AutocompleteProps = {
+  suggestions: Person[];
+  conditionShowDropdown: boolean;
+  selectPersonFromTheList: (
+    event: React.MouseEvent<HTMLParagraphElement>,
+  ) => void;
+};
+
+export const Autocomplete: React.FC<AutocompleteProps> = ({
+  suggestions,
+  conditionShowDropdown,
+  selectPersonFromTheList,
+}) => {
+  return (
+    <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
+      {conditionShowDropdown && (
+        <div className="dropdown-content">
+          {suggestions.map(({ name, slug }) => (
+            <div className="dropdown-item" data-cy="suggestion-item" key={slug}>
+              <p className="has-text-link" onClick={selectPersonFromTheList}>
+                {name}
+              </p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
