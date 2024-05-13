908d81694cccb1e294065d7db044d827ac404781
Valeriia Fomenko <valeriia.fomenko@ukr.net>
2024-05-13 21:28:53 +0300

    Solution

--- src/components/Autocomplete.tsx
+++ src/components/Autocomplete.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useMemo } from 'react';
+import React, { useState, useRef, useMemo, useEffect } from 'react';
 import { Person } from '../types/Person';
 import debounce from 'lodash.debounce';
 import cn from 'classnames';
@@ -39,11 +39,32 @@ export const Autocomplete: React.FC<Props> = ({
     );
   }, [appliedQuery, people]);
 
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        inputRef.current &&
+        !inputRef.current.contains(event.target as Node)
+      ) {
+        setShowSuggestions(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className="dropdown is-active">
+    <div
+      className={cn('dropdown', {
+        'is-active': showSuggestions,
+      })}
+      ref={inputRef}
+    >
       <div className="dropdown-trigger">
         <input
-          ref={inputRef}
           type="text"
           placeholder="Enter a part of the name"
           className="input"
@@ -57,36 +78,38 @@ export const Autocomplete: React.FC<Props> = ({
       {showSuggestions && (
         <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
           <div className="dropdown-content">
-            {filteredPeople.map(person => (
-              <div
-                key={person.slug}
-                className="dropdown-item"
-                data-cy="suggestion-item"
-                onClick={() => handleSuggestionClick(person)}
-              >
-                <p
-                  className={cn('has-text-link', 'is-clickable', {
-                    'has-text-danger': person.sex === 'f',
-                  })}
+            {filteredPeople.length > 0 ? (
+              filteredPeople.map(person => (
+                <div
+                  key={person.slug}
+                  className="dropdown-item"
+                  data-cy="suggestion-item"
+                  onClick={() => handleSuggestionClick(person)}
                 >
-                  {person.name}
-                </p>
+                  <p
+                    className={cn('has-text-link', 'is-clickable', {
+                      'has-text-danger': person.sex === 'f',
+                    })}
+                  >
+                    {person.name}
+                  </p>
+                </div>
+              ))
+            ) : (
+              <div
+                className="
+                  notification
+                  is-danger
+                  is-light
+                  mt-3
+                  is-align-self-flex-start
+                "
+                role="alert"
+                data-cy="no-suggestions-message"
+              >
+                <p className="has-text-danger">No matching suggestions</p>
               </div>
-            ))}
-          </div>
-
-          <div
-            className="
-            notification
-            is-danger
-            is-light
-            mt-3
-            is-align-self-flex-start
-          "
-            role="alert"
-            data-cy="no-suggestions-message"
-          >
-            <p className="has-text-danger">No matching suggestions</p>
+            )}
           </div>
         </div>
       )}
