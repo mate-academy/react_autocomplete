6e1bf0548cd7c01ac54edbb2c9ce825ead54b0ab
Hanna Svirina <26anna07@ukr.net>
2024-06-21 22:00:42 +0200

    fix solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,10 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
-import { peopleFromServer } from './data/people';
-import debounce from 'lodash.debounce';
 import { Person } from './types/Person';
-import cn from 'classnames';
 import { Autocomplete } from './components/Autocomplete';
 
 export const App: React.FC = () => {
   const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
-  const [query, setQuery] = useState('');
-  const [appliedQuery, setAppliedQuery] = useState('');
-  const [isFocused, setIsFocused] = useState(false);
-
-  const delay = 300;
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const applyQuery = useCallback(
-    debounce(e => {
-      setAppliedQuery(e);
-      setIsFocused(true);
-    }, delay),
-    [],
-  );
-
-  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setQuery(event.target.value);
-    setSelectedPerson(null);
-    setIsFocused(false);
-    applyQuery(event.target.value);
-  };
-
-  const filterPeople = useMemo(() => {
-    return peopleFromServer.filter(person =>
-      person.name
-        .toLowerCase()
-        .trim()
-        .includes(appliedQuery.trim().toLowerCase()),
-    );
-  }, [appliedQuery]);
 
   return (
     <div className="container">
@@ -47,44 +15,7 @@ export const App: React.FC = () => {
             : 'No selected person'}
         </h1>
 
-        <div className={cn('dropdown', { 'is-active': isFocused })}>
-          <div className="dropdown-trigger">
-            <input
-              type="text"
-              placeholder="Enter a part of the name"
-              className="input"
-              data-cy="search-input"
-              value={query}
-              onFocus={() => setIsFocused(true)}
-              onChange={handleQueryChange}
-            />
-          </div>
-
-          {isFocused && filterPeople.length > 0 && (
-            <Autocomplete
-              setQuery={setQuery}
-              setFocus={setIsFocused}
-              onSelect={setSelectedPerson}
-              people={filterPeople}
-            />
-          )}
-        </div>
-
-        {filterPeople.length === 0 && appliedQuery && (
-          <div
-            className="
-              notification
-              is-danger
-              is-light
-              mt-3
-              is-align-self-flex-start
-            "
-            role="alert"
-            data-cy="no-suggestions-message"
-          >
-            <p className="has-text-danger">No matching suggestions</p>
-          </div>
-        )}
+        <Autocomplete onSelect={setSelectedPerson} />
       </main>
     </div>
   );
--- src/components/Autocomplete.tsx
+++ src/components/Autocomplete.tsx
@@ -1,33 +1,92 @@
+import cn from 'classnames';
+import { peopleFromServer } from '../data/people';
 import { Person } from '../types/Person';
+import React, { useCallback, useMemo, useState } from 'react';
+import debounce from 'lodash.debounce';
 
 type Props = {
-  setQuery: (name: string) => void;
-  setFocus: (arg: boolean) => void;
   onSelect: (person: Person | null) => void;
-  people: Person[];
 };
 
-export const Autocomplete: React.FC<Props> = ({
-  setQuery,
-  setFocus,
-  onSelect,
-  people,
-}) => {
+export const Autocomplete: React.FC<Props> = ({ onSelect = () => {} }) => {
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
+  const [isFocused, setIsFocused] = useState(false);
+
+  const delay = 300;
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const applyQuery = useCallback(
+    debounce(e => {
+      setAppliedQuery(e);
+      setIsFocused(true);
+    }, delay),
+    [],
+  );
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+    onSelect(null);
+    setIsFocused(false);
+    applyQuery(event.target.value);
+  };
+
+  const filterPeople = useMemo(() => {
+    return peopleFromServer.filter(person =>
+      person.name
+        .toLowerCase()
+        .trim()
+        .includes(appliedQuery.trim().toLowerCase()),
+    );
+  }, [appliedQuery]);
+
   return (
-    <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
-      <div className="dropdown-content">
-        {people.map((person: Person) => (
-          <div
-            className="dropdown-item"
-            data-cy="suggestion-item"
-            key={person.name}
-            onClick={() => (
-              onSelect(person), setQuery(person.name), setFocus(false)
-            )}
-          >
-            <p className="has-text-link">{person.name}</p>
+    <div className={cn('dropdown', { 'is-active': isFocused })}>
+      <div className="dropdown-trigger">
+        <input
+          type="text"
+          placeholder="Enter a part of the name"
+          className="input"
+          data-cy="search-input"
+          value={query}
+          onFocus={() => setIsFocused(true)}
+          onChange={handleQueryChange}
+        />
+      </div>
+
+      <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
+        {isFocused && filterPeople.length > 0 && (
+          <div className="dropdown-content">
+            {filterPeople.map((person: Person) => (
+              <div
+                className="dropdown-item"
+                data-cy="suggestion-item"
+                key={person.name}
+                onClick={() => (
+                  onSelect(person), setQuery(person.name), setIsFocused(false)
+                )}
+              >
+                <p className="has-text-link">{person.name}</p>
+              </div>
+            ))}
           </div>
-        ))}
+        )}
+
+        {filterPeople.length === 0 && appliedQuery && (
+          <div
+            className="
+                notification
+                is-danger
+                is-light
+                mt-3
+                is-align-self-flex-start
+              "
+            role="alert"
+            data-cy="no-suggestions-message"
+          >
+            <p className="has-text-danger">No matching suggestions</p>
+          </div>
+        )}
       </div>
     </div>
   );
