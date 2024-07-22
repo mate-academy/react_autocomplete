2a722fab449b692890172f10e75d060827cfaaa1
Ekaterina Bessonnaya <iekatierinovna9090@gmail.com>
2024-07-22 13:22:48 +0300

    Autocomplete component was implement

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,107 @@
-import React from 'react';
 import './App.scss';
+import { Person } from './types/Person';
 import { peopleFromServer } from './data/people';
+import React, { useEffect, useMemo, useState } from 'react';
+import { Autocomplete } from './component/Autocomplete';
 
-export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+type AppProps = {
+  debounceDelay?: number;
+};
+
+export const App: React.FC<AppProps> = ({ debounceDelay = 300 }) => {
+  const [query, setQuery] = useState('');
+  const [debouncedQuery, setDebouncedQuery] = useState('');
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+  const [isDropDownActive, setIsDropDownActive] = useState(false);
+  const [isInputFocused, setIsInpunFocused] = useState(false);
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+    setSelectedPerson(null);
+    setIsDropDownActive(true);
+  };
+
+  const handleSelected = (person: Person) => {
+    setQuery(person.name);
+    setSelectedPerson(person);
+    setIsDropDownActive(false);
+  };
+
+  const handleDeleteClick = () => {
+    setQuery('');
+    setSelectedPerson(null);
+    setIsDropDownActive(false);
+  };
+
+  const filltered = useMemo(() => {
+    if (isInputFocused && query === '') {
+      return peopleFromServer;
+    }
+
+    return peopleFromServer.filter(person =>
+      person.name.toLowerCase().includes(debouncedQuery.toLowerCase()),
+    );
+  }, [debouncedQuery, isInputFocused, query]);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedQuery(query);
+    }, debounceDelay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [query, debounceDelay]);
 
   return (
     <div className="container">
       <main className="section is-flex is-flex-direction-column">
         <h1 className="title" data-cy="title">
-          {`${name} (${born} - ${died})`}
+          {selectedPerson
+            ? `${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`
+            : 'No selected person'}
         </h1>
 
-        <div className="dropdown is-active">
+        <div className={`dropdown ${isDropDownActive ? 'is-active' : ''}`}>
           <div className="dropdown-trigger">
             <input
               type="text"
               placeholder="Enter a part of the name"
               className="input"
               data-cy="search-input"
+              value={query}
+              onChange={handleQueryChange}
+              onFocus={() => {
+                setIsDropDownActive(true);
+                setIsInpunFocused(true);
+              }}
             />
+
+            {isDropDownActive && (
+              <button className="delete" onClick={handleDeleteClick}></button>
+            )}
           </div>
 
           <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
-            <div className="dropdown-content">
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Bernard Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Antone Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Petronella de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Hercke</p>
-              </div>
-            </div>
+            <Autocomplete people={filltered} onSelected={handleSelected} />
           </div>
         </div>
 
-        <div
-          className="
-            notification
-            is-danger
-            is-light
-            mt-3
-            is-align-self-flex-start
-          "
-          role="alert"
-          data-cy="no-suggestions-message"
-        >
-          <p className="has-text-danger">No matching suggestions</p>
-        </div>
+        {query && filltered.length === 0 && (
+          <div
+            className="
+                    notification
+                    is-danger
+                    is-light
+                    mt-3
+                    is-align-self-flex-start
+                  "
+            role="alert"
+            data-cy="no-suggestions-message"
+          >
+            <p className="has-text-danger">No matching suggestions</p>
+          </div>
+        )}
       </main>
     </div>
   );
