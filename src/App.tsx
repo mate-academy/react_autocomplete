a70bd1a1427b8febc5590ad6d459b35ab4c2dd68
beliyandr <beliyandr@gmail.com>
2024-08-20 23:12:28 +0300

    123

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,78 @@
-// import React, { useState } from 'react';
-// import './App.scss';
-// import { peopleFromServer } from './data/people';
-// import { Message } from './components/Message';
+import React, { useCallback, useState } from 'react';
+import './App.scss';
+import { peopleFromServer } from './data/people';
+import { Message } from './components/Message';
 // import { DropdownItem } from './components/DropdownItem';
 // import { Title } from './components/Title';
+import { Person } from './types/Person';
+import debounce from 'lodash.debounce';
 // import { Person } from './types/Person';
-// // import { Person } from './types/Person';
 
-// export const App: React.FC = () => {
-//   // const { name, born, died } = peopleFromServer[0];
-//   const [people, setPeople] = useState<Person[]>(peopleFromServer);
-//   const [filteredPersons, setFilteredPersons] = useState<Person[]>([]);
-//   const [value, setValue] = useState<string>('');
-//   const [person, setPerson] = useState<Person>();
+export const App: React.FC = () => {
+  const [people] = useState<Person[]>(peopleFromServer);
+  const [filteredPersons, setFilteredPersons] = useState<Person[]>(people);
+  const [value, setValue] = useState<string>('');
+  // const [person, setPerson] = useState<Person>();
 
-//   const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-//     setValue(event.target.value);
+  // const [focus, setFocus] = useState<boolean>(false);
 
-//     setFilteredPersons(
-//       people.filter(person =>
-//         person.name.toLowerCase().includes(event.target.value.toLowerCase()),
-//       ),
-//     );
-//     // setPerson()
-//   };
+  const apliedFilterPersons = useCallback(
+    debounce(setFilteredPersons, 300),
+    [],
+  );
 
-//   return (
-//     <div className="container">
-//       <main className="section is-flex is-flex-direction-column">
-//         {person && <Title person={person} />}
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setValue(event.target.value);
 
-//         <div className="dropdown is-active">
-//           <div className="dropdown-trigger">
-//             <input
-//               type="text"
-//               placeholder="Enter a part of the name"
-//               className="input"
-//               data-cy="search-input"
-//               value={value}
-//               onChange={handleChange}
-//             />
-//           </div>
+    apliedFilterPersons(
+      people.filter(person =>
+        person.name.toLowerCase().includes(event.target.value.toLowerCase()),
+      ),
+    );
+    // setPerson()
+  };
 
-//           <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
-//             <div className="dropdown-content">
-//               {filteredPersons.map(person => (
-//                 <DropdownItem
-//                   name={person.name}
-//                   key={person.name + person.died}
-//                 />
-//               ))}
-//             </div>
-//           </div>
-//         </div>
+  // const handleFocus = (e: React.FocusEvent<HTMLInputElement>): void => {
+  //   setFocus(true);
+  // };
+  // const handleBlur = (e: React.FocusEvent<HTMLInputElement>): void => {
+  //   setFocus(false);
+  // };
 
-//         <Message />
-//       </main>
-//     </div>
-//   );
-// };
+  return (
+    <div className="container">
+      <main className="section is-flex is-flex-direction-column">
+        {/* {person && <Title person={person} />} */}
+
+        <div className="dropdown is-active">
+          <div className="dropdown-trigger">
+            <input
+              type="text"
+              placeholder="Enter a part of the name"
+              className="input"
+              data-cy="search-input"
+              value={value}
+              onChange={handleChange}
+              // onFocus={handleFocus}
+              // onBlur={handleBlur}
+            />
+          </div>
+
+          <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
+            <div className="dropdown-content">
+              {/* {focus &&
+                filteredPersons.map(person => (
+                  <DropdownItem
+                    name={person.name}
+                    key={person.name + person.died}
+                  />
+                ))} */}
+            </div>
+          </div>
+        </div>
+
+        {!filteredPersons.length && <Message />}
+      </main>
+    </div>
+  );
+};
--- src/components/DropdownItem.tsx
+++ src/components/DropdownItem.tsx
@@ -6,8 +6,8 @@ type DropdownItemProps = {
 
 export const DropdownItem: FC<DropdownItemProps> = ({ name }) => {
   return (
-    <div className="dropdown-item" data-cy="suggestion-item">
-      <p className="has-text-link">{name}</p>
+    <div className="dropdown-item " data-cy="suggestion-item">
+      <p className="has-text-link ">{name}</p>
     </div>
   );
 };
