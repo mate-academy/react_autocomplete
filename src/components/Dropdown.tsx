3c53dddfb379c54fd828980bcc3b484b51ded303
Yurii Kryvorotko <krivorotkokrivorotko2016@gmail.com>
2024-10-07 18:58:18 +0300

    Solution

---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,78 @@
+import cn from 'classnames';
+import { Person } from '../types/Person';
+import { useRef, useState } from 'react';
+import '../App.scss';
+
+interface Props {
+  query: string;
+  onQueryChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  filteredPeople: Person[];
+  onSelected: (person: Person) => void;
+  setQuery: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export const Dropdown: React.FC<Props> = ({
+  query,
+  onQueryChange,
+  filteredPeople,
+  onSelected,
+  setQuery,
+}) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [isInputFocused, setIsInputFocused] = useState(false);
+
+  const handleFocus = () => {
+    setIsInputFocused(true);
+  };
+
+  const handleUnfocus = () => {
+    setIsInputFocused(false);
+  };
+
+  const handleSelectAndFocus = (person: Person) => {
+    onSelected(person);
+    setQuery(person.name);
+    handleUnfocus();
+  };
+
+  return (
+    <div className="dropdown is-active">
+      <div className="dropdown-trigger">
+        <input
+          ref={inputRef}
+          onFocus={handleFocus}
+          type="search"
+          placeholder="Enter a part of the name"
+          className="input"
+          data-cy="search-input"
+          value={query}
+          onChange={onQueryChange}
+        />
+      </div>
+
+      {isInputFocused && !!filteredPeople.length && (
+        <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
+          <div className="dropdown-content">
+            {filteredPeople.map(person => (
+              <div
+                onClick={() => handleSelectAndFocus(person)}
+                key={person.slug}
+                className="dropdown-item hovered"
+                data-cy="suggestion-item"
+              >
+                <p
+                  className={cn({
+                    'has-text-link': person.sex === 'm',
+                    'has-text-danger': person.sex === 'f',
+                  })}
+                >
+                  {person.name}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
