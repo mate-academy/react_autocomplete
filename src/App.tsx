5234688110c4c548353f62a60331e3c24b764774
Artem Puzenko <a.puzenko2004@gmail.com>
2025-01-06 22:20:41 +0200

    add filteredNames and debounced func

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,92 @@
-import React from 'react';
+import React, { useMemo, useRef, useState } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import debounce from 'lodash.debounce';
 
-export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+type Props = {
+  delay: number;
+  onSelected: () => void;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const App: React.FC<Props> = ({ delay = 300 }) => {
+  // const { name, born, died } = peopleFromServer[0];
+  const [inputValue, setInputValue] = useState('');
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const debouncedSetInputValue = useMemo(
+    () =>
+      debounce((value: string) => {
+        setInputValue(value);
+      }, delay),
+    [delay],
+  );
+
+  const filteredNames = useMemo(() => {
+    return peopleFromServer.filter(person => person.name.includes(inputValue));
+  }, [inputValue]);
 
   return (
     <div className="container">
       <main className="section is-flex is-flex-direction-column">
         <h1 className="title" data-cy="title">
-          {`${name} (${born} - ${died})`}
+          {/* {`${name} (${born} - ${died})`} */}
         </h1>
 
-        <div className="dropdown is-active">
+        <div className={`dropdown ${isDropdownVisible ? 'is-active' : ''}`}>
           <div className="dropdown-trigger">
             <input
+              ref={inputRef}
               type="text"
               placeholder="Enter a part of the name"
               className="input"
               data-cy="search-input"
+              value={inputValue}
+              onChange={event => setInputValue(event.target.value)}
+              onFocus={() => setIsDropdownVisible(true)}
+              onBlur={() => setIsDropdownVisible(false)}
             />
           </div>
 
-          <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
-            <div className="dropdown-content">
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Bernard Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Antone Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Petronella de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Hercke</p>
+          {isDropdownVisible && (
+            <div
+              className="dropdown-menu"
+              role="menu"
+              data-cy="suggestions-list"
+            >
+              <div className="dropdown-content">
+                {filteredNames.map(person => (
+                  <div
+                    className="dropdown-item"
+                    key={person.born}
+                    data-cy="suggestion-item"
+                  >
+                    <p className="has-text-link">{person.name}</p>
+                  </div>
+                ))}
               </div>
             </div>
-          </div>
+          )}
         </div>
 
-        <div
-          className="
+        {filteredNames.length === 0 && (
+          <div
+            className="
             notification
             is-danger
             is-light
             mt-3
             is-align-self-flex-start
           "
-          role="alert"
-          data-cy="no-suggestions-message"
-        >
-          <p className="has-text-danger">No matching suggestions</p>
-        </div>
+            role="alert"
+            data-cy="no-suggestions-message"
+          >
+            <p className="has-text-danger">No matching suggestions</p>
+          </div>
+        )}
       </main>
     </div>
   );
