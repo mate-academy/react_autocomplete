70bad26dd8aafa5fc25b98e34569900dabe39f0a
Artem Puzenko <a.puzenko2004@gmail.com>
2025-01-07 15:55:38 +0200

    completed the task

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,31 @@
-import React, { useMemo, useRef, useState } from 'react';
+import React, { useCallback, useMemo, useRef, useState } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import debounce from 'lodash.debounce';
 
 type Props = {
   delay: number;
-  onSelected: () => void;
+  onSelected: (
+    person: { name: string; born: number; died: number } | null,
+  ) => void;
 };
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const App: React.FC<Props> = ({ delay = 300 }) => {
-  // const { name, born, died } = peopleFromServer[0];
+type Person = {
+  name: string;
+  born: number;
+  died: number;
+};
+
+export const App: React.FC<Props> = ({ delay = 300, onSelected }) => {
   const [inputValue, setInputValue] = useState('');
   const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+  const [selectedPerson, setSelectedPerson] = useState<{
+    name: string;
+    born: number;
+    died: number;
+  } | null>(null);
   const inputRef = useRef<HTMLInputElement>(null);
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const debouncedSetInputValue = useMemo(
     () =>
       debounce((value: string) => {
@@ -25,15 +34,40 @@ export const App: React.FC<Props> = ({ delay = 300 }) => {
     [delay],
   );
 
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+
+    debouncedSetInputValue(value);
+
+    if (selectedPerson) {
+      setSelectedPerson(null);
+      onSelected(null);
+    }
+  };
+
+  const handleSelectPerson = useCallback(
+    (person: Person) => {
+      setSelectedPerson(person);
+      setInputValue(person.name);
+      setIsDropdownVisible(false);
+      onSelected(person);
+    },
+    [onSelected],
+  );
+
   const filteredNames = useMemo(() => {
-    return peopleFromServer.filter(person => person.name.includes(inputValue));
+    return peopleFromServer.filter(person =>
+      person.name.toLowerCase().includes(inputValue.toLowerCase()),
+    );
   }, [inputValue]);
 
   return (
     <div className="container">
       <main className="section is-flex is-flex-direction-column">
         <h1 className="title" data-cy="title">
-          {/* {`${name} (${born} - ${died})`} */}
+          {selectedPerson
+            ? `${selectedPerson?.name} (${selectedPerson?.born} - ${selectedPerson?.born})`
+            : `No selected person`}
         </h1>
 
         <div className={`dropdown ${isDropdownVisible ? 'is-active' : ''}`}>
@@ -44,10 +78,10 @@ export const App: React.FC<Props> = ({ delay = 300 }) => {
               placeholder="Enter a part of the name"
               className="input"
               data-cy="search-input"
-              value={inputValue}
-              onChange={event => setInputValue(event.target.value)}
+              defaultValue={inputValue}
+              onChange={handleInputChange}
               onFocus={() => setIsDropdownVisible(true)}
-              onBlur={() => setIsDropdownVisible(false)}
+              onBlur={() => setTimeout(() => setIsDropdownVisible(false), 200)}
             />
           </div>
 
@@ -63,8 +97,11 @@ export const App: React.FC<Props> = ({ delay = 300 }) => {
                     className="dropdown-item"
                     key={person.born}
                     data-cy="suggestion-item"
+                    onClick={() => handleSelectPerson(person)}
                   >
-                    <p className="has-text-link">{person.name}</p>
+                    <p style={{ cursor: 'default' }} className="has-text-link">
+                      {person.name}
+                    </p>
                   </div>
                 ))}
               </div>
