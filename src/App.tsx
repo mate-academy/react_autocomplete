4012b6b04b49d0a0cbd0b5aae61d4c0bf001587c
tiblazy <hauntedapply@gmail.com>
2025-02-17 01:02:00 -0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,60 @@
-import React from 'react';
+import debounce from 'lodash.debounce';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
+import { Person } from './types/Person';
 
-export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+export const App: React.FC<{ delay?: number }> = ({ delay = 300 }) => {
+  const [query, setQuery] = useState('');
+  const [applyQuery, setApplyQuery] = useState('');
+  const [filterPeople, setFilterPeople] = useState<Person[]>(peopleFromServer);
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>();
+
+  const applyDebounce = useRef<(value: string) => void>();
+
+  useEffect(() => {
+    applyDebounce.current = debounce((value: string) => {
+      setApplyQuery(value);
+    }, delay);
+
+    return () => {
+      applyDebounce.current?.cancel();
+    };
+  }, [delay]);
+
+  useEffect(() => {
+    if (applyQuery.trim() === '') {
+      setFilterPeople(peopleFromServer);
+    } else {
+      setFilterPeople(
+        peopleFromServer.filter(personFromServer =>
+          personFromServer.name
+            .toLowerCase()
+            .includes(applyQuery.toLowerCase()),
+        ),
+      );
+    }
+  }, [applyQuery]);
+
+  const handlePerson = (person: Person) => {
+    setSelectedPerson(person);
+    setQuery(person.name);
+    setFilterPeople([]);
+  };
+
+  useEffect(() => {
+    if (selectedPerson && query !== selectedPerson.name) {
+      setSelectedPerson(null);
+    }
+  }, [query, selectedPerson]);
 
   return (
     <div className="container">
       <main className="section is-flex is-flex-direction-column">
         <h1 className="title" data-cy="title">
-          {`${name} (${born} - ${died})`}
+          {selectedPerson
+            ? `${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`
+            : 'No selected person'}
         </h1>
 
         <div className="dropdown is-active">
@@ -19,54 +64,40 @@ export const App: React.FC = () => {
               placeholder="Enter a part of the name"
               className="input"
               data-cy="search-input"
+              value={query}
+              onChange={e => {
+                const value = e.target.value;
+
+                setQuery(value);
+                applyDebounce.current?.(value);
+              }}
             />
           </div>
 
           <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
             <div className="dropdown-content">
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Bernard Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Antone Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Petronella de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Hercke</p>
-              </div>
+              {filterPeople.length > 0 ? (
+                filterPeople.map((filterPerson, index) => (
+                  <div
+                    key={index}
+                    className="dropdown-item"
+                    data-cy="suggestion-item"
+                    onClick={() => handlePerson(filterPerson)}
+                  >
+                    <p className="has-text-link">{filterPerson.name}</p>
+                  </div>
+                ))
+              ) : (
+                <div
+                  className="dropdown-item"
+                  data-cy="no-suggestions-message"
+                  role="alert"
+                >
+                  <p className="has-text-danger">No matching suggestions</p>
+                </div>
+              )}
             </div>
           </div>
-        </div>
-
-        <div
-          className="
-            notification
-            is-danger
-            is-light
-            mt-3
-            is-align-self-flex-start
-          "
-          role="alert"
-          data-cy="no-suggestions-message"
-        >
-          <p className="has-text-danger">No matching suggestions</p>
         </div>
       </main>
     </div>
