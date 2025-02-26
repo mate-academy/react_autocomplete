d4b214590bf62049aec9d806aeac7cb01388d1fb
serbin.nick@gmail.com <serbin.nick@gmail.com>
2025-02-26 15:03:06 +0100

    sollution

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,57 @@
-import React from 'react';
-import './App.scss';
-import { peopleFromServer } from './data/people';
+import React from "react";
+import "./App.scss";
+import { useState, useRef, useEffect, useCallback } from "react";
+import { peopleFromServer } from "./data/people";
+import classNames from "classnames";
+import debounce from "debounce";
 
-export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+import { Person } from "./types/Person";
+
+function getPeopleList(people: Person[], query: string) {
+  return people.filter((person) =>
+    person.name.toLowerCase().includes(query.trim().toLowerCase()),
+  );
+}
+
+type Props = {
+  delay: number;
+};
+export const App: React.FC<Props> = ({ delay = 1000 }) => {
+  const [query, setQuery] = useState("");
+  const [appliedQuery, setAppliedQuery] = useState("");
+  const [isActive, setIsActive] = useState(false);
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  const filteredList = getPeopleList(peopleFromServer, appliedQuery);
+
+  const applyQuery = useCallback(debounce(setAppliedQuery, delay), []);
+
+  // Закрытие dropdown при клике вне
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsActive(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <div className="container">
       <main className="section is-flex is-flex-direction-column">
         <h1 className="title" data-cy="title">
-          {`${name} (${born} - ${died})`}
+          {selectedPerson === null
+            ? "No selected person"
+            : `${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`}
         </h1>
 
         <div className="dropdown is-active">
@@ -19,80 +61,63 @@ export const App: React.FC = () => {
               placeholder="Enter a part of the name"
               className="input"
               data-cy="search-input"
+              onChange={(event) => {
+                setQuery(event.target.value);
+                applyQuery(event.target.value);
+              }}
+              onClick={() => setIsActive(true)}
+              // onBlur={() => setIsActive(false)}
+              value={query}
             />
           </div>
-
-          <div
-            className="dropdown-menu"
-            role="menu"
-            data-cy="suggestions-list"
-          >
-            <div className="dropdown-content">
-              <div
-                className="dropdown-item"
-                data-cy="suggestion-item"
-              >
-                <p className="has-text-link">Pieter Haverbeke</p>
-              </div>
-
-              <div
-                className="dropdown-item"
-                data-cy="suggestion-item"
-              >
-                <p className="has-text-link">Pieter Bernard Haverbeke</p>
-              </div>
-
-              <div
-                className="dropdown-item"
-                data-cy="suggestion-item"
-              >
-                <p className="has-text-link">Pieter Antone Haverbeke</p>
-              </div>
-
-              <div
-                className="dropdown-item"
-                data-cy="suggestion-item"
-              >
-                <p className="has-text-danger">Elisabeth Haverbeke</p>
-              </div>
-
-              <div
-                className="dropdown-item"
-                data-cy="suggestion-item"
-              >
-                <p className="has-text-link">Pieter de Decker</p>
-              </div>
-
-              <div
-                className="dropdown-item"
-                data-cy="suggestion-item"
-              >
-                <p className="has-text-danger">Petronella de Decker</p>
-              </div>
-
-              <div
-                className="dropdown-item"
-                data-cy="suggestion-item"
-              >
-                <p className="has-text-danger">Elisabeth Hercke</p>
+          {isActive && filteredList.length && (
+            <div
+              className="dropdown-menu"
+              role="menu"
+              data-cy="suggestions-list"
+              ref={dropdownRef}
+            >
+              <div className="dropdown-content">
+                {filteredList.map((person) => (
+                  <div
+                    className="dropdown-item"
+                    data-cy="suggestion-item"
+                    style={{ cursor: "pointer" }}
+                    key={person.name}
+                  >
+                    <p
+                      className={classNames({
+                        "has-text-link": person.sex === "m",
+                        "has-text-danger": person.sex === "f",
+                      })}
+                      onClick={() => {
+                        setSelectedPerson(person);
+                        setQuery(person.name);
+                      }}
+                    >
+                      {person.name}
+                    </p>
+                  </div>
+                ))}
               </div>
             </div>
+          )}
+        </div>
+        {!filteredList.length && (
+          <div
+            className="
+              notification
+              is-danger
+              is-light
+              mt-3
+              is-align-self-flex-start
+            "
+            role="alert"
+            data-cy="no-suggestions-message"
+          >
+            <p className="has-text-danger">No matching suggestions</p>
           </div>
-        </div>
-
-        <div
-          className="
-            notification
-            is-danger
-            is-light
-            mt-3
-            is-align-self-flex-start
-          "
-          role="alert"
-          data-cy="no-suggestions-message"
-        >
-          <p className="has-text-danger">No matching suggestions</p>
-        </div>
+        )}
       </main>
     </div>
   );
