60766959edd1c39ed7e59236af42dc9b2e00622e
Your Name <you@example.com>
2025-06-29 11:53:24 +0200

    first attempt

--- src/components/Autocomplete/Autocomplete.tsx
+++ src/components/Autocomplete/Autocomplete.tsx
@@ -36,6 +36,39 @@ const Autocomplete: React.FC<AutocompleteProps> = ({
     };
   }, [query, people, delay]);
 
+  let suggestionsContent;
+
+  if (suggestions.length > 0) {
+    suggestionsContent = suggestions.map(person => (
+      <div
+        key={person.name}
+        className="dropdown-item"
+        data-cy="suggestion-item"
+        onMouseDown={e => {
+          e.preventDefault();
+          setQuery(person.name);
+          onSelected(person);
+          setIsOpen(false);
+        }}
+      >
+        <p className="has-text-link">{person.name}</p>
+      </div>
+    ));
+  } else if (query.trim() !== '') {
+    suggestionsContent = (
+      <div
+        className="
+        notification is-danger is-light mt-3 is-align-self-flex-start"
+        role="alert"
+        data-cy="no-suggestions-message"
+      >
+        <p className="has-text-danger">No matching suggestions</p>
+      </div>
+    );
+  } else {
+    suggestionsContent = null;
+  }
+
   return (
     <div className={`dropdown ${isOpen ? 'is-active' : ''}`}>
       <div className="dropdown-trigger">
@@ -45,6 +78,7 @@ const Autocomplete: React.FC<AutocompleteProps> = ({
           placeholder="Enter a part of the name"
           data-cy="search-input"
           onChange={e => {
+            setIsOpen(true);
             setQuery(e.target.value);
             onSelected(null);
           }}
@@ -54,43 +88,12 @@ const Autocomplete: React.FC<AutocompleteProps> = ({
               setSuggestions(people);
             }
           }}
-          onBlur={() => setTimeout(() => setIsOpen(false), 150)}
+          onBlur={() => setTimeout(() => setIsOpen(false), 600)}
         />
       </div>
 
       <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
-        <div className="dropdown-content">
-          {suggestions.length > 0 ? (
-            suggestions.map(person => (
-              <div
-                key={person.name}
-                className="dropdown-item"
-                data-cy="suggestion-item"
-                onMouseDown={() => {
-                  setQuery(person.name);
-                  onSelected(person);
-                  setIsOpen(false);
-                }}
-              >
-                <p className="has-text-link">{person.name}</p>
-              </div>
-            ))
-          ) : (
-            <div
-              className="
-      notification
-      is-danger
-      is-light
-      mt-3
-      is-align-self-flex-start
-    "
-              role="alert"
-              data-cy="no-suggestions-message"
-            >
-              <p className="has-text-danger">No matching suggestions</p>
-            </div>
-          )}
-        </div>
+        <div className="dropdown-content">{suggestionsContent}</div>
       </div>
     </div>
   );
