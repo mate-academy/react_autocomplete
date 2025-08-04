6df839849836d9dc91f22aa2bec8d172f9d9c259
Dmytro Hrabchak <dmytro@MacBook-Air-Dmytro.local>
2025-08-04 19:21:35 +0300

    fix linter problems

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
 import { Person } from './types/Person';
