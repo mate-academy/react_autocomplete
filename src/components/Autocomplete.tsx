c2bb7f051cf7ed78f34a53413715f5cea59738d6
vmikhailov <tavis.ua@gmail.com>
2025-08-18 14:43:15 +0300

    refactoring code

--- src/components/Autocomplete.tsx
+++ src/components/Autocomplete.tsx
@@ -29,7 +29,11 @@ export const Autocomplete: FC<Props> = ({
 
   const filteredPeople = isOnlySpaces
     ? []
-    : people.filter(person => person.name.toLowerCase().includes(debouncedQuery.toLowerCase()));
+    : people.filter(person =>
+      person.name
+        .toLowerCase()
+        .includes(debouncedQuery.toLowerCase()),
+    );
 
   return (
     <div className="dropdown is-active">
