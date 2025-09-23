6ad0fa33e541957e472ea5111278deeb04521690
Anastasiia Boreiko <kravchenkoanastasiia92@gmail.com>
2025-09-23 13:15:22 +0300

    React_Autocomplete

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,188 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useRef } from 'react';
+import cn from 'classnames';
+
 import './App.scss';
 import { peopleFromServer } from './data/people';
+import { Person } from './types/Person';
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  //#region State
+  const [inputValue, setInputValue] = useState(''); // value that we type in the input
+  const [appliedQuery, setAppliedQuery] = useState(''); // value that we use for filtering the array of people
+  const [suggestions, setSuggestions] = useState<Person[]>([]); // array of filtered people
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isFocused, setFocused] = useState(false);
+  const [highlighted, setHighlighted] = useState<number>(-1);
+  //#endregion
+
+  //#region Ref
+  const timerRef = useRef<number | null>(null);
+  const lastAppliedRef = useRef<string>('');
+  //#endregion
+
+  //#region Constants
+  const delay = 300;
+  //#endregion
+
+  //#region UseEffect
+  //reset timerRef.current if delay has been changed
+  useEffect(() => {
+    return () => {
+      if (timerRef.current) {
+        window.clearTimeout(timerRef.current);
+      }
+    };
+  }, [delay]);
+
+  useEffect(() => {
+    const normalizedQuery = appliedQuery.trim().toLowerCase();
+
+    if (lastAppliedRef.current === appliedQuery) {
+      // use appliedQuery as its what the user typed
+      return;
+    }
+
+    lastAppliedRef.current = appliedQuery;
+
+    if (isFocused && normalizedQuery === '') {
+      setSuggestions(peopleFromServer);
+      // setHighlighted(0);
+
+      return;
+    }
+
+    if (normalizedQuery === '') {
+      setSuggestions([]);
+      // setHighlighted(-1);
+
+      return;
+    }
+
+    const newSuggestion = peopleFromServer.filter(person =>
+      person.name.toLowerCase().includes(normalizedQuery),
+    );
+
+    setSuggestions(newSuggestion);
+
+    if (newSuggestion.length > 0 && isOpen) {
+      setHighlighted(0);
+    } else {
+      setHighlighted(-1);
+    }
+  }, [appliedQuery, isFocused, isOpen]);
+  //#endregion
+
+  //#region handleChanges
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+
+    setInputValue(value); // change input value immediately
+    setSelectedPerson(null); // reset any selected person when starting typing in the input
+    setIsOpen(true); // make the list visible while operating with input
+
+    //debounce
+    if (timerRef.current) {
+      window.clearTimeout(timerRef.current);
+    }
+
+    timerRef.current = window.setTimeout(() => {
+      setAppliedQuery(value);
+    }, delay);
+  };
+
+  const handleFocusChange = () => {
+    setFocused(true);
+
+    if (inputValue.trim() === '') {
+      setSuggestions(peopleFromServer);
+      setIsOpen(true);
+    }
+  };
+
+  const handleBlurChange = () => {
+    setFocused(false);
+    setIsOpen(false);
+  };
+
+  const handlePick = (person: Person) => {
+    setInputValue(person.name);
+    setIsOpen(false);
+    setAppliedQuery(person.name);
+    setSelectedPerson(person);
+  };
+
+  // const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+  //   if (event.key === 'Escape') {
+  //     setIsOpen(false);
+  //   }
+  // };
+
+  //#endregion
 
   return (
     <div className="container">
       <main className="section is-flex is-flex-direction-column">
         <h1 className="title" data-cy="title">
-          {`${name} (${born} - ${died})`}
+          {selectedPerson
+            ? `${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`
+            : 'No selected person'}
         </h1>
 
-        <div className="dropdown is-active">
+        <div className={cn('dropdown', { 'is-active': isOpen })}>
           <div className="dropdown-trigger">
             <input
               type="text"
               placeholder="Enter a part of the name"
               className="input"
               data-cy="search-input"
+              value={inputValue}
+              onChange={handleInputChange}
+              onFocus={handleFocusChange}
+              onBlur={handleBlurChange}
+              // onKeyDown={handleKeyDown}
             />
           </div>
 
           <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
             <div className="dropdown-content">
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Bernard Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Antone Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Petronella de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Hercke</p>
-              </div>
+              {suggestions.map((person, index) => (
+                <a
+                  key={person.slug}
+                  className={cn('dropdown-item', {
+                    'is-active': index === highlighted,
+                  })}
+                  data-cy="suggestion-item"
+                  onMouseDown={() => handlePick(person)}
+                  onMouseEnter={() => setHighlighted(index)}
+                  role="option"
+                  aria-selected={index === highlighted}
+                  id={`opt-${index}`}
+                >
+                  {person.name}
+                </a>
+              ))}
             </div>
           </div>
         </div>
 
-        <div
-          className="
-            notification
-            is-danger
-            is-light
-            mt-3
-            is-align-self-flex-start
-          "
-          role="alert"
-          data-cy="no-suggestions-message"
-        >
-          <p className="has-text-danger">No matching suggestions</p>
-        </div>
+        {isOpen && appliedQuery.trim() !== '' && suggestions.length === 0 && (
+          <div
+            className="
+              notification
+              is-danger
+              is-light
+              mt-3
+              is-align-self-flex-start
+            "
+            role="alert"
+            data-cy="no-suggestions-message"
+          >
+            <p className="has-text-danger">No matching suggestions</p>
+          </div>
+        )}
       </main>
     </div>
   );
