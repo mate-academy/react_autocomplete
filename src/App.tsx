60efee0fb7b146f4b33cf289e67c2112eabaf3c2
Ivan Shcherbatiuk <shcherbatyuk13@gmail.com>
2025-12-13 21:50:22 +0200

    add task solution2

--- src/App.tsx
+++ src/App.tsx
@@ -1,93 +1,14 @@
-import React, { useMemo, useState } from 'react';
 import './App.scss';
 import 'bulma/css/bulma.min.css';
-import debounce from 'lodash.debounce';
-import classNames from 'classnames';
 
 import { peopleFromServer } from './data/people';
 import { Autocomplete } from './components/Autocomplete';
-import { Person } from './types/Person';
 
 export const App: React.FC = () => {
-  const persons = peopleFromServer;
-  const [query, setQuery] = useState('');
-  const [appliedQuery, setAppliedQuery] = useState('');
-  const [showField, setShowField] = useState(false);
-  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
-
-  const debouncedApplyQuery = useMemo(() => debounce(setAppliedQuery, 300), []);
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
-
-    setQuery(value);
-    debouncedApplyQuery(value);
-    setSelectedPerson(null);
-  };
-
-  const visiblePersons = persons.filter(person =>
-    person.name.toLowerCase().includes(appliedQuery.toLowerCase()),
-  );
-
   return (
     <div className="container">
       <main className="section is-flex is-flex-direction-column">
-        <Autocomplete person={selectedPerson} />
-
-        <div
-          className={classNames('dropdown', {
-            'is-active': showField && visiblePersons.length > 0,
-          })}
-        >
-          <div className="dropdown-trigger">
-            <input
-              type="text"
-              className="input"
-              placeholder="Enter a part of the name"
-              data-cy="search-input"
-              value={query}
-              onChange={handleInputChange}
-              onFocus={() => setShowField(true)}
-            />
-          </div>
-
-          <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
-            <div className="dropdown-content">
-              {visiblePersons.map(person => (
-                <div
-                  key={person.slug}
-                  className="dropdown-item"
-                  data-cy="suggestion-item"
-                  onClick={() => {
-                    setSelectedPerson(person);
-                    setShowField(false);
-                    setQuery('');
-                  }}
-                >
-                  <p
-                    className={classNames({
-                      'has-text-link': person.sex === 'm',
-                      'has-text-danger': person.sex === 'f',
-                    })}
-                  >
-                    {person.name}
-                  </p>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-
-        {showField && visiblePersons.length === 0 && (
-          <div
-            className="notification
-            is-danger is-light mt-3 is-align-self-flex-start"
-            role="alert"
-            data-cy="no-suggestions-message"
-          >
-            <p className="has-text-danger">No matching suggestions</p>
-          </div>
-        )}
+        <Autocomplete people={peopleFromServer} delay={300} />
       </main>
     </div>
   );
--- src/components/Autocomplete/Autocomplete.tsx
+++ src/components/Autocomplete/Autocomplete.tsx
@@ -1,23 +1,106 @@
+import { useMemo, useState } from 'react';
+import debounce from 'lodash.debounce';
+import classNames from 'classnames';
+
 import { Person } from '../../types/Person';
 
 type Props = {
-  person: Person | null;
+  people: Person[];
+  onSelect?: (person: Person | null) => void;
+  delay?: number;
 };
 
-export const Autocomplete: React.FC<Props> = ({ person }) => {
-  if (!person) {
-    return (
-      <h1 className="title" data-cy="title">
-        No selected person
-      </h1>
-    );
-  }
+export const Autocomplete: React.FC<Props> = ({
+  people,
+  onSelect,
+  delay = 300,
+}) => {
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
 
-  const { name, born, died } = person;
+  const debouncedApplyQuery = useMemo(
+    () => debounce(setAppliedQuery, delay),
+    [delay],
+  );
+
+  const visiblePeople = people.filter(person =>
+    person.name.toLowerCase().includes(appliedQuery.toLowerCase()),
+  );
+
+  const handleInputChange = (value: string) => {
+    setQuery(value);
+    debouncedApplyQuery(value);
+    setSelectedPerson(null);
+    onSelect?.(null);
+  };
+
+  const handleSelectPerson = (person: Person) => {
+    setSelectedPerson(person);
+    setShowDropdown(false);
+    setQuery('');
+    onSelect?.(person);
+  };
 
   return (
-    <h1 className="title" data-cy="title">
-      {`${name} (${born} - ${died})`}
-    </h1>
+    <>
+      <h1 className="title" data-cy="title">
+        {selectedPerson
+          ? `${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`
+          : 'No selected person'}
+      </h1>
+
+      <div
+        className={classNames('dropdown', {
+          'is-active': showDropdown && visiblePeople.length > 0,
+        })}
+      >
+        <div className="dropdown-trigger">
+          <input
+            type="text"
+            className="input"
+            placeholder="Enter a part of the name"
+            data-cy="search-input"
+            value={query}
+            onChange={e => handleInputChange(e.target.value)}
+            onFocus={() => setShowDropdown(true)}
+          />
+        </div>
+
+        <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
+          <div className="dropdown-content">
+            {visiblePeople.map(person => (
+              <div
+                key={person.slug}
+                className="dropdown-item"
+                data-cy="suggestion-item"
+                onClick={() => handleSelectPerson(person)}
+              >
+                <p
+                  className={classNames({
+                    'has-text-link': person.sex === 'm',
+                    'has-text-danger': person.sex === 'f',
+                  })}
+                >
+                  {person.name}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      {showDropdown && visiblePeople.length === 0 && (
+        <div
+          className="notification
+          is-danger is-light mt-3 is-align-self-flex-start"
+          role="alert"
+          data-cy="no-suggestions-message"
+        >
+          <p className="has-text-danger">No matching suggestions</p>
+        </div>
+      )}
+    </>
   );
 };
