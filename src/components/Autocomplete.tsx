9a6ee6608328910ddb89c7c574576bfc4ef3d871
chumachenkoUA <kirito22122005@gmail.com>
2026-02-03 23:37:44 +0200

    first try

---
+++ src/components/Autocomplete.tsx
@@ -0,0 +1,85 @@
+import React, { useState, useMemo } from 'react';
+import debounce from 'lodash.debounce';
+import { Person } from '../types/Person';
+
+type Props = {
+  people: Person[];
+  delay?: number;
+  onSelected: (person: Person | null) => void;
+};
+
+export const Autocomplete: React.FC<Props> = ({
+  people,
+  delay = 300,
+  onSelected,
+}) => {
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
+  const [isFocused, setIsFocused] = useState(false);
+
+  const applyQuery = useMemo(() => debounce(setAppliedQuery, delay), [delay]);
+
+  const filteredPeople = people.filter(person =>
+    person.name.toLowerCase().includes(appliedQuery.toLowerCase()),
+  );
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+    applyQuery(e.target.value);
+    onSelected(null);
+  };
+
+  const handleSelect = (person: Person) => {
+    setQuery(person.name);
+    setAppliedQuery(person.name);
+    setIsFocused(false);
+    onSelected(person);
+  };
+
+  const showDropdown = isFocused && (query === '' || filteredPeople.length > 0);
+
+  return (
+    <div className="dropdown is-active">
+      <div className="dropdown-trigger">
+        <input
+          type="text"
+          className="input"
+          data-cy="search-input"
+          placeholder="Enter a part of the name"
+          value={query}
+          onChange={handleChange}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setTimeout(() => setIsFocused(false), 200)}
+        />
+      </div>
+
+      {showDropdown && (
+        <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
+          <div className="dropdown-content">
+            {filteredPeople.map(person => (
+              <div
+                key={person.slug}
+                className="dropdown-item"
+                data-cy="suggestion-item"
+                onClick={() => handleSelect(person)}
+              >
+                <p className="has-text-link">{person.name}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+
+      {isFocused && appliedQuery && filteredPeople.length === 0 && (
+        <div
+          className="notification
+          is-danger is-light mt-3 is-align-self-flex-start"
+          role="alert"
+          data-cy="no-suggestions-message"
+        >
+          <p className="has-text-danger">No matching suggestions</p>
+        </div>
+      )}
+    </div>
+  );
+};
