d0f461e9c1181d9a46a4a909479ec562f2f78c4c
Vladyslav <vkobeluh@gmail.com>
2026-03-14 14:05:22 +0200

    add solution

---
+++ src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,112 @@
+import React, { useEffect, useMemo, useState } from 'react';
+import { Person } from '../../types/Person';
+import './Dropdown.scss';
+import { clsx } from 'clsx';
+
+interface Props {
+  people: Person[];
+  onSelected: (value: Person | null) => void;
+}
+
+export const Dropdown = ({ people, onSelected }: Props) => {
+  const [inputText, setInputText] = useState('');
+  const [filterQuery, setFilterQuery] = useState('');
+  const [isPersonSelected, setIsPersonSelected] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsPersonSelected(false);
+    onSelected(null);
+    setInputText(event.target.value);
+  };
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setFilterQuery(inputText);
+    }, 1000);
+
+    return () => clearTimeout(timerId);
+  }, [inputText]);
+
+  const filteredPeople = useMemo(() => {
+    if (!filterQuery) {
+      return people;
+    }
+
+    const normalizedQuery = filterQuery.toLowerCase();
+
+    return people.filter(person => {
+      return person.name.toLowerCase().includes(normalizedQuery);
+    });
+  }, [filterQuery, people]);
+
+  const hasNoSuggests = filteredPeople.length === 0;
+  const isTyping = inputText !== filterQuery;
+
+  const handlePersonSelect = (person: Person) => {
+    setInputText(person.name);
+    setFilterQuery(person.name);
+    setIsPersonSelected(true);
+    onSelected(person);
+  };
+
+  return (
+    <>
+      <div
+        className={clsx('dropdown', {
+          'is-active':
+            isOpen && !hasNoSuggests && !isPersonSelected && !isTyping,
+        })}
+      >
+        <div className="dropdown-trigger">
+          <input
+            type="text"
+            placeholder="Enter a part of the name"
+            className="input"
+            data-cy="search-input"
+            value={inputText}
+            onChange={onTextChange}
+            onFocus={() => setIsOpen(true)}
+            onBlur={() => setIsOpen(false)}
+          />
+        </div>
+
+        <div
+          className="dropdown-menu"
+          role="menu"
+          data-cy="suggestions-list"
+          onMouseDown={event => event.preventDefault()}
+        >
+          <div className="dropdown-content">
+            {filteredPeople.map(person => (
+              <div
+                key={person.name}
+                className="dropdown-item"
+                data-cy="suggestion-item"
+                onClick={() => handlePersonSelect(person)}
+              >
+                <p className="has-text-link">{person.name}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      {hasNoSuggests && !isPersonSelected && !isTyping && (
+        <div
+          className="
+            notification
+            is-danger
+            is-light
+            mt-3
+            is-align-self-flex-start
+          "
+          role="alert"
+          data-cy="no-suggestions-message"
+        >
+          <p className="has-text-danger">No matching suggestions</p>
+        </div>
+      )}
+    </>
+  );
+};
