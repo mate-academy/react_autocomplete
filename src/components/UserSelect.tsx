ef2b4faad973a7050dca03bcc2a62761cfcfb1fe
Mykola Fatkullin <fatkullinmykola@gmail.com>
2026-03-15 14:01:21 +0200

    Added solution for user selection
    (cherry picked from commit b46d916b1f57b6a9781a6e9557715a5d604c0060)

---
+++ src/components/UserSelect.tsx
@@ -0,0 +1,97 @@
+import { Person } from '../types/Person';
+import React, { useCallback, useMemo } from 'react';
+import debounce from 'lodash.debounce';
+import classNames from 'classnames';
+
+type Props = {
+  people: Person[];
+  delay?: number;
+  onSelect?: (person: Person | null) => void;
+};
+
+export const UserSelect = ({
+  people,
+  delay = 300,
+  onSelect = () => {},
+}: Props) => {
+  const [query, setQuery] = React.useState('');
+  const [appliedQuery, setAppliedQuery] = React.useState('');
+  const [isDropdownOpen, setIsDropdownOpen] = React.useState(false);
+
+  const applyQuery = useCallback(debounce(setAppliedQuery, delay), [
+    appliedQuery,
+  ]);
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+    applyQuery(event.target.value);
+    onSelect(null);
+  };
+
+  const filterPeople = useMemo(() => {
+    if (!appliedQuery) {
+      return people;
+    }
+
+    return people.filter(person => person.name.includes(appliedQuery));
+  }, [people, appliedQuery]);
+
+  return (
+    <>
+      <div className={classNames('dropdown', { 'is-active': isDropdownOpen })}>
+        <div className="dropdown-trigger">
+          <input
+            type="text"
+            placeholder="Enter a part of the name"
+            className="input"
+            data-cy="search-input"
+            value={query}
+            onChange={handleQueryChange}
+            onFocus={() => setIsDropdownOpen(true)}
+            onBlur={() => setIsDropdownOpen(false)}
+          />
+        </div>
+
+        {isDropdownOpen && (
+          <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
+            <div className="dropdown-content">
+              {filterPeople.map(person => (
+                <div
+                  className="dropdown-item"
+                  key={person.slug}
+                  data-cy="suggestion-item"
+                  onClick={() => onSelect(person)}
+                >
+                  <p
+                    className={classNames({
+                      'has-text-link': person.sex === 'm',
+                      'has-text-danger': person.sex === 'f',
+                    })}
+                  >
+                    {person.name}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+
+      {filterPeople.length === 0 && (
+        <div
+          className="
+            notification
+            is-danger
+            is-light
+            mt-3
+            is-align-self-flex-start
+          "
+          role="alert"
+          data-cy="no-suggestions-message"
+        >
+          <p className="has-text-danger">No matching suggestions</p>
+        </div>
+      )}
+    </>
+  );
+};
