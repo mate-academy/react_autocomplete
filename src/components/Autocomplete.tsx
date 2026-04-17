0d89c20ce29f1c3e4b05112f939bc87c8887beea
Yevhen Serdiuk <yevhen.srdk@gmail.com>
2026-04-17 15:25:11 +0300

    renamed data-cy attribute to data-qa and renamed onFocus func to handleFocus

--- src/components/Autocomplete.tsx
+++ src/components/Autocomplete.tsx
@@ -49,7 +49,7 @@ export const Autocomplete: React.FC<Props> = ({
     onSelected(null);
   };
 
-  const onFocus = () => {
+  const handleFocus = () => {
     setIsOpen(true);
   };
 
@@ -60,20 +60,20 @@ export const Autocomplete: React.FC<Props> = ({
           type="text"
           placeholder="Enter a part of the name"
           className="input"
-          data-cy="search-input"
+          data-qa="search-input"
           value={query}
           onChange={handleQueryChange}
-          onFocus={onFocus}
+          onFocus={handleFocus}
         />
       </div>
 
-      <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
+      <div className="dropdown-menu" role="menu" data-qa="suggestions-list">
         <div className="dropdown-content">
           {suggestions.map((human: Person) => (
             <div
               key={human.name + human.born}
               className="dropdown-item"
-              data-cy="suggestion-item"
+              data-qa="suggestion-item"
               onMouseDown={() => {
                 setQuery(human.name);
                 setIsOpen(false);
@@ -96,7 +96,7 @@ export const Autocomplete: React.FC<Props> = ({
             is-align-self-flex-start
           "
           role="alert"
-          data-cy="no-suggestions-message"
+          data-qa="no-suggestions-message"
         >
           <p className="has-text-danger">No matching suggestions</p>
         </div>
